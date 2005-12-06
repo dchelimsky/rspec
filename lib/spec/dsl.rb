9f14b9206f5f755b49e63760492909d8a72c9720
srbaker <srbaker@410327ef-2207-0410-a325-f78bbcb22a5a>
2005-12-06 00:18:04 +0000

    * move specification addition test to its own file  * specification method should add specification method to default context
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@132 410327ef-2207-0410-a325-f78bbcb22a5a

--- lib/spec/dsl.rb
+++ lib/spec/dsl.rb
@@ -3,6 +3,9 @@ require 'spec'
 module DSLExtensions
 
   def specification(name, &block)
+    $default_context ||= Class.new(Spec::Context)
+    
+    $default_context.add_specification(name, &block)
   end
 
 end
