ce6f8862dd40c415be450a0bf27224f21d10d08c
srbaker <srbaker@410327ef-2207-0410-a325-f78bbcb22a5a>
2005-12-09 00:40:46 +0000

    add context keyword to the RSpec DSL
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@141 410327ef-2207-0410-a325-f78bbcb22a5a

--- lib/spec/dsl.rb
+++ lib/spec/dsl.rb
@@ -2,10 +2,16 @@ require 'spec'
 
 module DSLExtensions
 
+  def context(name)
+    eval "$#{name} = Class.new(Spec::Context)"
+    $current_context = eval "$#{name}"
+  end
+
   def specification(name, &block)
     $default_context ||= Class.new(Spec::Context)
-    
-    $default_context.add_specification(name.to_sym, &block)
+    $current_context = $default_context if $current_context.nil?
+
+    $current_context.add_specification(name.to_sym, &block)
   end
 
 end
--- test/dsl_test.rb
+++ test/dsl_test.rb
@@ -4,6 +4,11 @@ require 'spec'
 
 class DSLTest < Test::Unit::TestCase
   
+  def setup
+    $default_context = nil
+    $current_context = nil
+  end
+  
   def test_should_have_specification_method_defined
     assert_equal true, Object.public_method_defined?(:specification)
   end
@@ -12,6 +17,32 @@ class DSLTest < Test::Unit::TestCase
     specification('foobar') { true.should_equal true }
     
     assert_equal true, $default_context.specifications.include?('foobar')
+  end
+  
+  def test_should_create_new_context
+    context 'my_context'
+    
+    assert_equal Spec::Context, $my_context.superclass
+  end
+  
+  def test_should_set_current_context_to_new_context
+    context 'bar'
+    
+    assert_equal $current_context, $bar
+  end
+  
+  def test_should_add_bar_specification_to_foo_context
+    context 'foo'
+    specification('bar') { true.should_equal true }
+    
+    assert_equal true, $foo.specifications.include?('bar')
+  end
+  
+  def test_should_add_bar_specification_to_current_context
+    context 'foo'
+    specification('bar') { true.should_equal true }
+    
+    assert_equal true, $current_context.specifications.include?('bar')
   end
 
 end
