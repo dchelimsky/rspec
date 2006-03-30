40b5d27b0a5fb16f21a9d42785023bfa12c5ed94
aslak_hellesoy <aslak_hellesoy@410327ef-2207-0410-a325-f78bbcb22a5a>
2006-03-30 13:55:06 +0000

    fixed tiny space bug
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@219 410327ef-2207-0410-a325-f78bbcb22a5a

--- lib/spec/tool/test_unit_converter.rb
+++ lib/spec/tool/test_unit_converter.rb
@@ -32,14 +32,14 @@ module Spec
         test_unit_file.each do |line|
           if line =~ /^require .*test.*/
             line = "require 'spec'\n"
-          elsif line =~ /^(\s+)class\s+(.*)\s+<\s+Test::Unit::TestCase/
+          elsif line =~ /^(\s*)class\s+(.*)\s+<\s+Test::Unit::TestCase/
             spaces = $1
             class_name = $2
 
             context_name = class_name.match(/(.*)Test/)[1]
             line = "#{spaces}context \"#{context_name}\" do\n"
             
-          elsif line =~ /(\s+)def\s+test_(.*)/
+          elsif line =~ /(\s*)def\s+test_(.*)/
             spaces = $1
             method_meaning = $2
 
