d5b1a7c671c6778a00cdb8608cca2225dd64c040
aslak_hellesoy <aslak_hellesoy@410327ef-2207-0410-a325-f78bbcb22a5a>
2006-03-31 08:16:50 +0000

    reorder/simplify execution of teardown
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@229 410327ef-2207-0410-a325-f78bbcb22a5a

--- lib/spec/new_runner/specification.rb
+++ lib/spec/new_runner/specification.rb
@@ -12,11 +12,6 @@ module Spec
       execution_context = ::Spec::Runner::ExecutionContext.new(self)
       begin
         execution_context.instance_exec(&setup_block) unless setup_block.nil?
-      rescue => e
-        @errors << e
-      end
-
-      begin
         execution_context.instance_exec(&@block)
       rescue => e
         @errors << e
@@ -24,11 +19,6 @@ module Spec
 
       begin
         execution_context.instance_exec(&teardown_block) unless teardown_block.nil?
-      rescue => e
-        @errors << e
-      end
-
-      begin
         @mocks.each do |mock|
           mock.__verify
         end
