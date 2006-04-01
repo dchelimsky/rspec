80bed8d2910b0982f90748494a6e4e02cfc4d100
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2006-04-01 08:25:04 +0000

    moved Specification to Spec::Runner
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@230 410327ef-2207-0410-a325-f78bbcb22a5a

--- lib/spec/new_runner/specification.rb
+++ lib/spec/new_runner/specification.rb
@@ -1,41 +1,42 @@
 module Spec
-  class Specification
+  module Runner
+    class Specification
     
-    def initialize(name, &block)
-      @name = name
-      @block = block
-      @mocks = []
-      @errors = []
-    end
-    
-    def run(reporter=nil, setup_block=nil, teardown_block=nil)
-      execution_context = ::Spec::Runner::ExecutionContext.new(self)
-      begin
-        execution_context.instance_exec(&setup_block) unless setup_block.nil?
-        execution_context.instance_exec(&@block)
-      rescue => e
-        @errors << e
+      def initialize(name, &block)
+        @name = name
+        @block = block
+        @mocks = []
+        @errors = []
       end
-
-      begin
-        execution_context.instance_exec(&teardown_block) unless teardown_block.nil?
-        @mocks.each do |mock|
-          mock.__verify
+    
+      def run(reporter=nil, setup_block=nil, teardown_block=nil)
+        execution_context = ::Spec::Runner::ExecutionContext.new(self)
+        begin
+          execution_context.instance_exec(&setup_block) unless setup_block.nil?
+          execution_context.instance_exec(&@block)
+        rescue => e
+          @errors << e
         end
-      rescue => e
-        @errors << e
+
+        begin
+          execution_context.instance_exec(&teardown_block) unless teardown_block.nil?
+          @mocks.each do |mock|
+            mock.__verify
+          end
+        rescue => e
+          @errors << e
+        end
+
+        reporter.add_spec(@name, @errors) unless reporter.nil?
       end
-
-      reporter.add_spec(@name, @errors) unless reporter.nil?
-    end
     
-    def run_docs(reporter)
-      reporter.add_spec(@name)
-    end
+      def run_docs(reporter)
+        reporter.add_spec(@name)
+      end
     
-    def add_mock(mock)
-      @mocks << mock
+      def add_mock(mock)
+        @mocks << mock
+      end
     end
-
   end
 end
