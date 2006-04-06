a01b789bbf5495129514e5e35548102856c78bdb
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2006-04-06 12:10:50 +0000

    oops, forgot the test
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@254 410327ef-2207-0410-a325-f78bbcb22a5a

---
+++ test/spec/api/mock_arg_constraints_test.rb
@@ -0,0 +1,90 @@
+require File.dirname(__FILE__) + '/../../test_helper'
+
+module Spec
+  module Api
+    class PassingConstraintsTest < Test::Unit::TestCase
+      
+      def setup
+        @mock = Mock.new("test mock")
+      end
+
+      def test_should_handle_anything
+        @mock.should.receive(:random_call).at.least(:once).with(:anything)
+        @mock.random_call()
+        @mock.random_call(1)
+        @mock.random_call("a", 2)
+        @mock.random_call([], {}, "joe", 7)
+        @mock.__verify
+      end
+      
+      def test_should_handle_anything_in_multi_args
+        @mock.should.receive(:random_call).with("a", :anything, "c")
+        @mock.random_call("a", "whatever", "c")
+        @mock.__verify
+      end
+      
+      def test_should_accept_fixnum_as_numeric
+        @mock.should.receive(:random_call).with(:numeric)
+        @mock.random_call(1)
+        @mock.__verify
+      end
+      
+      def test_should_accept_float_as_numeric
+        @mock.should.receive(:random_call).with(:numeric)
+        @mock.random_call(1.5)
+        @mock.__verify
+      end
+      
+      def test_should_match_true_for_boolean
+        @mock.should.receive(:random_call).with(:boolean)
+        @mock.random_call(true)
+        @mock.__verify
+      end
+      
+      def test_should_match_false_for_boolean
+        @mock.should.receive(:random_call).with(:boolean)
+        @mock.random_call(false)
+        @mock.__verify
+      end
+      
+      def test_should_match_string
+        @mock.should.receive(:random_call).with(:string)
+        @mock.random_call("a string")
+        @mock.__verify
+      end
+      
+    end
+    
+    class FailingConstraintsTest < Test::Unit::TestCase
+      
+      def setup
+        @mock = Mock.new("test mock")
+      end
+
+      def test_should_reject_non_numeric
+        @mock.should.receive(:random_call).with(:numeric)
+        assert_raise(MockExpectationError) do
+          @mock.random_call("1")
+          @mock.__verify
+        end
+      end
+      
+      def test_should_reject_non_boolean
+        @mock.should.receive(:random_call).with(:boolean)
+        assert_raise(MockExpectationError) do
+          @mock.random_call("false")
+          @mock.__verify
+        end
+      end
+      
+      def test_should_reject_non_string
+        @mock.should.receive(:random_call).with(:string)
+        assert_raise(MockExpectationError) do
+          @mock.random_call(123)
+          @mock.__verify
+        end
+      end
+      
+    end
+  end
+end
