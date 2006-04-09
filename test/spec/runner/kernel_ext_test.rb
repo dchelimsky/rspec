8437367f0894d0c3dcd8c78ee7d76a89e9caa6c3
dastels <dastels@410327ef-2207-0410-a325-f78bbcb22a5a>
2006-04-09 18:05:36 +0000

    added tests for kernel_ext
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@301 410327ef-2207-0410-a325-f78bbcb22a5a

---
+++ test/spec/runner/kernel_ext_test.rb
@@ -0,0 +1,14 @@
+require File.dirname(__FILE__) + '/../../test_helper'
+
+module Spec
+  module Runner
+    class KernelExtTest < Test::Unit::TestCase
+      def test_create_context
+        assert_nothing_raised do
+          @cxt = context("") {}
+        end
+        assert @cxt.instance_of? Spec::Runner::Context
+      end
+    end
+  end
+end
