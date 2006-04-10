b48ec62f42b7c291d0a1610d0e386de3e181f9b9
dastels <dastels@410327ef-2207-0410-a325-f78bbcb22a5a>
2006-04-10 01:12:00 +0000

    boost test coverage
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@304 410327ef-2207-0410-a325-f78bbcb22a5a

--- test/spec/runner/context_runner_test.rb
+++ test/spec/runner/context_runner_test.rb
@@ -2,6 +2,7 @@ require File.dirname(__FILE__) + '/../../test_helper'
 module Spec
   module Runner
     class ContextRunnerTest < Test::Unit::TestCase
+    
       def test_should_call_run_doc_on_context
         context1 = Api::Mock.new "context1"
         context2 = Api::Mock.new "context2"
@@ -27,6 +28,15 @@ module Spec
         context1.__verify
         context2.__verify
       end
+      
+
+      def test_should_call_run_for_standalone
+        context1 = Api::Mock.new "context1"
+        context1.should.receive(:run)
+        runner = ContextRunner.standalone context1
+        context1.__verify
+      end
+      
     end
   end
 end
