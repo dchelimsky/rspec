030044b39ff50fb1313c7d515ed9b877842e3f3c
dastels <dastels@410327ef-2207-0410-a325-f78bbcb22a5a>
2006-04-18 06:09:55 +0000

    tweaked to reflect DuckType renaming/replacement
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@349 410327ef-2207-0410-a325-f78bbcb22a5a

--- test/spec/api/duck_type_test.rb
+++ test/spec/api/duck_type_test.rb
@@ -5,13 +5,13 @@ module Spec
     class DuckTypeTest < Test::Unit::TestCase
       
       def test_should_talk_like_something_with_one_message_specified
-        duck_type = DuckType.new(:length)
-        assert duck_type.walks_like?([])
+        duck_type = DuckTypeArgConstraint.new(:length)
+        assert duck_type.matches?([])
       end
 
       def test_should_talk_like_something_with_two_messages_specified
-        duck_type = DuckType.new(:length, :empty?)
-        assert duck_type.walks_like?([])
+        duck_type = DuckTypeArgConstraint.new(:length, :empty?)
+        assert duck_type.matches?([])
       end
 
     end
--- test/spec/api/mock_arg_constraints_test.rb
+++ test/spec/api/mock_arg_constraints_test.rb
@@ -45,13 +45,13 @@ module Spec
       end
       
       def test_should_process_duck_type_with_one_method
-        @mock.should.receive(:random_call).with(DuckType.new(:length))
+        @mock.should.receive(:random_call).with(DuckTypeArgConstraint.new(:length))
         @mock.random_call([])
         @mock.__verify
       end
       
       def test_should_process_duck_type_with_two_methods
-        @mock.should.receive(:random_call).with(DuckType.new(:abs, :div))
+        @mock.should.receive(:random_call).with(DuckTypeArgConstraint.new(:abs, :div))
         @mock.random_call(1)
         @mock.__verify
       end
@@ -89,7 +89,7 @@ module Spec
       end
       
       def test_should_reject_goose_when_expecting_a_duck
-        @mock.should.receive(:random_call).with(DuckType.new(:abs, :div))
+        @mock.should.receive(:random_call).with(DuckTypeArgConstraint.new(:abs, :div))
         assert_raise(MockExpectationError) do
           @mock.random_call("I don't respond to :abs or :div")
           @mock.__verify
