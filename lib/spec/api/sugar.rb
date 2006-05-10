5738ac45074e45203844482aa9fc4c86e272f36c
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2006-05-10 10:13:45 +0000

    removed duplication in MessageExpectationSugar
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@455 410327ef-2207-0410-a325-f78bbcb22a5a

--- lib/spec/api/sugar.rb
+++ lib/spec/api/sugar.rb
@@ -5,21 +5,6 @@ module Spec
     module Sugar
       alias_method :__orig_method_missing, :method_missing
       def method_missing(method, *args, &block)
-        if method.to_s[0,7] == "should_"
-          object = self
-          calls = method.to_s.split("_")
-          while calls.length > 1
-            object = object.__send__(calls.shift)
-          end
-          return object.__send__(calls.shift, *args, &block)
-        end
-        __orig_method_missing(method, *args, &block)
-      end
-    end
-    
-    module MessageExpectationSugar
-      alias_method :__orig_method_missing, :method_missing
-      def method_missing(method, *args, &block)
         if should_sweeten? method
           object = self
           calls = method.to_s.split("_")
@@ -31,6 +16,13 @@ module Spec
         __orig_method_missing(method, *args, &block)
       end
       
+      def should_sweeten? name
+        return true if name.to_s[0,7] == "should_"
+        return false
+      end
+    end
+    
+    module MessageExpectationSugar
       def should_sweeten? name
         return true if name.to_s[0,4] == "and_"
         return true if name.to_s[0,3] == "at_"
@@ -51,5 +43,6 @@ class Spec::Api::Mock #:nodoc:
 end
 
 class Spec::Api::MessageExpectation
+  include Spec::Api::Sugar
   include Spec::Api::MessageExpectationSugar
 end
