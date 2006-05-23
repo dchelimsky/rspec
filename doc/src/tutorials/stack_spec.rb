df1637d19da1132f420b36732075b3792cd7b4d5
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2006-05-23 15:57:03 +0000

    tutorial update
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@476 410327ef-2207-0410-a325-f78bbcb22a5a

--- doc/src/tutorials/stack_spec.rb
+++ doc/src/tutorials/stack_spec.rb
@@ -4,38 +4,22 @@ context "A new stack" do
   setup do
     @stack = Stack.new
   end
-
   specify "should be empty" do
     @stack.should_be_empty
   end
 end
 
-context "An empty stack" do
-  setup do
-    @stack = Stack.new
-  end
-
-  specify "should keep its mouth shut when you send it 'push'" do
-    lambda { @stack.push Object.new }.should.not.raise
-  end
-end
-  
 context "A stack with one item" do
   setup do
     @stack = Stack.new
     @stack.push "one item"
   end
 
-  specify "should not be empty" do
-    @stack.should_not_be_empty
-  end
-
   specify "should return top when you send it 'top'" do
     @stack.top.should_equal "one item"
   end
   
-  specify "should not be empty after 'top'" do
-    @stack.top
+  specify "should not be empty" do
     @stack.should_not_be_empty
   end
 end
