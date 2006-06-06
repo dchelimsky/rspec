1aa688158757016263e9fd71e6f18359414dfca6
aslak_hellesoy <aslak_hellesoy@410327ef-2207-0410-a325-f78bbcb22a5a>
2006-06-06 17:00:25 +0000

    Generate a spec for each action
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@492 410327ef-2207-0410-a325-f78bbcb22a5a

--- vendor/rspec_on_rails/vendor/generators/rspec/templates/rspec_controller/templates/controller_spec.rb
+++ vendor/rspec_on_rails/vendor/generators/rspec/templates/rspec_controller/templates/controller_spec.rb
@@ -2,11 +2,19 @@ require File.dirname(__FILE__) + '<%= '/..' * class_nesting_depth %>/../spec_hel
 
 context "The <%= class_name %>Controller" do
   # fixtures :<%= table_name %>
-  controller_name '<%= class_name.underscore %>'
+  controller_name :<%= class_name.underscore %>
 
   specify "should be a <%= class_name %>Controller" do
     controller.should.be.an.instance.of <%= class_name %>Controller
   end
+
+<% for action in actions -%>
+
+  specify "should accept GET to <%= action %>"
+    get '<%= action %>'
+    response.should.be.success
+  end
+<% end -%>
 
   specify "should have more specifications" do
     violated "not enough specs"
