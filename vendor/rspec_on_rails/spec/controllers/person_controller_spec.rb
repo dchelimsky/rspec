8c8dbcf1a84163cdc4a2f078d53601c95e0fd42c
aslak_hellesoy <aslak_hellesoy@410327ef-2207-0410-a325-f78bbcb22a5a>
2006-06-06 21:08:24 +0000

    Spec wording fixes
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@505 410327ef-2207-0410-a325-f78bbcb22a5a

--- vendor/rspec_on_rails/spec/controllers/person_controller_spec.rb
+++ vendor/rspec_on_rails/spec/controllers/person_controller_spec.rb
@@ -14,14 +14,14 @@ context "The PersonController" do
     assigns('people').should_equal [people(:lachie)]
   end
 
-  specify "should instantiate unsaved person record on GET to create" do
+  specify "should create an unsaved person record on GET to create" do
     get 'create'
     response.should_be_success
     response.should_not_be_redirect
     assigns('person').should_be_new_record
   end
 
-  specify "should persist new person and redirect to index on POST to create" do
+  specify "should persist a new person and redirect to index on POST to create" do
     post 'create', {:person => {:name => 'Aslak'}}
     Person.find_by_name('Aslak').should_not_be_nil
     response.should_be_redirect
