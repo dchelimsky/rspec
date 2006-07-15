3e832d64c0ac8ba6849f73b6f9fce5894be20842
aslak_hellesoy <aslak_hellesoy@410327ef-2207-0410-a325-f78bbcb22a5a>
2006-07-15 03:18:33 +0000

    removed aliased context and specify as they now make rspec fall over and bleed on its nose
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@635 410327ef-2207-0410-a325-f78bbcb22a5a

--- failing_examples/airport_spec.rb
+++ failing_examples/airport_spec.rb
@@ -10,29 +10,20 @@ class Airport
   end
 end
 
-class Spec::Runner::Context
-  alias should specify
-  alias must specify
-  alias fact specify
-end
-module Kernel
-  alias topic context
-end
-
-topic "Airport at home" do
+context "Airport at home" do
   setup do
     @airport = Airport.new
   end
   
-  fact "should always work" do
+  specify "should always work" do
     @airport.should_be.working
   end
 
-  must "should not need cables" do
+  specify "should not need cables" do
     @airport.should_not_need :cables
   end
 
-  must "should not need electricity" do
+  specify "should not need electricity" do
     # This will fail...
     @airport.should_not_need :electricity
   end
