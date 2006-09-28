d77855c132bdabdab18d67b954a8561d2e3e5c97
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2006-09-28 04:41:23 +0000

    fixed require statement in view_spec template
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@806 410327ef-2207-0410-a325-f78bbcb22a5a

--- vendor/rspec_on_rails/vendor/plugins/rspec/generators/rspec_controller/templates/view_spec.rb
+++ vendor/rspec_on_rails/vendor/plugins/rspec/generators/rspec_controller/templates/view_spec.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + '<%= '/..' * class_nesting_depth %>/../spec_helper'
+require File.dirname(__FILE__) + '<%= '/..' * class_nesting_depth %>/../../spec_helper'
 
 context "/<%= model %>/<%= action %>" do
   # fixtures :<%= table_name %>
