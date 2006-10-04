316ff134e8a234ee2731b4b7c2afc082e5c06753
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2006-10-04 17:04:05 +0000

    added a couple of settings back to spec_helper in rails plugin
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@848 410327ef-2207-0410-a325-f78bbcb22a5a

--- vendor/rspec_on_rails/vendor/plugins/rspec/generators/rspec/templates/spec_helper.rb
+++ vendor/rspec_on_rails/vendor/plugins/rspec/generators/rspec/templates/spec_helper.rb
@@ -5,7 +5,13 @@ require 'rspec_on_rails'
 module Test
   module Unit
     class TestCase
-      self.fixture_path = RAILS_ROOT + '/spec/fixtures'
+      self.use_transactional_fixtures = true
+      self.use_instantiated_fixtures  = false
+      self.fixture_path = RAILS_ROOT + '/test/fixtures'
+
+      # You can set up your global fixtures here, or you
+      # can do it in individual contexts
+      #fixtures :table_a, :table_b
     end
   end
 end
