2a6610cf75b91c763f6f3c887a32518d8aa8d0d1
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2006-10-04 16:32:54 +0000

    cleaning up rails plugin duplication and dir structure
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@846 410327ef-2207-0410-a325-f78bbcb22a5a

--- vendor/rspec_on_rails/vendor/plugins/rspec/lib/rspec_on_rails.rb
+++ vendor/rspec_on_rails/vendor/plugins/rspec/lib/rspec_on_rails.rb
@@ -8,9 +8,9 @@ require 'action_controller/test_process'
 require 'action_controller/integration'
 
 dir = File.dirname(__FILE__)
-require File.expand_path("#{dir}/spec/expectations/tag_expectations")
-require File.expand_path("#{dir}/spec/expectations/rjs_expectations")
-require File.expand_path("#{dir}/spec/expectations/response_body")
+require File.expand_path("#{dir}/spec/rails_plugin/tag_expectations")
+require File.expand_path("#{dir}/spec/rails_plugin/rjs_expectations")
+require File.expand_path("#{dir}/spec/rails_plugin/response_body")
 require File.expand_path("#{dir}/extensions/active_record/base")
 require File.expand_path("#{dir}/extensions/action_controller/test_response")
 require File.expand_path("#{dir}/extensions/context_eval")
