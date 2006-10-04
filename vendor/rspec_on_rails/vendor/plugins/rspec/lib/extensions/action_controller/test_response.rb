c124191bccba8399d3bae49a67de2ec09c569933
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2006-10-04 16:53:50 +0000

    cleaning up rails plugin duplication and dir structure
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@847 410327ef-2207-0410-a325-f78bbcb22a5a

--- vendor/rspec_on_rails/vendor/plugins/rspec/lib/extensions/action_controller/test_response.rb
+++ vendor/rspec_on_rails/vendor/plugins/rspec/lib/extensions/action_controller/test_response.rb
@@ -7,19 +7,24 @@ module ActionController
     end
     
     def should_have_rjs element, *args
-      Spec::RailsPlugin::ResponseBody.new(self.body).should_have_rjs element, *args
+      __response_body.should_have_rjs element, *args
     end
 
     def should_not_have_rjs element, *args
-      Spec::RailsPlugin::ResponseBody.new(self.body).should_not_have_rjs element, *args
+      __response_body.should_not_have_rjs element, *args
     end
 
     def should_have_tag tag, *opts
-      Spec::RailsPlugin::ResponseBody.new(self.body).should_have_tag tag, *opts
+      __response_body.should_have_tag tag, *opts
     end
 
     def should_not_have_tag tag, *opts
-      Spec::RailsPlugin::ResponseBody.new(self.body).should_not_have_tag tag, *opts
+      __response_body.should_not_have_tag tag, *opts
+    end
+    
+    private
+    def __response_body
+      Spec::RailsPlugin::ResponseBody.new(self.body)
     end
   end
 end
--- vendor/rspec_on_rails/vendor/plugins/rspec/lib/rspec_on_rails.rb
+++ vendor/rspec_on_rails/vendor/plugins/rspec/lib/rspec_on_rails.rb
@@ -1,18 +1,18 @@
 require 'application'
 
 silence_warnings { RAILS_ENV = "test" }
-require 'spec'
-require 'active_record/base'
-require 'active_record/fixtures'
 require 'action_controller/test_process'
 require 'action_controller/integration'
+require 'active_record/base'
+require 'active_record/fixtures'
+require 'spec'
 
 dir = File.dirname(__FILE__)
-require File.expand_path("#{dir}/spec/rails_plugin/tag_expectations")
-require File.expand_path("#{dir}/spec/rails_plugin/rjs_expectations")
-require File.expand_path("#{dir}/spec/rails_plugin/response_body")
 require File.expand_path("#{dir}/extensions/active_record/base")
 require File.expand_path("#{dir}/extensions/action_controller/test_response")
-require File.expand_path("#{dir}/extensions/context_eval")
-require File.expand_path("#{dir}/extensions/context")
 require File.expand_path("#{dir}/extensions/nil_class")
+require File.expand_path("#{dir}/extensions/spec/runner/context_eval")
+require File.expand_path("#{dir}/extensions/spec/runner/context")
+require File.expand_path("#{dir}/spec/rails_plugin/response_body")
+require File.expand_path("#{dir}/spec/rails_plugin/rjs_expectations")
+require File.expand_path("#{dir}/spec/rails_plugin/tag_expectations")
