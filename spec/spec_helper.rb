d9801d27a097ab41dc67d0d8322f194e4844750d
aslak_hellesoy <aslak_hellesoy@410327ef-2207-0410-a325-f78bbcb22a5a>
2006-10-14 20:46:27 +0000

    a little more cleanup
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@887 410327ef-2207-0410-a325-f78bbcb22a5a

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,4 @@ require File.dirname(__FILE__) + '/../spec/spec/test_classes'
 RSPEC_TESTING = true unless defined? RSPEC_TESTING # This causes the diff extension to not be loaded
 require 'spec/expectations/diff'
 $context_runner ||= ::Spec::Runner::OptionParser.create_context_runner(['test'], false, STDERR, STDOUT)
-require 'spec/test_to_spec/sexp_transformer'
-require 'spec/test_to_spec/ruby2ruby'
 
