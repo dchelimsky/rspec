88c1733130153102281d1a28544956bcfdea1bca
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2006-10-16 02:09:23 +0000

    format spec file
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@889 410327ef-2207-0410-a325-f78bbcb22a5a

--- spec/spec/mocks/partial_mock_spec.rb
+++ spec/spec/mocks/partial_mock_spec.rb
@@ -1,41 +1,41 @@
 require File.dirname(__FILE__) + '/../../spec_helper.rb'
 
 module Spec
-module Mocks
-context "PartialMock" do
-    setup do
+  module Mocks
+    context "using a Partial Mock," do
+      setup do
         @object = Object.new
+      end
       
-    end
-    specify "should not receive  should mock out the method" do
+      specify "should_not_receive should mock out the method" do
         @object.should_not_receive(:fuhbar)
         @object.fuhbar
         lambda do
           @object.__verify
         end.should_raise(Spec::Mocks::MockExpectationError)
-      
-    end
-    specify "should not receive  should return a negative message expectation" do
+      end
+
+      specify "should_not_receive should return a negative message expectation" do
         @object.should_not_receive(:foobar).should_be_kind_of(NegativeMessageExpectation)
-      
-    end
-    specify "should receive  should mock out the method" do
+      end
+
+      specify "should_receive should mock out the method" do
         @object.should_receive(:foobar).with(:test_param).and_return(1)
         @object.foobar(:test_param).should_equal(1)
-      
-    end
-    specify "should receive  should return a message expectation" do
+      end
+
+      specify "should_receive should return a message expectation" do
         @object.should_receive(:foobar).should_be_kind_of(MessageExpectation)
-      @object.foobar
-    end
-    specify "should receive  should verify method was called" do
+        @object.foobar
+      end
+
+      specify "should_receive should verify method was called" do
         @object.should_receive(:foobar).with(:test_param).and_return(1)
         lambda do
           @object.__verify
         end.should_raise(Spec::Mocks::MockExpectationError)
-      
+      end
+
     end
-  
-end
-end
+  end
 end
