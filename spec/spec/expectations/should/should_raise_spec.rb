86b1e50cd2401104b5a1c440433ee2b1f37e1401
aslak_hellesoy <aslak_hellesoy@410327ef-2207-0410-a325-f78bbcb22a5a>
2006-10-18 19:51:34 +0000

    formatting
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@922 410327ef-2207-0410-a325-f78bbcb22a5a

--- spec/spec/expectations/should/should_raise_spec.rb
+++ spec/spec/expectations/should/should_raise_spec.rb
@@ -1,73 +1,56 @@
 require File.dirname(__FILE__) + '/../../../spec_helper.rb'
 
-module Spec
-module Expectations
-module Helper
 context "ShouldRaise" do
-    specify "should fail when exact exception is raised with wrong message" do
-        lambda do
-          lambda do
-            raise(StandardError.new("chunky bacon"))
-          end.should_raise(StandardError, "rotten tomatoes")
-        end.should_raise(Spec::Expectations::ExpectationNotMetError)
-      
+  specify "should fail when exact exception is raised with wrong message" do
+    lambda do
+      lambda do
+        raise(StandardError.new("chunky bacon"))
+      end.should_raise(StandardError, "rotten tomatoes")
+    end.should_raise(Spec::Expectations::ExpectationNotMetError)
+  end
+
+  specify "should fail when no exception is raised" do
+    begin
+      lambda {}.should_raise(SyntaxError)
+    rescue 
+      e=$!
     end
-    specify "should fail when no exception is raised" do
-      
-        begin
-        lambda do
-          
-        end.should_raise(SyntaxError)
-        rescue 
-        e=$!
-        
-        
-        
-        end
-        e.message.should_eql("<Proc> should raise <SyntaxError> but raised nothing")
-      
+    e.message.should_eql("<Proc> should raise <SyntaxError> but raised nothing")
+  end
+
+  specify "should fail when wrong exception is raised" do
+    begin
+      lambda do
+        "".nonexistent_method
+      end.should_raise(SyntaxError)
+    rescue 
+      e=$!
     end
-    specify "should fail when wrong exception is raised" do
-      
-        begin
-        lambda do
-          "".nonexistent_method
-        end.should_raise(SyntaxError)
-        rescue 
-        e=$!
-        
-        
-        
-        end
-        e.message.should_eql("<Proc> should raise <SyntaxError> but raised #<NoMethodError: undefined method `nonexistent_method' for \"\":String>")
-      
-    end
-    specify "should pass when exact exception is raised" do
-        lambda do
-          lambda do
-            "".nonexistent_method
-          end.should_raise(NoMethodError)
-        end.should_not_raise
-      
-    end
-    specify "should pass when exact exception is raised with message" do
-        lambda do
-          lambda do
-            raise(StandardError.new("this is standard"))
-          end.should_raise(StandardError, "this is standard")
-        end.should_not_raise
-      
-    end
-    specify "should pass when subclass exception is raised" do
-        lambda do
-          lambda do
-            "".nonexistent_method
-          end.should_raise
-        end.should_not_raise
-      
-    end
+    e.message.should_eql("<Proc> should raise <SyntaxError> but raised #<NoMethodError: undefined method `nonexistent_method' for \"\":String>")
+  end
+
+  specify "should pass when exact exception is raised" do
+    lambda do
+      lambda do
+        "".nonexistent_method
+      end.should_raise(NoMethodError)
+    end.should_not_raise
+  end
+
+  specify "should pass when exact exception is raised with message" do
+    lambda do
+      lambda do
+        raise(StandardError.new("this is standard"))
+      end.should_raise(StandardError, "this is standard")
+    end.should_not_raise
+  end
+
+  specify "should pass when subclass exception is raised" do
+    lambda do
+      lambda do
+        "".nonexistent_method
+      end.should_raise
+    end.should_not_raise
+  end
   
-end
-end
-end
 end
