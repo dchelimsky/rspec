7b29922cbe60a661c3dbf14033bd9f71619f2fe8
aslak_hellesoy <aslak_hellesoy@410327ef-2207-0410-a325-f78bbcb22a5a>
2006-10-18 19:46:04 +0000

    formatting
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@921 410327ef-2207-0410-a325-f78bbcb22a5a

--- spec/spec/expectations/should/should_not_respond_to_spec.rb
+++ spec/spec/expectations/should/should_not_respond_to_spec.rb
@@ -1,23 +1,16 @@
 require File.dirname(__FILE__) + '/../../../spec_helper.rb'
 
-module Spec
-module Expectations
-module Helper
 context "ShouldNotRespondTo" do
-    specify "should fail when target responds to" do
-        lambda do
-          "".should_not_respond_to(:length)
-        end.should_fail
-      
-    end
-    specify "should pass when target doesnt respond to" do
-        lambda do
-          "".should_not_respond_to(:connect)
-        end.should_not_raise
-      
-    end
-  
-end
-end
-end
+
+  specify "should fail when target responds to" do
+    lambda do
+      "".should_not_respond_to(:length)
+    end.should_fail
+  end
+
+  specify "should pass when target doesnt respond to" do
+    lambda do
+      "".should_not_respond_to(:connect)
+    end.should_not_raise
+  end  
 end
