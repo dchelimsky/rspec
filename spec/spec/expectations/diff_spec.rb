77266455fb200a69f907dacdc4d0e406992482d5
aslak_hellesoy <aslak_hellesoy@410327ef-2207-0410-a325-f78bbcb22a5a>
2006-10-18 19:44:17 +0000

    formatting
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@920 410327ef-2207-0410-a325-f78bbcb22a5a

--- spec/spec/expectations/diff_spec.rb
+++ spec/spec/expectations/diff_spec.rb
@@ -2,7 +2,6 @@ require File.dirname(__FILE__) + '/../../spec_helper.rb'
 
 context "Diff" do
   specify "should output unified diff of two strings" do
-    expected=actual=expected_diff=
     expected="foo\nbar\nzap\nthis\nis\nsoo\nvery\nvery\nequal\ninsert\na\nline\n"
     actual="foo\nzap\nbar\nthis\nis\nsoo\nvery\nvery\nequal\ninsert\na\nanother\nline\n"
     expected_diff="\n\n@@ -1,6 +1,6 @@\n foo\n-bar\n zap\n+bar\n this\n is\n soo\n@@ -9,5 +9,6 @@\n equal\n insert\n a\n+another\n line\n"
