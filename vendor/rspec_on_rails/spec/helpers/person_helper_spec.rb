4496da82655076fe50961fe1b0471520f748f0e0
aslak_hellesoy <aslak_hellesoy@410327ef-2207-0410-a325-f78bbcb22a5a>
2006-10-26 23:04:40 +0000

    better example
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@985 410327ef-2207-0410-a325-f78bbcb22a5a

--- vendor/rspec_on_rails/spec/helpers/person_helper_spec.rb
+++ vendor/rspec_on_rails/spec/helpers/person_helper_spec.rb
@@ -8,6 +8,8 @@ context "the PersonHelper" do
   end
   
   specify "tag helper" do
-    person_address_text_field.should_eql "<input id=\"person_address\" name=\"person[address]\" size=\"30\" type=\"text\" />"
+    @person = mock("person")
+    @person.stub!(:address).and_return("The moon")
+    person_address_text_field.should_eql "<input id=\"person_address\" name=\"person[address]\" size=\"30\" type=\"text\" value=\"The moon\" />"
   end
 end
