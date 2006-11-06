3f58b783320007ad0e687fb503234d031ac9cf0e
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2006-11-06 05:10:40 +0000

    website updates
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@1041 410327ef-2207-0410-a325-f78bbcb22a5a

--- lib/spec/version.rb
+++ lib/spec/version.rb
@@ -4,7 +4,7 @@ module Spec
       MAJOR  = 0
       MINOR  = 7
       TINY   = 0
-      # RANDOM_TOKEN: 0.846310547367564
+      # RANDOM_TOKEN: 0.382691615287267
       REV    = "$LastChangedRevision$".match(/LastChangedRevision: (\d+)/)[1]
 
       STRING = [MAJOR, MINOR, TINY].join('.')
--- vendor/rspec_on_rails/vendor/plugins/rspec/lib/spec/rails/version.rb
+++ vendor/rspec_on_rails/vendor/plugins/rspec/lib/spec/rails/version.rb
@@ -2,7 +2,7 @@ module Spec
   module Rails
     module VERSION
       unless defined?(REV)
-        # RANDOM_TOKEN: 0.846310547367564
+        # RANDOM_TOKEN: 0.382691615287267
         REV = "$LastChangedRevision$".match(/LastChangedRevision: (\d+)/)[1]
       end
     end
