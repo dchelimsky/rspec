b2b2959a8f7325f12093153f69645b3a845c5a93
aslak_hellesoy <aslak_hellesoy@410327ef-2207-0410-a325-f78bbcb22a5a>
2006-11-10 01:40:20 +0000

    Words
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@1077 410327ef-2207-0410-a325-f78bbcb22a5a

--- lib/spec/version.rb
+++ lib/spec/version.rb
@@ -4,7 +4,7 @@ module Spec
       MAJOR  = 0
       MINOR  = 7
       TINY   = 1
-      # RANDOM_TOKEN: 0.843887139977282
+      # RANDOM_TOKEN: 0.704313229332974
       REV    = "$LastChangedRevision$".match(/LastChangedRevision: (\d+)/)[1]
 
       STRING = [MAJOR, MINOR, TINY].join('.')
--- vendor/rspec_on_rails/vendor/plugins/rspec/lib/spec/rails/version.rb
+++ vendor/rspec_on_rails/vendor/plugins/rspec/lib/spec/rails/version.rb
@@ -2,7 +2,7 @@ module Spec
   module Rails
     module VERSION
       unless defined?(REV)
-        # RANDOM_TOKEN: 0.843887139977282
+        # RANDOM_TOKEN: 0.704313229332974
         REV = "$LastChangedRevision$".match(/LastChangedRevision: (\d+)/)[1]
       end
     end
