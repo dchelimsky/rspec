2f8807f8c95bf04813326622082e65af1671f54d
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2006-11-13 17:53:19 +0000

    Fixed [#6650] Reserved characters in the TextMate bundle break svn on Win32
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@1103 410327ef-2207-0410-a325-f78bbcb22a5a

--- vendor/rspec_on_rails/vendor/plugins/rspec/lib/spec/rails/version.rb
+++ vendor/rspec_on_rails/vendor/plugins/rspec/lib/spec/rails/version.rb
@@ -2,7 +2,7 @@ module Spec
   module Rails
     module VERSION
       unless defined?(REV)
-        # RANDOM_TOKEN: 0.265208200141975
+        # RANDOM_TOKEN: 0.702471609856632
         REV = "$LastChangedRevision$".match(/LastChangedRevision: (\d+)/)[1]
       end
     end
