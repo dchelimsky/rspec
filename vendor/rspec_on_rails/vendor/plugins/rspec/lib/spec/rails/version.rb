05623e26bece1526760687e54e6c5bd29b89fbef
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2006-11-17 03:08:20 +0000

    added docs for lambda{}.should_change
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@1117 410327ef-2207-0410-a325-f78bbcb22a5a

--- vendor/rspec_on_rails/vendor/plugins/rspec/lib/spec/rails/version.rb
+++ vendor/rspec_on_rails/vendor/plugins/rspec/lib/spec/rails/version.rb
@@ -2,7 +2,7 @@ module Spec
   module Rails
     module VERSION
       unless defined?(REV)
-        # RANDOM_TOKEN: 0.159528511819015
+        # RANDOM_TOKEN: 0.528706284886416
         REV = "$LastChangedRevision$".match(/LastChangedRevision: (\d+)/)[1]
       end
     end
