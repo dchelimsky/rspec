f3ff453e3d1a6532e43b17b293f7d3b4da1db724
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2006-11-19 22:46:41 +0000

    Added [#6731] Partial templates [should be] rendered [in view specs]
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@1124 410327ef-2207-0410-a325-f78bbcb22a5a

--- vendor/rspec_on_rails/vendor/plugins/rspec/spec/view_spec_spec.rb
+++ vendor/rspec_on_rails/vendor/plugins/rspec/spec/view_spec_spec.rb
@@ -14,12 +14,25 @@ context "Given a view requiring an explicit helper", :context_type => :view do
   end
 end
 
-
 context "Given a view requiring multiple explicit helpers", :context_type => :view do
   specify "the helper should be included if specified" do
     render "view_spec/multiple_helpers", :helpers => ['explicit', 'more_explicit']
     response.should_have_tag 'div', :content => "This is text from a method in the ExplicitHelper"
     response.should_have_tag 'div', :content => "This is text from a method in the MoreExplicitHelper"
+  end
+end
+
+context "Given a view that includes a partial", :context_type => :view do
+  setup do
+    render "view_spec/partial_including_template"
+  end
+  
+  specify "the enclosing template should get rendered" do
+    response.should_have_tag 'div', :content => "method_in_included_partial in ViewSpecHelper"
+  end
+  
+  specify "the partial should get rendered" do
+    response.should_have_tag 'div', :content => "method_in_partial_including_template in ViewSpecHelper"
   end
 end
 
