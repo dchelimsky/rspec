086c77eb8f4abedef3320f0597e6d485c01e9cc3
aslak_hellesoy <aslak_hellesoy@410327ef-2207-0410-a325-f78bbcb22a5a>
2006-11-23 00:25:50 +0000

    more jruby work
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@1129 410327ef-2207-0410-a325-f78bbcb22a5a

--- vendor/jruby/jruby_install.sh
+++ vendor/jruby/jruby_install.sh
@@ -1,8 +1,16 @@
+#!/bin/bash
 cd `dirname $0`
-svn co svn://svn.codehaus.org/jruby/branches/enebo jruby
-cd jruby
+if [ -d jruby_code ]
+then
+  echo "Checking out jruby"
+  svn co svn://svn.codehaus.org/jruby/trunk jruby_code
+else
+  echo "Updating out jruby"
+  svn up
+fi
+cd jruby_code/jruby
 ant jar
 export JRUBY_HOME=`pwd`
 export PATH=$PATH:$JRUBY_HOME/bin
-$JRUBY_HOME/bin/gem install diff-lcs
-$JRUBY_HOME/bin/jruby -I../../../lib ../../../bin/spec ../../../spec
+$JRUBY_HOME/bin/gem install diff-lcs --no-rdoc --no-ri
+$JRUBY_HOME/bin/jruby -I../../../../lib ../../../../bin/spec ../../../../spec
