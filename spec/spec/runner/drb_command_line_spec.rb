685991a9885174758ad956e40f00a61b9741d44d
aslak_hellesoy <aslak_hellesoy@410327ef-2207-0410-a325-f78bbcb22a5a>
2006-11-28 20:36:39 +0000

    guard picky specs from jruby
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@1168 410327ef-2207-0410-a325-f78bbcb22a5a

--- spec/spec/runner/drb_command_line_spec.rb
+++ spec/spec/runner/drb_command_line_spec.rb
@@ -1,25 +1,28 @@
 require 'stringio'
+require 'rbconfig'
 
 context "CommandLine" do
   
-  specify "should print error when there is no running local server" do
-    err = StringIO.new
-    out = StringIO.new
-    Spec::Runner::DrbCommandLine.run(['--version'], err, out)
+  unless Config::CONFIG['ruby_install_name'] == 'jruby'
+    specify "should print error when there is no running local server" do
+      err = StringIO.new
+      out = StringIO.new
+      Spec::Runner::DrbCommandLine.run(['--version'], err, out)
     
-    err.rewind
-    err.read.should =~ /No server is running/
-  end
+      err.rewind
+      err.read.should =~ /No server is running/
+    end
 
-  specify "should run against local server" do
-    DRb.start_service("druby://localhost:8989", Spec::Runner::CommandLine)
+    specify "should run against local server" do
+      DRb.start_service("druby://localhost:8989", Spec::Runner::CommandLine)
     
-    err = StringIO.new
-    out = StringIO.new
-    Spec::Runner::DrbCommandLine.run(['--version'], err, out, true)
+      err = StringIO.new
+      out = StringIO.new
+      Spec::Runner::DrbCommandLine.run(['--version'], err, out, true)
     
-    out.rewind
-    out.read.should =~ /RSpec/n
+      out.rewind
+      out.read.should =~ /RSpec/n
+    end
   end
 
 end
--- vendor/rspec_on_rails/vendor/plugins/rspec/lib/spec/rails/version.rb
+++ vendor/rspec_on_rails/vendor/plugins/rspec/lib/spec/rails/version.rb
@@ -2,7 +2,7 @@ module Spec
   module Rails
     module VERSION
       unless defined?(REV)
-        # RANDOM_TOKEN: 0.559853074557286
+        # RANDOM_TOKEN: 0.90643297839256
         REV = "$LastChangedRevision$".match(/LastChangedRevision: (\d+)/)[1]
       end
     end
