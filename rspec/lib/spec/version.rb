203210ffede801a3e8d556a5d386f8425e74d600
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2006-12-04 23:31:13 +0000

    testing the subversion revision incrementation
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@1252 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec/lib/spec/version.rb
+++ rspec/lib/spec/version.rb
@@ -4,7 +4,7 @@ module Spec
       MAJOR  = 0
       MINOR  = 7
       TINY   = 5
-      # RANDOM_TOKEN: 0.75683033426556
+      # RANDOM_TOKEN: 0.465490066846623
       REV    = "$LastChangedRevision$".match(/LastChangedRevision: (\d+)/)[1]
 
       STRING = [MAJOR, MINOR, TINY].join('.')
--- rspec_on_rails/plugin/lib/spec/rails/version.rb
+++ rspec_on_rails/plugin/lib/spec/rails/version.rb
@@ -2,8 +2,8 @@ module Spec
   module Rails
     module VERSION
       unless defined?(REV)
-        # RANDOM_TOKEN: 0.75683033426556
-        REV = "$LastChangedRevision: 1250 $".match(/LastChangedRevision: (\d+)/)[1]
+        # RANDOM_TOKEN: 0.465490066846623
+        REV = "$LastChangedRevision$".match(/LastChangedRevision: (\d+)/)[1]
       end
     end
   end
