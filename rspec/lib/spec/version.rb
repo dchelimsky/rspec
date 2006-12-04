4dee000cfd18d6c0db0ed6863271fb2b477db315
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2006-12-04 18:05:02 +0000

    mid move - don't expect things to work
    git-svn-id: http://rspec.rubyforge.org/svn/newtrunk@1223 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec/lib/spec/version.rb
+++ rspec/lib/spec/version.rb
@@ -4,7 +4,7 @@ module Spec
       MAJOR  = 0
       MINOR  = 7
       TINY   = 5
-      # RANDOM_TOKEN: 0.97071755614196
+      # RANDOM_TOKEN: 0.66260940230323
       REV    = "$LastChangedRevision$".match(/LastChangedRevision: (\d+)/)[1]
 
       STRING = [MAJOR, MINOR, TINY].join('.')
