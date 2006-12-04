0afaf873d422f50ad1906abe52dcd3a61c511556
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2006-12-04 15:46:19 +0000

    removed the rspec_on_rails specs' controllers, views and helpers from the demo app
    git-svn-id: http://rspec.rubyforge.org/svn/newtrunk@1218 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec_on_rails_demo/app/controllers/render_spec_controller.rb
+++
@@ -1,8 +0,0 @@
-class RenderSpecController < ApplicationController
-  def some_action
-  end
-  
-  def text_action
-    render :text=>"this the text for this action"
-  end
-end
--- rspec_on_rails_demo/app/helpers/explicit_helper.rb
+++
@@ -1,5 +0,0 @@
-module ExplicitHelper
-  def method_in_explicit_helper
-    "<div>This is text from a method in the ExplicitHelper</div>"
-  end
-end
