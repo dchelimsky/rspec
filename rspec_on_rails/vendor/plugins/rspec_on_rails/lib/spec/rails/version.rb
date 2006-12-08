1d571c83f9bb32d9819d8f8acbc2cf4f8c1de47c
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2006-12-08 14:53:17 +0000

    removed init code from vendor/plugins/rspec_on_rails/init.rb this keeps rspec from stepping on test/unit
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@1272 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec_on_rails/vendor/plugins/rspec_on_rails/lib/spec/rails/version.rb
+++ rspec_on_rails/vendor/plugins/rspec_on_rails/lib/spec/rails/version.rb
@@ -2,7 +2,7 @@ module Spec
   module Rails
     module VERSION
       unless defined?(REV)
-        # RANDOM_TOKEN: 0.637806981921119
+        # RANDOM_TOKEN: 0.26520027485203
         REV = "$LastChangedRevision$".match(/LastChangedRevision: (\d+)/)[1]
       end
     end
