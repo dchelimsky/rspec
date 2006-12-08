a9ff19461eed020f5d2679e38ad34f00af9924a9
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2006-12-08 15:43:06 +0000

    righting the wrongs of the last commit (committed from below root dir)
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@1274 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec/lib/spec/version.rb
+++ rspec/lib/spec/version.rb
@@ -4,7 +4,7 @@ module Spec
       MAJOR  = 0
       MINOR  = 7
       TINY   = 5
-      # RANDOM_TOKEN: 0.26520027485203
+      # RANDOM_TOKEN: 0.954613313118617
       REV    = "$LastChangedRevision$".match(/LastChangedRevision: (\d+)/)[1]
 
       STRING = [MAJOR, MINOR, TINY].join('.')
