66959e1c8a66610d3925ba27aab36103bcf247ce
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2006-12-08 21:56:59 +0000

    preliminary port of assert_select for 1.1.6 ONLY
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@1276 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec_on_rails/vendor/plugins/rspec_on_rails/lib/spec/rails/extensions/action_controller/test_response.rb
+++ rspec_on_rails/vendor/plugins/rspec_on_rails/lib/spec/rails/extensions/action_controller/test_response.rb
@@ -2,25 +2,45 @@ module ActionController
   class TestResponse
     
     module InstanceMethodsForRSpec
-      def should_have_rjs element, *args
+      def should_have_rjs(element, *args, &block)
         __response_body.should_have_rjs element, *args
       end
 
-      def should_not_have_rjs element, *args
+      def should_not_have_rjs(element, *args)
         __response_body.should_not_have_rjs element, *args
       end
 
-      def should_have_tag tag, *opts
+      def should_have_tag(tag, *opts)
         __response_body.should_have_tag tag, *opts
       end
 
-      def should_not_have_tag tag, *opts
+      def should_not_have_tag(tag, *opts)
         __response_body.should_not_have_tag tag, *opts
+      end
+      
+      def should_have(*args, &block)
+        __assert_select_wrapper.should_have(*args, &block)
+      end
+      
+      def should_have_feed(type, version=nil, &block)
+        __assert_select_wrapper.should_have_feed(type, version, &block)
+      end
+      
+      def should_have_encoded(element=nil, &block)
+        __assert_select_wrapper.should_have_encoded(element, &block)
+      end
+      
+      def should_have_email(&block)
+        __assert_select_wrapper.should_have_email(&block)
       end
       
       private
       def __response_body
         Spec::Rails::ResponseBody.new(self.body)
+      end
+      
+      def __assert_select_wrapper
+        @__assert_select_wrapper ||= Spec::Rails::AssertSelectWrapper.new(self)
       end
     end
     
