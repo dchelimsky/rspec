f61371a7e5d2f5241cb4ef0201dd7ed8fe20b2f5
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2006-12-12 14:41:45 +0000

    clean up some unnecessaries in rspec_on_rails
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@1283 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec/lib/spec/version.rb
+++ rspec/lib/spec/version.rb
@@ -4,7 +4,7 @@ module Spec
       MAJOR  = 0
       MINOR  = 7
       TINY   = 5
-      # RANDOM_TOKEN: 0.42852593012127
+      # RANDOM_TOKEN: 0.136691990678378
       REV    = "$LastChangedRevision$".match(/LastChangedRevision: (\d+)/)[1]
 
       STRING = [MAJOR, MINOR, TINY].join('.')
