eaa66f543ac825dc3318ce27776e6ea46d399952
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2006-12-15 17:23:06 +0000

    Applied [#7270] RSpec render_partial colliding with simply_helpful
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@1290 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec_on_rails/vendor/plugins/rspec_on_rails/spec/view_spec_spec.rb
+++ rspec_on_rails/vendor/plugins/rspec_on_rails/spec/view_spec_spec.rb
@@ -74,6 +74,39 @@ context "Given a view that includes a partial using :collection and :spacer_temp
 
 end
 
+context "Given a view that includes a partial using an array as partial_path", :context_type => :view do
+  setup do
+    module ActionView::Partials
+      def render_partial_with_array_support(partial_path, local_assigns = nil, deprecated_local_assigns = nil)
+        if partial_path.is_a?(Array)
+          "Array Partial"
+        else
+          render_partial_without_array_support(partial_path, local_assigns, deprecated_local_assigns)
+        end
+      end
+
+      alias :render_partial_without_array_support :render_partial
+      alias :render_partial :render_partial_with_array_support
+    end
+
+    @array = ['Alice', 'Bob']
+    assigns[:array] = @array
+  end
+
+  teardown do
+    module ActionView::Partials
+      alias :render_partial_with_array_support :render_partial
+      alias :render_partial :render_partial_without_array_support
+      undef render_partial_with_array_support
+    end
+  end
+
+  specify "the array should be passed through to render_partial without modification" do
+    render "view_spec/partial_with_array" 
+    response.body.should_match /^Array Partial$/
+  end
+end
+
 context "Different types of renders (not :template)", :context_type => :view do
   specify "partial with local" do
     render :partial => "view_spec/partial_with_local_variable", :locals => {:x => "Ender"}
