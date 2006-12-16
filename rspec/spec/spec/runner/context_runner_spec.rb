f5f2bdd30bcb191f79285d23fa78f9ca2a7e54dd
aslak_hellesoy <aslak_hellesoy@410327ef-2207-0410-a325-f78bbcb22a5a>
2006-12-16 23:56:36 +0000

    Applied [#7282] dump results even if spec is interruptted. IMPORTANT: revision 1282 (david) introduced a regression with inccorect absolute paths in backtraces. I have changed it to my paths just to get a successful build, but this is a bug that has to be fixed. The same thing happened a couple of weeks ago - I fixed it, but now it's back. I'll try to fix it again and add some security to avoid it sneaking back in a third time.
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@1294 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec/spec/spec/runner/context_runner_spec.rb
+++ rspec/spec/spec/runner/context_runner_spec.rb
@@ -1,58 +1,67 @@
 require File.dirname(__FILE__) + '/../../spec_helper.rb'
 
-module Spec
-module Runner
 context "ContextRunner" do
-    specify "should call run on context" do
-        context1=context2=reporter=
-        context1=Spec::Mocks::Mock.new("context1", {
-          :null_object => true
-        })
-        context2=Spec::Mocks::Mock.new("context2", {
-          :null_object => true
-        })
-        context1.should_receive(:run)
-        context1.should_receive(:number_of_specs).and_return(0)
-        context2.should_receive(:run)
-        context2.should_receive(:number_of_specs).and_return(0)
-        reporter=Spec::Mocks::Mock.new("reporter")
-        reporter.should_receive(:start).with(0)
-        reporter.should_receive(:end)
-        reporter.should_receive(:dump)
-        runner=ContextRunner.new(reporter, false)
-        runner.add_context(context1)
-        runner.add_context(context2)
-        runner.run(false)
-        context1.__verify
-        context2.__verify
-        reporter.__verify
-      
+  specify "should call run on context" do
+    context1 = mock("context1", {
+     :null_object => true
+    })
+    context2 = mock("context2", {
+     :null_object => true
+    })
+    context1.should_receive(:run)
+    context1.should_receive(:number_of_specs).and_return(0)
+    context2.should_receive(:run)
+    context2.should_receive(:number_of_specs).and_return(0)
+    reporter = mock("reporter")
+    reporter.should_receive(:start).with(0)
+    reporter.should_receive(:end)
+    reporter.should_receive(:dump)
+    runner = Spec::Runner::ContextRunner.new(reporter, false)
+    runner.add_context(context1)
+    runner.add_context(context2)
+    runner.run(false)
+  end
+
+  specify "should support single spec" do
+    desired_context = mock("desired context")
+    desired_context.should_receive(:matches?).at_least(:once).and_return(true)
+    desired_context.should_receive(:run)
+    desired_context.should_receive(:run_single_spec)
+    desired_context.should_receive(:number_of_specs).and_return(1)
+    other_context = mock("other context")
+    other_context.should_receive(:matches?).and_return(false)
+    other_context.should_receive(:run).never
+    other_context.should_receive(:number_of_specs).never
+    reporter = mock("reporter")
+    runner = Spec::Runner::ContextRunner.new(reporter, false, "desired context legal spec")
+    runner.add_context(desired_context)
+    runner.add_context(other_context)
+    reporter.should_receive(:start)
+    reporter.should_receive(:end)
+    reporter.should_receive(:dump)
+    runner.run(false)
+  end
+
+  specify "should dump even if Interrupt exception is occurred" do
+    context = Spec::Runner::Context.new("context") do
+      specify "no error" do
+      end
+
+      specify "should interrupt" do
+        raise Interrupt
+      end
     end
-    specify "should support single spec" do
-        desired_context=other_context=reporter=
-        desired_context=Spec::Mocks::Mock.new("desired context")
-        desired_context.should_receive(:matches?).at_least(:once).and_return(true)
-        desired_context.should_receive(:run)
-        desired_context.should_receive(:run_single_spec)
-        desired_context.should_receive(:number_of_specs).and_return(1)
-        other_context=Spec::Mocks::Mock.new("other context")
-        other_context.should_receive(:matches?).and_return(false)
-        other_context.should_receive(:run).never
-        other_context.should_receive(:number_of_specs).never
-        reporter=Spec::Mocks::Mock.new("reporter")
-        runner=ContextRunner.new(reporter, false, "desired context legal spec")
-        runner.add_context(desired_context)
-        runner.add_context(other_context)
-        reporter.should_receive(:start)
-        reporter.should_receive(:end)
-        reporter.should_receive(:dump)
-        runner.run(false)
-        desired_context.__verify
-        other_context.__verify
-        reporter.__verify
-      
-    end
-  
-end
-end
+
+    reporter = mock("reporter")
+    runner = Spec::Runner::ContextRunner.new(reporter, false)
+    runner.add_context(context)
+    reporter.should_receive(:start)
+    reporter.should_receive(:add_context).with("context")
+    reporter.should_receive(:spec_started).with("no error")
+    reporter.should_receive(:spec_finished)
+    reporter.should_receive(:spec_started).with("should interrupt")
+    reporter.should_receive(:end)
+    reporter.should_receive(:dump)
+    runner.run(false)
+  end
 end
