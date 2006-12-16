95d7710d702da10e7ad7becb533685245f2d4bca
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2006-12-16 15:58:48 +0000

    mysql config
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@1293 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec_on_rails/vendor/plugins/rspec_on_rails/lib/spec/rails/version.rb
+++ rspec_on_rails/vendor/plugins/rspec_on_rails/lib/spec/rails/version.rb
@@ -2,7 +2,7 @@ module Spec
   module Rails
     module VERSION
       unless defined?(REV)
-        # RANDOM_TOKEN: 0.0628775961337711
+        # RANDOM_TOKEN: 0.353391842331133
         REV = "$LastChangedRevision$".match(/LastChangedRevision: (\d+)/)[1]
       end
     end
