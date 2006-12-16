ca0146e224b02b89f0b2474b34db786c0a216ac7
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2006-12-16 15:58:48 +0000

    mysql config
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@1293 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec/lib/spec/version.rb
+++ rspec/lib/spec/version.rb
@@ -4,7 +4,7 @@ module Spec
       MAJOR  = 0
       MINOR  = 7
       TINY   = 5
-      # RANDOM_TOKEN: 0.0628775961337711
+      # RANDOM_TOKEN: 0.353391842331133
       REV    = "$LastChangedRevision$".match(/LastChangedRevision: (\d+)/)[1]
 
       STRING = [MAJOR, MINOR, TINY].join('.')
