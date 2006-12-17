56712eb9f04a1bf73842356d2b676076e119372b
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2006-12-17 00:52:10 +0000

    tweaked backtrace_tweaker so it excludes anything w/ /\/lib\/spec\//
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@1298 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec_on_rails/vendor/plugins/rspec_on_rails/lib/spec/rails/version.rb
+++ rspec_on_rails/vendor/plugins/rspec_on_rails/lib/spec/rails/version.rb
@@ -2,7 +2,7 @@ module Spec
   module Rails
     module VERSION
       unless defined?(REV)
-        # RANDOM_TOKEN: 0.526602413137978
+        # RANDOM_TOKEN: 0.335301839982397
         REV = "$LastChangedRevision$".match(/LastChangedRevision: (\d+)/)[1]
       end
     end
