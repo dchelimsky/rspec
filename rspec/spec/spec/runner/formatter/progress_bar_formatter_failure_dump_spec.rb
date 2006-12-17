607fb1ffcf8c904f3866768ef66eff544741d5d7
aslak_hellesoy <aslak_hellesoy@410327ef-2207-0410-a325-f78bbcb22a5a>
2006-12-17 01:55:17 +0000

    Fixed path problems once and for all, removed old instance_exec references and obsolete specs (we can no longer easily identify setup and teardown in backtraces, but that's ok)
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@1310 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec/spec/spec/runner/formatter/progress_bar_formatter_failure_dump_spec.rb
+++ rspec/spec/spec/runner/formatter/progress_bar_formatter_failure_dump_spec.rb
@@ -12,31 +12,22 @@ module Spec
 
         specify "should end with line break" do
           error=Spec::Expectations::ExpectationNotMetError.new("message")
-          error.set_backtrace(["/a/b/c/d/e.rb:34:in `some_method'"])
+          set_backtrace(error)
           @reporter.spec_finished("spec", error, "spec")
           @reporter.dump
           @io.string.should_match(/\n\z/)
         end
 
-        specify "should include context and setup in backtrace if error in setup" do
-          error=RuntimeError.new
-          @reporter.spec_finished("spec", error, "setup")
-          @reporter.dump
-          @io.string.should_match(/RuntimeError in 'context spec'/)
-        end
-
         specify "should include context and spec name in backtrace if error in spec" do
-          error=RuntimeError.new
+          error=RuntimeError.new("message")
+          set_backtrace(error)
           @reporter.spec_finished("spec", error, "spec")
           @reporter.dump
           @io.string.should_match(/RuntimeError in 'context spec'/)
         end
 
-        specify "should include context in backtrace if error in teardown" do
-          error=RuntimeError.new
-          @reporter.spec_finished("spec", error, "teardown")
-          @reporter.dump
-          @io.string.should_match(/RuntimeError in 'context spec'/)
+        def set_backtrace(error)
+          error.set_backtrace(["/a/b/c/d/e.rb:34:in `whatever'"])
         end
 
       end
