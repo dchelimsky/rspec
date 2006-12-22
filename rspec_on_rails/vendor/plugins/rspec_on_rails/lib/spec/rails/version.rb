1a446a1687bf2bfe3818021b3485d854085360a5
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2006-12-22 17:12:29 +0000

    back to 100% (the last commit left things at 99.9)
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@1338 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec_on_rails/vendor/plugins/rspec_on_rails/lib/spec/rails/version.rb
+++ rspec_on_rails/vendor/plugins/rspec_on_rails/lib/spec/rails/version.rb
@@ -2,7 +2,7 @@ module Spec
   module Rails
     module VERSION
       unless defined?(REV)
-        # RANDOM_TOKEN: 0.832010877972632
+        # RANDOM_TOKEN: 0.0165488220724319
         REV = "$LastChangedRevision$".match(/LastChangedRevision: (\d+)/)[1]
       end
     end
