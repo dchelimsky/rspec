4648671800998799a71a0b82662e8dc4e373153a
aslak_hellesoy <aslak_hellesoy@410327ef-2207-0410-a325-f78bbcb22a5a>
2006-12-24 03:21:19 +0000

    Fixed [#7339] Turn off caching in HTML reports
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@1341 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec_on_rails/vendor/plugins/rspec_on_rails/lib/spec/rails/version.rb
+++ rspec_on_rails/vendor/plugins/rspec_on_rails/lib/spec/rails/version.rb
@@ -2,7 +2,7 @@ module Spec
   module Rails
     module VERSION
       unless defined?(REV)
-        # RANDOM_TOKEN: 0.688637187951317
+        # RANDOM_TOKEN: 0.31682228427813
         REV = "$LastChangedRevision$".match(/LastChangedRevision: (\d+)/)[1]
       end
     end
