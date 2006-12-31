72af67556fae7ea8d41aced0cafcfef46577cb82
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2006-12-31 04:10:10 +0000

    better messages for non-redirects on response.should_be_success
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@1351 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec_on_rails/vendor/plugins/rspec_on_rails/spec/expectations/should_be_success_spec.rb
+++ rspec_on_rails/vendor/plugins/rspec_on_rails/spec/expectations/should_be_success_spec.rb
@@ -16,7 +16,23 @@ require File.dirname(__FILE__) + '/../spec_helper'
       get 'action_with_redirect_to_somewhere'
       lambda {
         response.should_be_success
-      }.should_fail_with "response should be success but was redirect to http://test.host/redirect_spec/somewhere"
+      }.should_fail_with "response code should be success (200) but was redirect (302) to http://test.host/redirect_spec/somewhere"
+    end
+    
+    specify "should fail on 404" do
+      get 'action_with_no_redirect'
+      response.headers['Status'] = '404 Server Error'
+      lambda {
+        response.should_be_success
+      }.should_fail_with "response code should be success (200) but was (404)"
+    end
+    
+    specify "should fail if server error" do
+      get 'action_with_no_redirect'
+      response.headers['Status'] = '500 Server Error'
+      lambda {
+        response.should_be_success
+      }.should_fail_with "response code should be success (200) but was (500)"
     end
   end
 end
