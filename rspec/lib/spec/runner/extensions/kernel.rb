c1e77d0c10c7fee59a2ac5280870d699be8b265c
btakita <btakita@410327ef-2207-0410-a325-f78bbcb22a5a>
2007-01-07 22:39:32 +0000

    Multilined Kernel#context_runner.
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@1367 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec/lib/spec/runner/extensions/kernel.rb
+++ rspec/lib/spec/runner/extensions/kernel.rb
@@ -1,15 +1,17 @@
-require 'spec/runner/extensions/object'
-
 module Kernel
   def context(name, &block)
     context = Spec::Runner::Context.new(name, &block)
     context_runner.add_context(context)
   end
-  
+
 private
-  
+
   def context_runner
-    if $context_runner.nil?; $context_runner = ::Spec::Runner::OptionParser.new.create_context_runner(ARGV.dup, STDERR, STDOUT, false); at_exit { $context_runner.run(false) }; end
+    # TODO: Figure out a better way to get this considered "covered" and keep this statement on multiple lines 
+    unless $context_runner; \
+      $context_runner = ::Spec::Runner::OptionParser.new.create_context_runner(ARGV.dup, STDERR, STDOUT, false); \
+      at_exit { $context_runner.run(false) }; \
+    end
     $context_runner
   end
 end
