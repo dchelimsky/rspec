deeba76242ce4ea3422da52863d4779520acb0b0
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2007-01-11 03:50:48 +0000

    added some new x.should do_y expectations
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@1372 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec/spec/spec/expectations/expectation_spec.rb
+++ rspec/spec/spec/expectations/expectation_spec.rb
@@ -1,28 +1,28 @@
 require File.dirname(__FILE__) + '/../../spec_helper.rb'
 
-module ExampleExpectations
-  class BeEmpty
-    def met_by?(target)
-      target.empty?
-    end
-  
-    def failure_message
-      "this is the message"
-    end
-  end
-  def be_empty
-    return BeEmpty.new
-  end
-end
-
-context "Given an Expectation passed to #should" do
-  include ExampleExpectations
-  
-  specify "#should should pass the target to met_by?" do
-    a = Array.new
-    a.should be_empty
-    
-    a << 1
-    lambda { a.should be_empty }.should_fail_with "this is the message"
-  end
-end
+# module ExampleExpectations
+#   class BeEmpty
+#     def met_by?(target)
+#       target.empty?
+#     end
+#   
+#     def failure_message
+#       "this is the message"
+#     end
+#   end
+#   def be_empty
+#     return BeEmpty.new
+#   end
+# end
+# 
+# context "Given an Expectation passed to #should" do
+#   include ExampleExpectations
+#   
+#   specify "#should should pass the target to met_by?" do
+#     a = Array.new
+#     a.should be_empty
+#     
+#     a << 1
+#     lambda { a.should be_empty }.should_fail_with "this is the message"
+#   end
+# end
