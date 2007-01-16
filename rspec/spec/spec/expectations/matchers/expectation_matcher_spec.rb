ea0351272d7975de7dd9f87cd0f48610f3126522
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2007-01-16 03:46:58 +0000

    more work on assert_select
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@1381 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec/spec/spec/expectations/matchers/expectation_matcher_spec.rb
+++ rspec/spec/spec/expectations/matchers/expectation_matcher_spec.rb
@@ -2,6 +2,34 @@ require File.dirname(__FILE__) + '/../../../spec_helper.rb'
 
 module ExampleExpectations
   
+  class MatcherThatAcceptsArgsAndBlock
+    def initialize(*args,&block)
+      if args.last.is_a?Hash
+        @expected = args.last[:expected]
+      elsif block_given?
+        @expected = block.call
+      end
+      @block = block
+    end
+    
+    def met_by?(target)
+      @target = target
+      return @expected == target
+    end
+    
+    def failure_message
+      "expected #{@expected}, got #{@target}"
+    end
+    
+    def negative_failure_message
+      "expected not #{@expected}, got #{@target}"
+    end
+  end
+  
+  def matcher_that_accepts_args_and_block(*args, &block)
+    MatcherThatAcceptsArgsAndBlock.new(*args, &block)
+  end
+  
   def passing_expectation
     Class.new do
       def met_by?(target)
@@ -38,8 +66,22 @@ context "ExpectationHandler behaviour" do
     @target.should passing_expectation
   end
 
+  specify "should handle submitted args" do
+    5.should matcher_that_accepts_args_and_block(:expected => 5)
+    lambda { 5.should matcher_that_accepts_args_and_block(:expected => 4) }.should_fail_with "expected 4, got 5"
+    5.should_not matcher_that_accepts_args_and_block(:expected => 4)
+    lambda { 5.should_not matcher_that_accepts_args_and_block(:expected => 5) }.should_fail_with "expected not 5, got 5"
+  end
+
+  specify "should handle the submitted block" do
+    5.should matcher_that_accepts_args_and_block { 5 }
+    lambda { 5.should matcher_that_accepts_args_and_block { 4 } }.should_fail_with "expected 4, got 5"
+    5.should_not matcher_that_accepts_args_and_block { 4 }
+    lambda { 5.should_not matcher_that_accepts_args_and_block { 5 } }.should_fail_with "expected not 5, got 5"
+  end
+
   specify "should fail if met_by? returns false" do
-    lambda {@target.should failing_expectation}.should_fail_with "expectation failed"
+    lambda { @target.should failing_expectation }.should_fail_with "expectation failed"
   end
 
   specify "should pass negative expectation if met_by? returns false" do
@@ -47,7 +89,7 @@ context "ExpectationHandler behaviour" do
   end
 
   specify "should fail negative expectation if met_by? returns true" do
-    lambda {@target.should_not passing_expectation}.should_fail_with "negative expectation failed"
+    lambda { @target.should_not passing_expectation }.should_fail_with "negative expectation failed"
   end
 
 end
