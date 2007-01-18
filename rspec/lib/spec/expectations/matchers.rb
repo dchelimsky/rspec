6e8093ac81dd5daf39c981c0769b4406e6d4ec73
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2007-01-18 06:01:22 +0000

    renamed the matcher files to match the matchers within
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@1389 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec/lib/spec/expectations/matchers.rb
+++ rspec/lib/spec/expectations/matchers.rb
@@ -1,39 +1,50 @@
-require 'spec/expectations/matchers/collections'
-require 'spec/expectations/matchers/equality'
-require 'spec/expectations/matchers/numeric'
-require 'spec/expectations/matchers/include'
 require 'spec/expectations/matchers/be'
+require 'spec/expectations/matchers/be_close'
+require 'spec/expectations/matchers/eql'
+require 'spec/expectations/matchers/equal'
+require 'spec/expectations/matchers/have'
+require 'spec/expectations/matchers/include'
 
 module Spec
   module Expectations
-    module MatcherMethods
+    module Matchers
       
-      #passes if receiver.equal?(expected)
-      def equal(other)
-        Matchers::Equal.new(other)
+      #passes if actual.equal?(expected)
+      def equal(expected)
+        Matchers::Equal.new(expected)
       end
       
-      #passes if receiver.eql?(expected)
-      def eql(other)
-        Matchers::Eql.new(other)
+      #passes if actual.eql?(expected)
+      def eql(expected)
+        Matchers::Eql.new(expected)
       end
       
+      #passes if actual == expected +/- delta
       def be_close(expected, delta)
         Matchers::BeClose.new(expected, delta)
       end
 
+      #actual.should have(n).items
+      #
+      #passes if actual owns a collection named 'items'
+      #which contains n elements
       def have(n)
         Matchers::Have.new(n)
       end
+      alias :have_exactly :have
     
-      def have_exactly(n)
-        Matchers::Have.new(n)
-      end
-    
+      #actual.should have_at_least(n).items
+      #
+      #passes if actual owns a collection named 'items'
+      #which contains at least n elements
       def have_at_least(n)
         Matchers::Have.new(n, :at_least)
       end
     
+      #actual.should have_at_most(n).items
+      #
+      #passes if actual owns a collection named 'items'
+      #which contains at most n elements
       def have_at_most(n)
         Matchers::Have.new(n, :at_most)
       end
@@ -49,7 +60,7 @@ module Spec
     end
     
     class Matcher
-      include MatcherMethods
+      include Matchers
     end
   end      
 end
