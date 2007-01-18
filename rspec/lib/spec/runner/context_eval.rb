edb828673027324ff4abd50e5ddaaf6b555d90cb
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2007-01-18 06:01:22 +0000

    renamed the matcher files to match the matchers within
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@1389 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec/lib/spec/runner/context_eval.rb
+++ rspec/lib/spec/runner/context_eval.rb
@@ -88,7 +88,7 @@ module Spec
         end
 
         def context_modules
-          @context_modules ||= [Spec::Expectations::MatcherMethods]
+          @context_modules ||= [Spec::Expectations::Matchers]
         end
         
         def specifications
