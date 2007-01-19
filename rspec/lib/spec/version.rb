8dd86ba5f276f45e0af55150144773d3f6a4ed96
aslak_hellesoy <aslak_hellesoy@410327ef-2207-0410-a325-f78bbcb22a5a>
2007-01-19 09:48:42 +0000

    More TextMate docs
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@1402 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec/lib/spec/version.rb
+++ rspec/lib/spec/version.rb
@@ -4,7 +4,7 @@ module Spec
       MAJOR  = 0
       MINOR  = 8
       TINY   = 0
-      # RANDOM_TOKEN: 0.801897923699004
+      # RANDOM_TOKEN: 0.379966397289924
       REV    = "$LastChangedRevision$".match(/LastChangedRevision: (\d+)/)[1]
 
       STRING = [MAJOR, MINOR, TINY].join('.')
