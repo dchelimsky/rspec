f7b19f1dc769cbd0c175afe11c639eb7355f14c5
aslak_hellesoy <aslak_hellesoy@410327ef-2207-0410-a325-f78bbcb22a5a>
2007-01-19 09:48:42 +0000

    More TextMate docs
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@1402 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec_on_rails/vendor/plugins/rspec_on_rails/lib/spec/rails/version.rb
+++ rspec_on_rails/vendor/plugins/rspec_on_rails/lib/spec/rails/version.rb
@@ -2,7 +2,7 @@ module Spec
   module Rails
     module VERSION
       unless defined?(REV)
-        # RANDOM_TOKEN: 0.801897923699004
+        # RANDOM_TOKEN: 0.379966397289924
         REV = "$LastChangedRevision$".match(/LastChangedRevision: (\d+)/)[1]
       end
     end
