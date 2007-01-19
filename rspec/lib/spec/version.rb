383d3b4b8a11c3137bf2be403d425a0168b3e363
aslak_hellesoy <aslak_hellesoy@410327ef-2207-0410-a325-f78bbcb22a5a>
2007-01-19 17:17:57 +0000

    Applied [#8008] Correct generated specs for view when calling resource generator
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@1408 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec/lib/spec/version.rb
+++ rspec/lib/spec/version.rb
@@ -4,7 +4,7 @@ module Spec
       MAJOR  = 0
       MINOR  = 8
       TINY   = 0
-      # RANDOM_TOKEN: 0.509591111870693
+      # RANDOM_TOKEN: 0.770483567747271
       REV    = "$LastChangedRevision$".match(/LastChangedRevision: (\d+)/)[1]
 
       STRING = [MAJOR, MINOR, TINY].join('.')
--- rspec_on_rails/vendor/plugins/rspec_on_rails/lib/spec/rails/version.rb
+++ rspec_on_rails/vendor/plugins/rspec_on_rails/lib/spec/rails/version.rb
@@ -2,7 +2,7 @@ module Spec
   module Rails
     module VERSION
       unless defined?(REV)
-        # RANDOM_TOKEN: 0.509591111870693
+        # RANDOM_TOKEN: 0.770483567747271
         REV = "$LastChangedRevision$".match(/LastChangedRevision: (\d+)/)[1]
       end
     end
