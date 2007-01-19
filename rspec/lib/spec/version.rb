fa96be36fb9b44b78f8f39aeb0ee81993d8f732c
aslak_hellesoy <aslak_hellesoy@410327ef-2207-0410-a325-f78bbcb22a5a>
2007-01-19 09:30:00 +0000

    Fixed [#7826] RSpect.tmbundle web page out of date
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@1400 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec/lib/spec/version.rb
+++ rspec/lib/spec/version.rb
@@ -4,7 +4,7 @@ module Spec
       MAJOR  = 0
       MINOR  = 8
       TINY   = 0
-      # RANDOM_TOKEN: 0.607710681564313
+      # RANDOM_TOKEN: 0.404198734956238
       REV    = "$LastChangedRevision$".match(/LastChangedRevision: (\d+)/)[1]
 
       STRING = [MAJOR, MINOR, TINY].join('.')
--- rspec_on_rails/vendor/plugins/rspec_on_rails/lib/spec/rails/version.rb
+++ rspec_on_rails/vendor/plugins/rspec_on_rails/lib/spec/rails/version.rb
@@ -2,7 +2,7 @@ module Spec
   module Rails
     module VERSION
       unless defined?(REV)
-        # RANDOM_TOKEN: 0.607710681564313
+        # RANDOM_TOKEN: 0.404198734956238
         REV = "$LastChangedRevision$".match(/LastChangedRevision: (\d+)/)[1]
       end
     end
