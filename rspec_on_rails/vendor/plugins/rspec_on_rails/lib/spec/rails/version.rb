a78c10a1b954064d5260a4b81f28618eab87fc4f
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2007-01-20 21:21:54 +0000

    more assert_select_matcher: response.should be_rjs response.should be_feed response.should send_email
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@1411 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec_on_rails/vendor/plugins/rspec_on_rails/lib/spec/rails/version.rb
+++ rspec_on_rails/vendor/plugins/rspec_on_rails/lib/spec/rails/version.rb
@@ -2,7 +2,7 @@ module Spec
   module Rails
     module VERSION
       unless defined?(REV)
-        # RANDOM_TOKEN: 0.824915441656601
+        # RANDOM_TOKEN: 0.134917716348762
         REV = "$LastChangedRevision$".match(/LastChangedRevision: (\d+)/)[1]
       end
     end
