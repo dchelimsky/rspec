c0f0b34bec029bbdaf10daaeb9ea362ac33aa081
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2007-01-21 21:35:26 +0000

    enhanced be(:sym) to take additional args - be(:sym, *args)
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@1419 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec/spec/spec/expectations/matchers/be_spec.rb
+++ rspec/spec/spec/expectations/matchers/be_spec.rb
@@ -1,31 +1,31 @@
 require File.dirname(__FILE__) + '/../../../spec_helper.rb'
 
 context "be(:sym)" do
-  specify "should pass when target returns true for :sym?" do
-    target = mock("target")
-    target.should_receive(:happy?).and_return(true)
-    be(:happy).matches?(target).should equal(true)
+  specify "should pass when actual returns true for :sym?" do
+    actual = mock("actual")
+    actual.should_receive(:happy?).and_return(true)
+    be(:happy).matches?(actual).should equal(true)
   end
 
-  specify "should fail when target returns false for :sym?" do
-    target = mock("target")
-    target.should_receive(:happy?).and_return(false)
-    be(:happy).matches?(target).should equal(false)
+  specify "should fail when actual returns false for :sym?" do
+    actual = mock("actual")
+    actual.should_receive(:happy?).and_return(false)
+    be(:happy).matches?(actual).should equal(false)
   end
   
-  specify "should fail when target does not respond to :sym?" do
-    target = Object.new
-    be(:happy).matches?(target).should equal(false)
+  specify "should fail when actual does not respond to :sym?" do
+    actual = Object.new
+    be(:happy).matches?(actual).should equal(false)
   end
   
   specify "should provide failure_message for :sym?" do
     #given
-    target = mock("target")
-    target.should_receive(:happy?).and_return(false)
+    actual = mock("actual")
+    actual.should_receive(:happy?).and_return(false)
     matcher = be(:happy)
 
     #when
-    matcher.matches?(target)
+    matcher.matches?(actual)
     
     #then
     matcher.failure_message.should == "expected actual.happy? to return true, got false"
@@ -33,24 +33,24 @@ context "be(:sym)" do
   
   specify "should provide negative_failure_message for :sym?" do
     #given
-    target = mock("target")
-    target.should_receive(:happy?).and_return(true)
+    actual = mock("actual")
+    actual.should_receive(:happy?).and_return(true)
     matcher = be(:happy)
 
     #when
-    matcher.matches?(target)
+    matcher.matches?(actual)
     
     #then
     matcher.negative_failure_message.should == "expected actual.happy? to return false, got true"
   end
 
-  specify "should explain when target does not respond to :sym?" do
+  specify "should explain when actual does not respond to :sym?" do
     #given
-    target = Object.new
+    actual = Object.new
     matcher = be(:happy)
 
     #when
-    matcher.matches?(target)
+    matcher.matches?(actual)
     
     #then
     matcher.failure_message.should == "actual does not respond to #happy?"
@@ -58,12 +58,85 @@ context "be(:sym)" do
   end
 end
 
+context "be(:sym, *args)" do
+  specify "should pass when actual returns true for :sym?(*args)" do
+    actual = mock("actual")
+    actual.should_receive(:older_than?).with(3).and_return(true)
+    be(:older_than, 3).matches?(actual).should equal(true)
+  end
+
+  specify "should fail when actual returns false for :sym?(*args)" do
+    actual = mock("actual")
+    actual.should_receive(:older_than?).with(3).and_return(false)
+    be(:older_than, 3).matches?(actual).should equal(false)
+  end
+  
+  specify "should fail when actual does not respond to :sym?" do
+    actual = Object.new
+    be(:happy).matches?(actual).should equal(false)
+  end
+  
+  specify "should provide failure_message for :sym?(*args) with one arg" do
+    #given
+    actual = mock("actual")
+    actual.should_receive(:older_than?).with(3).and_return(false)
+    matcher = be(:older_than, 3)
+  
+    #when
+    matcher.matches?(actual)
+    
+    #then
+    matcher.failure_message.should == "expected actual.older_than?(3) to return true, got false"
+  end
+  
+  specify "should provide failure_message for :sym?(*args) with multi args" do
+    #given
+    actual = mock("actual")
+    actual.should_receive(:ok_with?).with(3, "a", obj = Object.new).and_return(false)
+    matcher = be(:ok_with, 3, "a", obj)
+  
+    #when
+    matcher.matches?(actual)
+    
+    #then
+    matcher.failure_message.should =~ /expected actual.ok_with\?\(3, \"a\", #<Object:.*>\) to return true, got false/
+  end
+  
+  specify "should provide negative_failure_message for :sym?(*args) with one arg" do
+    #given
+    actual = mock("actual")
+    actual.should_receive(:older_than?).with(3).and_return(true)
+    matcher = be(:older_than, 3)
+  
+    #when
+    matcher.matches?(actual)
+    
+    #then
+    matcher.negative_failure_message.should == "expected actual.older_than?(3) to return false, got true"
+  end
+  
+  specify "should provide failure_message for :sym?(*args) with multi args" do
+    #given
+    actual = mock("actual")
+    actual.should_receive(:ok_with?).with(3, "a", obj = Object.new).and_return(false)
+    matcher = be(:ok_with, 3, "a", obj)
+  
+    #when
+    matcher.matches?(actual)
+    
+    #then
+    matcher.negative_failure_message.should =~ /expected actual.ok_with\?\(3, \"a\", #<Object:.*>\) to return false, got true/
+  end
+  
+
+end
+
 context "be(true)" do
-  specify "should pass when target equal(true)" do
+  specify "should pass when actual equal(true)" do
     be(true).matches?(true).should equal(true)
   end
 
-  specify "should fail when target equal(false)" do
+  specify "should fail when actual equal(false)" do
     be(true).matches?(false).should equal(false)
   end
   
@@ -85,11 +158,11 @@ context "be(true)" do
 end
 
 context "be(false)" do
-  specify "should pass when target equal(false)" do
+  specify "should pass when actual equal(false)" do
     be(false).matches?(false).should equal(true)
   end
 
-  specify "should fail when target equal(true)" do
+  specify "should fail when actual equal(true)" do
     be(false).matches?(true).should equal(false)
   end
   
@@ -111,11 +184,11 @@ context "be(false)" do
 end
 
 context "be(nil)" do
-  specify "should pass when target is nil" do
+  specify "should pass when actual is nil" do
     be(nil).matches?(nil).should equal(true)
   end
 
-  specify "should fail when target is not nil" do
+  specify "should fail when actual is not nil" do
     be(false).matches?(true).should equal(false)
   end
   
