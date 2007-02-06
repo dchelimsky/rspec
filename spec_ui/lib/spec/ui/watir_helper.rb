11285ac0c75010bd83d27b8054f3db353af2f01e
aslak_hellesoy <aslak_hellesoy@410327ef-2207-0410-a325-f78bbcb22a5a>
2007-02-06 17:05:55 +0000

    Less magic, matchers for Watir
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@1466 410327ef-2207-0410-a325-f78bbcb22a5a

--- spec_ui/lib/spec/ui/watir_helper.rb
+++ spec_ui/lib/spec/ui/watir_helper.rb
@@ -35,7 +35,57 @@ module Spec
       end
       
       def failure_message
-        "Expected browser to have HTML matching #{@text_or_regexp}, but it was not found in:\n#{@browser.text}"
+        "Expected browser to have HTML matching #{@text_or_regexp}, but it was not found in:\n#{@browser.html}"
+      end
+    end
+
+    class HaveLink
+      def initialize(how, what)
+        @how, @what = how, what
+      end
+      
+      def matches?(browser)
+        @browser = browser
+        begin
+          link = @browser.link(@how, @what)
+          link.assert_exists
+          true
+        rescue ::Watir::Exception::UnknownObjectException => e
+          false
+        end
+      end
+      
+      def failure_message
+        "Expected browser to have link(#{@how}, #{@what}), but it was not found"
+      end
+
+      def negative_failure_message
+        "Expected browser not to have link(#{@how}, #{@what}), but it was found"
+      end
+    end
+
+    class HaveTextField
+      def initialize(how, what)
+        @how, @what = how, what
+      end
+      
+      def matches?(browser)
+        @browser = browser
+        begin
+          text_field = @browser.text_field(@how, @what)
+          text_field.assert_exists
+          true
+        rescue ::Watir::Exception::UnknownObjectException => e
+          false
+        end
+      end
+      
+      def failure_message
+        "Expected browser to have text_field(#{@how}, #{@what}), but it was not found"
+      end
+
+      def negative_failure_message
+        "Expected browser not to have text_field(#{@how}, #{@what}), but it was found"
       end
     end
   end
@@ -43,4 +93,12 @@ end
 
 def have_html(text)
   Spec::Watir::HaveHtml.new(text)
+end
+
+def have_link(how, what)
+  Spec::Watir::HaveLink.new(how, what)
+end
+
+def have_text_field(how, what)
+  Spec::Watir::HaveTextField.new(how, what)
 end
