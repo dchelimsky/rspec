2318142e87cf4671b84c54fef8e141c819e82885
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2007-02-22 03:34:02 +0000

    added name generation for Spec::Rails expectations
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@1521 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec_on_rails/vendor/plugins/rspec_on_rails/lib/spec/rails/version.rb
+++ rspec_on_rails/vendor/plugins/rspec_on_rails/lib/spec/rails/version.rb
@@ -2,7 +2,7 @@ module Spec
   module Rails
     module VERSION #:nodoc:
       unless defined?(REV)
-        # RANDOM_TOKEN: 0.182762669458567
+        # RANDOM_TOKEN: 0.0788532396303099
         REV = "$LastChangedRevision$".match(/LastChangedRevision: (\d+)/)[1]
       end
     end
