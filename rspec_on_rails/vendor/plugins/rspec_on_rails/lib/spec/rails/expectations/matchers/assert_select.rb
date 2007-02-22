57d3ac6217fe419504c92f2419d7f5f83cbc7401
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2007-02-22 03:34:02 +0000

    added name generation for Spec::Rails expectations
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@1521 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec_on_rails/vendor/plugins/rspec_on_rails/lib/spec/rails/expectations/matchers/assert_select.rb
+++ rspec_on_rails/vendor/plugins/rspec_on_rails/lib/spec/rails/expectations/matchers/assert_select.rb
@@ -483,6 +483,315 @@ module Spec # :nodoc:
 
           end
       
+          # :call-seq:
+          #   response.should have_tag(selector, equality?, message?)
+          #   response.should have_tag(element, selector, equality?, message?)
+          #   response.should_not have_tag(selector, equality?, message?)
+          #   response.should_not have_tag(element, selector, equality?, message?)
+          #
+          # RSpec wrapper for <tt>assert_select</tt>
+          #
+          # Used to expect specific content in the response to an http request.
+          #
+          #   response.should have_tag("div", "Go Bears!")
+          #
+          # An expectation that selects elements and makes one or more equality comparisons.
+          #
+          # If the first argument is an element, selects all matching elements
+          # starting from (and including) that element and all its children in
+          # depth-first order.
+          #
+          # If no element if specified, calling #have_tag will select from the
+          # response HTML. Calling #have_tag inside an #have_tag block will
+          # run the assertion for each element selected by the enclosing assertion.
+          #
+          # For example:
+          #   response.should have_tag("ol>li") do |elements|
+          #     elements.each do |element|
+          #       element.should have_tag("li")
+          #     end
+          #   end
+          # Or for short:
+          #   response.should have_tag("ol>li") do
+          #     with_tag("li")
+          #   end
+          # The selector may be a CSS selector expression (+String+), an expression
+          # with substitution values, or an HTML::Selector object.
+          #
+          # === Equality
+          #
+          # The equality test may be one of the following:
+          # * <tt>true</tt> -- passes if at least one element selected.
+          # * <tt>false</tt> -- passes if no element selected.
+          # * <tt>String/Regexp</tt> -- passes if the text value of at least
+          #   one element matches the string or regular expression.
+          # * <tt>Integer</tt> -- passes if exactly that number of
+          #   elements are selected.
+          # * <tt>Range</tt> -- passes if the number of selected
+          #   elements fit the range.
+          # If no equality test specified, the expecatation passes if at least one
+          # element selected.
+          #
+          # To set more than one equality expectation, use a hash with the following keys:
+          # * <tt>:text</tt> -- Narrow the selection to elements that have this text
+          #   value (string or regexp).
+          # * <tt>:html</tt> -- Narrow the selection to elements that have this HTML
+          #   content (string or regexp).
+          # * <tt>:count</tt> -- Assertion is true if the number of selected elements
+          #   is equal to this value.
+          # * <tt>:minimum</tt> -- Assertion is true if the number of selected
+          #   elements is at least this value.
+          # * <tt>:maximum</tt> -- Assertion is true if the number of selected
+          #   elements is at most this value.
+          #
+          # If the method is called with a block, once all expectations are
+          # evaluated the block is called with an array of all selected elements.
+          #
+          # === Examples
+          # 
+          #   # At least one form element
+          #   response.should have_tag("form")
+          #
+          #   # Form element includes four input fields
+          #   response.should have_tag("form input", 4)
+          #
+          #   # Page title is "Welcome"
+          #   response.should have_tag("title", "Welcome")
+          #
+          #   # Page title is "Welcome" and there is only one title element
+          #   response.should have_tag("title", {:count=>1, :text=>"Welcome"},
+          #       "Wrong title or more than one title element")
+          #
+          #   # Page contains no forms
+          #   response.should have_tag("form", false, "This page must contain no forms")
+          #
+          #   # Test the content and style
+          #   response.should have_tag("body div.header ul.menu")
+          #
+          #   # Use substitution values
+          #   response.should have_tag("ol>li#?", /item-\d+/)
+          #
+          #   # All input fields in the form have a name
+          #   response.should have_tag("form input") {
+          #     with_tag("[name=?]", /.+/)
+          #   }
+          def have_tag(*args, &block)
+            args.unshift(response)
+            AssertSelect.new(*args, &block)
+          end
+
+          # :call-seq:
+          #   response.should have_rjs(id?) { |elements| ... }
+          #   response.should have_rjs(statement, id?) { |elements| ... }
+          #   response.should have_rjs(:insert, position, id?) { |elements| ... }
+          #   response.should_not have_rjs(id?) { |elements| ... }
+          #   response.should_not have_rjs(statement, id?) { |elements| ... }
+          #   response.should_not have_rjs(:insert, position, id?) { |elements| ... }
+          #
+          # RSpec wrapper for <tt>assert_select_rjs</tt>
+          #
+          # === Narrowing down
+          #
+          # With no arguments, expects that one or more elements are updated or
+          # inserted by RJS statements.
+          #
+          # Use the +id+ argument to narrow down the expectation to only statements
+          # that update or insert an element with that identifier.
+          #
+          # Use the first argument to narrow down expectations to only statements
+          # of that type. Possible values are +:replace+, +:replace_html+ and
+          # +:insert_html+.
+          #
+          # Use the argument +:insert+ followed by an insertion position to narrow
+          # down the expectation to only statements that insert elements in that
+          # position. Possible values are +:top+, +:bottom+, +:before+ and +:after+.
+          #
+          # === Using blocks
+          #
+          # Without a block, response.should have_rjs merely expects that the response
+          # contains one or more RJS statements that replace or update content.
+          #
+          # With a block, response.should have_rjs also selects all elements used in
+          # these statements and passes them to the block. Nested expectations are
+          # supported.
+          #
+          # Calling response.should have_rjs with no arguments and using nested expectations
+          # expects that the HTML content is returned by one or more RJS statements.
+          # Using #with_tag directly makes the same assertion on the content,
+          # but without distinguishing whether the content is returned in an HTML
+          # or JavaScript.
+          #
+          # === Examples
+          #
+          #   # Updating the element foo.
+          #   response.should have_rjs(:update, "foo")
+          #
+          #   # Inserting into the element bar, top position.
+          #   response.should have_rjs(:insert, :top, "bar")
+          #
+          #   # Changing the element foo, with an image.
+          #   response.should have_rjs("foo") do
+          #     with_tag("img[src=/images/logo.gif"")
+          #   end
+          #
+          #   # RJS inserts or updates a list with four items.
+          #   response.should have_rjs do
+          #     with_tag("ol>li", 4)
+          #   end
+          #
+          #   # The same, but shorter.
+          #   response.should have_rjs("ol>li", 4)
+          #
+          #   # RJS effects
+          #   response.should have_rjs(:effect, :fade, "mydiv")
+          #   response.should have_rjs(:effect, :toggle_blind, "mydiv")
+          def have_rjs(*args, &block)
+            args.unshift(response)
+            case args.last
+            when Hash
+              args.last[:select_type] = "rjs"
+            else
+              args << {:select_type => "rjs"}
+            end
+            AssertSelect.new(*args, &block)
+          end
+
+          # :call-seq:
+          #   with_tag(selector, equality?, message?)
+          #
+          # RSpec wrapper for a nested <tt>assert_select</tt>
+          #
+          # This is used within a block to set expectations about specific
+          # tags nested within the element selected before the block.
+          #
+          # === Example
+          #  
+          #  # Defining a login form
+          #  response.should have_tag("form[action=/login]") do
+          #    with_tag("input[type=text][name=email]")
+          #    with_tag("input[type=password][name=password]")
+          #    with_tag("input[type=submit][value=Login]")
+          #  end
+          def with_tag(*args, &block)
+            AssertSelect.selected.should have_tag(*args, &block)
+          end
+
+          # :call-seq:
+          #   with_encoded(element?) { |elements| ... }
+          #
+          # RSpec wrapper for <tt>assert_select_encoded</tt>
+          #
+          # Extracts the content of an element, treats it as encoded HTML and runs
+          # nested assertion on it.
+          #
+          # This is intended to called within another expectation to operate on
+          # all currently selected elements.
+          #
+          # The content of each element is un-encoded, and wrapped in the root
+          # element +encoded+. It then calls the block with all un-encoded elements.
+          #
+          # === Example
+          #
+          #   response.should be_feed(:rss, 2.0) do
+          #     with_tag("channel>item>description") do
+          #       with_encoded do
+          #         with_tag("p")
+          #       end
+          #     end
+          #   end
+          def with_encoded(*args, &block)
+            case args.last
+            when Hash
+              args.last[:select_type] = "encoded"
+            else
+              args << {:select_type => "encoded"}
+            end
+            AssertSelect.selected.should have_tag(*args, &block)
+          end
+
+          # :call-seq:
+          #   without_tag(selector, equality?, message?)
+          #
+          # RSpec wrapper for a nested <tt>assert_select</tt>, allowing you
+          # to specify the absence of a nested tag.
+          #
+          # === Example
+          #  
+          #  # A list of users in which the logged in user can edit her own record
+          #   response.should have_tag("div#users") do
+          #     with_tag("div#user_1") do
+          #       with_tag("a")
+          #     end
+          #     with_tag("div#user_2") do
+          #       #user 2 is logged in and gets a link to edit record
+          #       without_tag("a[href=/users/2;edit]")
+          #     end
+          #     with_tag("div#user_3") do
+          #       with_tag("a")
+          #     end
+          #   end
+          def without_tag(*args, &block)
+            AssertSelect.selected.should_not have_tag(*args, &block)
+          end
+
+          # :call-seq:
+          #   response.should be_feed(type, version?) { ... }
+          #   response.should_not be_feed(type, version?) { ... }
+          #
+          # RSpec wrapper for a <tt>assert_select_feed</tt>
+          #
+          # Selects root of the feed element. Calls the block for nested expectations.
+          #
+          # The feed type may be <tt>:atom</tt> or <tt>:rss</tt>. Currently supported
+          # are versions 2.0 (default) and 0.92 for RSS and versions 0.3 and 1.0 (default)
+          # for Atom.
+          #
+          # === Example
+          #
+          #   response.should be_feed(:rss, 2.0) {
+          #     with_tag("title", "My feed")
+          #     with_tag("item") { |items|
+          #       . . .
+          #     }
+          #   }
+          def be_feed(*args, &block)
+            args.unshift(response)
+            case args.last
+            when Hash
+              args.last[:select_type] = "feed"
+            else
+              args << {:select_type => "feed"}
+            end
+            AssertSelect.new(*args, &block)
+          end
+
+          # :call-seq:
+          #   response.should send_email { }
+          #   response.should_not send_email { }
+          #
+          # RSpec wrapper for a <tt>assert_select_email</tt>
+          #
+          # Extracts the body of an email and runs nested expectations on it.
+          #
+          # You must enable deliveries for this to work, use:
+          #   ActionMailer::Base.perform_deliveries = true
+          #
+          # === Example
+          #
+          #   response.should send_mail {
+          #     with_tag("h1", "Email alert")
+          #   }
+          def send_email(*args, &block)
+            args.unshift(response)
+            case args.last
+            when Hash
+              args.last[:select_type] = "email"
+            else
+              args << {:select_type => "email"}
+            end
+            AssertSelect.new(*args, &block)
+          end
+
       end
     end
   end
--- rspec_on_rails/vendor/plugins/rspec_on_rails/lib/spec/rails/expectations/matchers/redirect_to.rb
+++ rspec_on_rails/vendor/plugins/rspec_on_rails/lib/spec/rails/expectations/matchers/redirect_to.rb
@@ -39,9 +39,33 @@ module Spec
               return %Q{expected redirect to #{@expected.inspect}, got no redirect}
             end
           end
+          
+          def to_s
+            "redirect to #{@actual.inspect}"
+          end
         end
 
+        # :call-seq:
+        #   response.should redirect_to(url)
+        #   response.should redirect_to(:action => action_name)
+        #   response.should redirect_to(:controller => controller_name, :action => action_name)
+        #   response.should_not redirect_to(url)
+        #   response.should_not redirect_to(:action => action_name)
+        #   response.should_not redirect_to(:controller => controller_name, :action => action_name)
+        #
+        # Passes if the response is a redirect to the url, action or controller/action.
+        # Useful in controller specs (integration or isolation mode).
+        #
+        # == Examples
+        #
+        #   response.should redirect_to("path/to/action")
+        #   response.should redirect_to("http://test.host/path/to/action")
+        #   response.should redirect_to(:action => 'list')
+        def redirect_to(opts)
+          RedirectTo.new(request, opts)
+        end
       end
+
     end
   end
 end
