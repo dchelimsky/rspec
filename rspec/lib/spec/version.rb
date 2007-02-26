d719320ef30ac167e757737cb8d8674b9ea0814a
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2007-02-26 23:19:20 +0000

    Applied [#8132] [PATCH] RSpec breaks "rake db:sessions:create" in a rails project that has the rspec_on_rails plugin (Patch from Erik Kastner)
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@1539 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec/lib/spec/version.rb
+++ rspec/lib/spec/version.rb
@@ -14,7 +14,7 @@ module Spec
       TINY   = 0
       RELEASE_CANDIDATE = "RC2"
       
-      # RANDOM_TOKEN: 0.901737520579898
+      # RANDOM_TOKEN: 0.582242818783688
       REV = "$LastChangedRevision$".match(/LastChangedRevision: (\d+)/)[1]
 
       STRING = [MAJOR, MINOR, TINY].join('.')
