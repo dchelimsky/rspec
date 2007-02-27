22aa172836a8d64b13cd25baaae3e1577f690835
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2007-02-27 22:47:57 +0000

    added spec for AR::Base extensions
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@1542 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec_on_rails/vendor/plugins/rspec_on_rails/lib/spec/rails/version.rb
+++ rspec_on_rails/vendor/plugins/rspec_on_rails/lib/spec/rails/version.rb
@@ -2,7 +2,7 @@ module Spec
   module Rails
     module VERSION #:nodoc:
       unless defined?(REV)
-        # RANDOM_TOKEN: 0.598704893979657
+        # RANDOM_TOKEN: 0.643232453756978
         REV = "$LastChangedRevision$".match(/LastChangedRevision: (\d+)/)[1]
       end
     end
