309906925f2703ba3bcbaf66b7bb7a3f45eef34c
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2007-03-02 14:50:20 +0000

    more subtle assert_select tweaks
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@1559 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec_on_rails/vendor/plugins/rspec_on_rails/lib/spec/rails/matchers/assert_select.rb
+++ rspec_on_rails/vendor/plugins/rspec_on_rails/lib/spec/rails/matchers/assert_select.rb
@@ -1,16 +1,10 @@
-# This is a port of assert_select for rspec.
-
-# assert_select plugins for Rails
-#
-# Copyright (c) 2006 Assaf Arkin, under Creative Commons Attribution and/or MIT License
-# Developed for http://co.mments.com
-# Code and documention: http://labnotes.org
+# This is a wrapper of assert_select for rspec.
 
 module Spec # :nodoc:
   module Rails
     module Matchers
       
-      class AssertSelect
+      class AssertSelect #:nodoc:
         def initialize(*args, &block)
           @message = args.shift
           @spec_scope = args.shift
@@ -29,66 +23,93 @@ module Spec # :nodoc:
           @error.message
         end
         def negative_failure_message
-          "negative_failure_message"
+          "should_not #{description}, but did"
         end
+        
         def description
-          "description"
+          {
+            :assert_select => "have_tag#{format_args(*@args)}",
+            :assert_select_email => "send_email#{format_args(*@args)}",
+            :assert_select => "have_tag#{format_args(*@args)}",
+          }[@message]
         end
+        
+        private
+          def format_args(*args)
+            return "" if args.empty?
+            return "(#{arg_list(*args)})"
+          end
+          def arg_list(*args)
+            args.collect do |arg|
+              arg.respond_to?(:description) ? arg.description : arg.inspect
+            end.join(", ")
+          end
       end
       
+      # :call-seq:
+      #   response.should have_tag(*args, &block)
+      #
       # wrapper for assert_select
-      # see documentation for assert_select
+      #
+      # see documentation for assert_select at http://api.rubyonrails.org/
       def have_tag(*args, &block)
         args.unshift(self)
         args.unshift(:assert_select)
         AssertSelect.new(*args, &block)
       end
     
-      # wrapper for assert_select
-      # syntactic sugar for nesting
+      # wrapper for a nested assert_select
       #
       #   response.should have_tag("div#1") do
       #     with_tag("span", "some text")
       #   end
       #
-      # see documentation for assert_select
+      # see documentation for assert_select at http://api.rubyonrails.org/
       def with_tag(*args, &block)
         response.should have_tag(*args, &block)
       end
     
-      # wrapper for assert_select with false
-      # syntactic sugar for nesting
+      # wrapper for a nested assert_select with false
       #
       #   response.should have_tag("div#1") do
-      #     without_tag("span", "some text")
+      #     without_tag("span", "some text that shouldn't be there")
       #   end
       #
-      # see documentation for assert_select
+      # see documentation for assert_select at http://api.rubyonrails.org/
       def without_tag(*args, &block)
         response.should_not have_tag(*args, &block)
       end
     
+      # :call-seq:
+      #   response.should have_rjs(*args, &block)
+      #
       # wrapper for assert_select_rjs
       #
-      # see documentation for assert_select_rjs
+      # see documentation for assert_select_rjs at http://api.rubyonrails.org/
       def have_rjs(*args, &block)
         args.unshift(self)
         args.unshift(:assert_select_rjs)
         AssertSelect.new(*args, &block)
       end
       
+      # :call-seq:
+      #   response.should be_feed(*args, &block)
+      #
       # wrapper for assert_select_feed
       #
-      # see documentation for assert_select_feed
+      # see documentation for assert_select_feed at http://api.rubyonrails.org/
       def be_feed(*args, &block)
         args.unshift(self)
         args.unshift(:assert_select_feed)
         AssertSelect.new(*args, &block)
       end
       
+      # :call-seq:
+      #   response.should send_email(*args, &block)
+      #
       # wrapper for assert_select_email
       #
-      # see documentation for assert_select_email
+      # see documentation for assert_select_email at http://api.rubyonrails.org/
       def send_email(*args, &block)
         args.unshift(self)
         args.unshift(:assert_select_email)
@@ -97,7 +118,7 @@ module Spec # :nodoc:
       
       # wrapper for assert_select_encoded
       #
-      # see documentation for assert_select_encoded
+      # see documentation for assert_select_encoded at http://api.rubyonrails.org/
       def with_encoded(*args, &block)
         args.unshift(self)
         args.unshift(:assert_select_encoded)
