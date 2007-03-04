5cc3abf1a572486537ed3d1c3dbdb136a7f1175e
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2007-03-04 14:48:38 +0000

    updated generated controller specs to 0.8
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@1564 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec_on_rails/vendor/plugins/rspec_on_rails/generators/rspec_controller/templates/view_spec.rb
+++ rspec_on_rails/vendor/plugins/rspec_on_rails/generators/rspec_controller/templates/view_spec.rb
@@ -6,6 +6,6 @@ context "Given a request to render <%= class_name.underscore %>/<%= action %>" d
   end
   
   specify "the response should tell you where to find the file" do
-    response.should have_tag('p', :content => 'Find me in app/views/<%= class_name.underscore %>/<%= action %>.rhtml')
+    response.should have_tag('p', 'Find me in app/views/<%= class_name.underscore %>/<%= action %>.rhtml')
   end
 end
--- rspec_on_rails/vendor/plugins/rspec_on_rails/lib/spec/rails/version.rb
+++ rspec_on_rails/vendor/plugins/rspec_on_rails/lib/spec/rails/version.rb
@@ -2,7 +2,7 @@ module Spec
   module Rails
     module VERSION #:nodoc:
       unless defined?(REV)
-        # RANDOM_TOKEN: 0.736270645693609
+        # RANDOM_TOKEN: 0.532301587260344
         REV = "$LastChangedRevision$".match(/LastChangedRevision: (\d+)/)[1]
       end
     end
