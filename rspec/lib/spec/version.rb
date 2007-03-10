c60ddccb433ade2515ca290dbcc31ea7333158c6
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2007-03-10 14:12:08 +0000

    Applied [#7637] [PATCH] add skip-migration option to rspec_resource generator
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@1583 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec/lib/spec/version.rb
+++ rspec/lib/spec/version.rb
@@ -14,7 +14,7 @@ module Spec
       TINY   = 3
       # RELEASE_CANDIDATE = "RC1"
       
-      # RANDOM_TOKEN: 0.565345121051479
+      # RANDOM_TOKEN: 0.525577319458746
       REV = "$LastChangedRevision$".match(/LastChangedRevision: (\d+)/)[1]
 
       STRING = [MAJOR, MINOR, TINY].join('.')
--- rspec_on_rails/vendor/plugins/rspec_on_rails/lib/spec/rails/version.rb
+++ rspec_on_rails/vendor/plugins/rspec_on_rails/lib/spec/rails/version.rb
@@ -2,7 +2,7 @@ module Spec
   module Rails
     module VERSION #:nodoc:
       unless defined?(REV)
-        # RANDOM_TOKEN: 0.565345121051479
+        # RANDOM_TOKEN: 0.525577319458746
         REV = "$LastChangedRevision$".match(/LastChangedRevision: (\d+)/)[1]
       end
     end
