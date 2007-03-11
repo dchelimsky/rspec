96d8903791e5ac597ea9ba6cc15d1763d976845f
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2007-03-11 23:01:17 +0000

    reworked some specs in light of describe/it - more to come
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@1588 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec_on_rails/vendor/plugins/rspec_on_rails/lib/spec/rails/version.rb
+++ rspec_on_rails/vendor/plugins/rspec_on_rails/lib/spec/rails/version.rb
@@ -2,7 +2,7 @@ module Spec
   module Rails
     module VERSION #:nodoc:
       unless defined?(REV)
-        # RANDOM_TOKEN: 0.237025888054271
+        # RANDOM_TOKEN: 0.260061528916506
         REV = "$LastChangedRevision$".match(/LastChangedRevision: (\d+)/)[1]
       end
     end
