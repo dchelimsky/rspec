4f64149ecca58a40881b30f7db4e278752134a9e
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2007-03-12 03:13:24 +0000

    reworking specs in light of describe/it
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@1589 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec/lib/spec/version.rb
+++ rspec/lib/spec/version.rb
@@ -14,7 +14,7 @@ module Spec
       TINY   = 3
       # RELEASE_CANDIDATE = "RC1"
       
-      # RANDOM_TOKEN: 0.260061528916506
+      # RANDOM_TOKEN: 0.367556322093444
       REV = "$LastChangedRevision$".match(/LastChangedRevision: (\d+)/)[1]
 
       STRING = [MAJOR, MINOR, TINY].join('.')
--- rspec/spec/spec/matchers/equal_spec.rb
+++ rspec/spec/spec/matchers/equal_spec.rb
@@ -1,41 +1,28 @@
 require File.dirname(__FILE__) + '/../../spec_helper.rb'
 
-describe "should equal(expected)" do
-  
-  it "should pass if target.equal?(expected)" do
-    1.should equal(1)
+module Spec
+  module Matchers
+    describe Equal do
+      it "should match when actual.equal?(expected)" do
+        Equal.new(1).matches?(1).should be_true
+      end
+      it "should not match when !actual.equal?(expected)" do
+        Equal.new("1").matches?("1").should be_false
+      end
+      it "should describe itself" do
+        matcher = Equal.new(1)
+        matcher.description.should == "equal 1"
+      end
+      it "should provide message, expected and actual on #failure_message" do
+        matcher = Equal.new("1")
+        matcher.matches?(1)
+        matcher.failure_message.should == ["expected \"1\", got 1 (using .equal?)", "1", 1]
+      end
+      it "should provide message, expected and actual on #negative_failure_message" do
+        matcher = Equal.new(1)
+        matcher.matches?(1)
+        matcher.negative_failure_message.should == ["expected 1 not to equal 1 (using .equal?)", 1, 1]
+      end
+    end
   end
-  
-  it "should fail unless target.equal?(expected)" do
-    lambda {
-      "1".should equal("1")
-    }.should fail
-  end
-  
-  it "should provide message, expected and actual on failure" do
-    matcher = equal("1")
-    matcher.matches?("1")
-    matcher.failure_message.should == ["expected \"1\", got \"1\" (using .equal?)", "1", "1"]
-  end
-  
-end
-
-describe "should_not equal(expected)" do
-  
-  it "should pass unless target.equal?(expected)" do
-    "1".should_not equal("1")
-  end
-  
-  it "should fail if target.equal?(expected)" do
-    lambda {
-      1.should_not equal(1)
-    }.should fail
-  end
-  
-  it "should provide message, expected and actual on failure" do
-    matcher = equal(1)
-    matcher.matches?(1)
-    matcher.negative_failure_message.should == ["expected 1 not to equal 1 (using .equal?)", 1, 1]
-  end
-  
 end
