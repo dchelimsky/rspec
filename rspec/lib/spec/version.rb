a5596fb3e44184765de17d39c813d08d4c76ecb6
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2007-03-24 18:41:07 +0000

    Added [#9522] support using rspec's expectations with test/unit
    git-svn-id: http://rspec.rubyforge.org/svn/branches/0.9-dev@1651 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec/lib/spec/version.rb
+++ rspec/lib/spec/version.rb
@@ -6,7 +6,7 @@ module Spec
       TINY   = 0
       RELEASE_CANDIDATE = nil #{}"RC1"
       
-      # RANDOM_TOKEN: 0.954060330844007
+      # RANDOM_TOKEN: 0.389051252292009
       REV = "$LastChangedRevision$".match(/LastChangedRevision: (\d+)/)[1]
 
       STRING = [MAJOR, MINOR, TINY, RELEASE_CANDIDATE].compact.join('.')
