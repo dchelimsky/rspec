587ae6c0f87d04ecfb2b567dd0c611964f8f0b69
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2007-03-29 10:42:17 +0000

    tweaked example
    git-svn-id: http://rspec.rubyforge.org/svn/branches/0.9-dev@1664 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec/examples/test_case_adapter_example.rb
+++ rspec/examples/test_case_adapter_example.rb
@@ -1,7 +1,6 @@
 #This is an example of using RSpec's expectations in test/unit.
 $LOAD_PATH.unshift File.join(File.dirname(__FILE__), "..", "lib")
 
-require 'rubygems'
 require 'test/unit'
 require 'spec/test_case_adapter'
 
