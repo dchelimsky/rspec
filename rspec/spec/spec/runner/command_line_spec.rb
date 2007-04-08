ccb443d029809bab0f65ae60c233912212c4ca5a
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2007-04-08 16:13:29 +0000

    updated command_line_spec.rb to accomodate new example in examples dir
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@1731 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec/spec/spec/runner/command_line_spec.rb
+++ rspec/spec/spec/runner/command_line_spec.rb
@@ -7,7 +7,7 @@ context "CommandLine" do
     Spec::Runner::CommandLine.run([file], err, out, false, true)
     
     out.rewind
-    out.read.should =~ /60 examples, 0 failures/n
+    out.read.should =~ /61 examples, 0 failures/n
   end
 
   specify "should run file" do
