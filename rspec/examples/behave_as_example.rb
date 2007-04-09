9f5fea22610b2a93216272142625ef6084bcc918
aslak_hellesoy <aslak_hellesoy@410327ef-2207-0410-a325-f78bbcb22a5a>
2007-04-09 12:15:07 +0000

    I turned the world of guitars on its head
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@1738 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec/examples/behave_as_example.rb
+++ rspec/examples/behave_as_example.rb
@@ -1,7 +1,7 @@
 require File.dirname(__FILE__) + '/spec_helper'
 
 def behave_as_guitarist
-  respond_to('read_notes', 'turn_down_amp')
+  respond_to(:read_notes, :turn_down_amp)
 end
 
 module BehaveAsExample
@@ -16,15 +16,26 @@ module BehaveAsExample
     def turn_down_amp; end
   end
   
+  class ClassicGuitarist
+    def read_notes; end
+  end
+  
   describe BluesGuitarist do
     it "should behave as guitarist" do
       BluesGuitarist.new.should behave_as_guitarist
     end
   end
-  
+
   describe RockGuitarist do
     it "should behave as guitarist" do
       RockGuitarist.new.should behave_as_guitarist
     end
   end
+
+  describe ClassicGuitarist do
+    it "should not behave as guitarist" do
+      ClassicGuitarist.new.should_not behave_as_guitarist
+    end
+  end
+  
 end
