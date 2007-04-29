ae634f8576b018cf0fc27842684b40771013c7f3
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2007-04-29 06:14:50 +0000

    better stack spec using shared behaviours
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@1827 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec/examples/stack_spec.rb
+++ rspec/examples/stack_spec.rb
@@ -37,7 +37,7 @@ describe "non-full Stack", :shared => true do
 end
 
 describe Stack, " (empty)" do
-  setup do
+  before(:each) do
     @stack = Stack.new
   end
   
@@ -45,11 +45,6 @@ describe Stack, " (empty)" do
   specify { @stack.should be_empty }
   
   it_should_behave_like "non-full Stack"
-  
-  it "should no longer be empty after #push" do
-    @stack.push "anything"
-    @stack.should_not be_empty
-  end
   
   it "should complain when sent #peek" do
     lambda { @stack.peek }.should raise_error(StackUnderflowError)
@@ -61,67 +56,39 @@ describe Stack, " (empty)" do
 end
 
 describe Stack, " (with one item)" do
-  setup do
+  before(:each) do
     @stack = Stack.new
     @stack.push 3
     @last_item_added = 3
   end
 
-  # NOTE that this one auto-generates the description "should not be full"
-  specify { @stack.should_not be_empty }
-  
   it_should_behave_like "non-empty Stack"
-  
-  it "should remain not empty after #peek" do
-    @stack.peek
-    @stack.should_not be_empty
-  end
-  
-  it "should become empty after #pop" do
-    @stack.pop
-    @stack.should be_empty
-  end
+  it_should_behave_like "non-full Stack"
+
 end
 
 describe Stack, " (with one item less than capacity)" do
-  setup do
+  before(:each) do
     @stack = Stack.new
     (1..9).each { |i| @stack.push i }
     @last_item_added = 9
   end
   
-  # NOTE that this one auto-generates the description "should not be full"
-  specify { @stack.should_not be_full }
-
   it_should_behave_like "non-empty Stack"
-  
-  it "should become full after #push" do
-    @stack.push Object.new
-    @stack.should be_full
-  end
+  it_should_behave_like "non-full Stack"
 end
 
 describe Stack, " (full)" do
-  setup do
+  before(:each) do
     @stack = Stack.new
     (1..10).each { |i| @stack.push i }
     @last_item_added = 10
   end
-  
+
   # NOTE that this one auto-generates the description "should be full"
-  it { @stack.should be_full }
-  
+  specify { @stack.should be_full }  
+
   it_should_behave_like "non-empty Stack"
-  
-  it "should remain full after #peek" do
-    @stack.peek
-    @stack.should be_full
-  end
-  
-  it "should no longer be full after #pop" do
-    @stack.pop
-    @stack.should_not be_full
-  end
 
   it "should complain on #push" do
     lambda { @stack.push Object.new }.should raise_error(StackOverflowError)
--- rspec/spec/spec/runner/command_line_spec.rb
+++ rspec/spec/spec/runner/command_line_spec.rb
@@ -7,7 +7,7 @@ context "CommandLine" do
     Spec::Runner::CommandLine.run([file], err, out, false, true)
     
     out.rewind
-    out.read.should =~ /77 examples, 0 failures/n
+    out.read.should =~ /73 examples, 0 failures/n
   end
 
   specify "should run file" do
