458054f7595b04458dfb57100ec72305673bbffd
aslak_hellesoy <aslak_hellesoy@410327ef-2207-0410-a325-f78bbcb22a5a>
2007-05-02 16:18:32 +0000

    Get the svn tag from version.rb
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@1888 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec_on_rails/lib/spec/rails/version.rb
+++ rspec_on_rails/lib/spec/rails/version.rb
@@ -2,7 +2,7 @@ module Spec
   module Rails
     module VERSION #:nodoc:
       unless defined?(REV)
-        # RANDOM_TOKEN: 0.293100926392895
+        # RANDOM_TOKEN: 0.160657349264112
         REV = "$LastChangedRevision$".match(/LastChangedRevision: (\d+)/)[1]
       end
     end
