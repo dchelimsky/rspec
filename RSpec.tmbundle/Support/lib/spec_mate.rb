2ced7e34836072c81de3366ea8bc3f54bab8e85e
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2007-05-15 05:40:47 +0000

    a little cleaner spec_mate (doesn't append to load path unless it finds the right directory)
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@1955 410327ef-2207-0410-a325-f78bbcb22a5a

--- RSpec.tmbundle/Support/lib/spec_mate.rb
+++ RSpec.tmbundle/Support/lib/spec_mate.rb
@@ -6,8 +6,8 @@ if File.directory?(rspec_rails_plugin)
   $LOAD_PATH.unshift(rspec_rails_plugin)
 elsif ENV['TM_RSPEC_HOME']
   rspec_lib = File.join(ENV['TM_RSPEC_HOME'], 'lib')
-  $LOAD_PATH.unshift(rspec_lib)
   raise "TM_RSPEC_HOME points to a bad location: #{ENV['TM_RSPEC_HOME']}" unless File.directory?(rspec_lib)
+  $LOAD_PATH.unshift(rspec_lib)
 end
 
 require 'spec'
