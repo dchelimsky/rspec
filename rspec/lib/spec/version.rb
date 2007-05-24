b3bdb011e8a53797cf8bf116d942683e0189d165
aslak_hellesoy <aslak_hellesoy@410327ef-2207-0410-a325-f78bbcb22a5a>
2007-05-24 22:02:41 +0000

    [#11101] StringHelpers.starts_with?(prefix) assumes a string parameter for _prefix_
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@2026 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec/lib/spec/version.rb
+++ rspec/lib/spec/version.rb
@@ -6,7 +6,7 @@ module Spec
       TINY   = 3
       RELEASE_CANDIDATE = nil
 
-      # RANDOM_TOKEN: 0.929740001443892
+      # RANDOM_TOKEN: 0.447669671185141
       REV = "$LastChangedRevision$".match(/LastChangedRevision: (\d+)/)[1]
 
       STRING = [MAJOR, MINOR, TINY].join('.')
--- rspec_on_rails/lib/spec/rails/version.rb
+++ rspec_on_rails/lib/spec/rails/version.rb
@@ -2,7 +2,7 @@ module Spec
   module Rails
     module VERSION #:nodoc:
       unless defined?(REV)
-        # RANDOM_TOKEN: 0.929740001443892
+        # RANDOM_TOKEN: 0.447669671185141
         REV = "$LastChangedRevision$".match(/LastChangedRevision: (\d+)/)[1]
       end
     end
