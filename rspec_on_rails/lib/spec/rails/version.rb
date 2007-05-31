e80ad1f105674a18ec698ea8f7e36e577b694c76
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2007-05-31 11:40:27 +0000

    Fixed [#11247] standalone autotest doesn't work because of unneeded autotest.rb
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@2062 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec_on_rails/lib/spec/rails/version.rb
+++ rspec_on_rails/lib/spec/rails/version.rb
@@ -2,7 +2,7 @@ module Spec
   module Rails
     module VERSION #:nodoc:
       unless defined?(REV)
-        # RANDOM_TOKEN: 0.896772030928402
+        # RANDOM_TOKEN: 0.118995292163283
         REV = "$LastChangedRevision$".match(/LastChangedRevision: (\d+)/)[1]
       end
     end
