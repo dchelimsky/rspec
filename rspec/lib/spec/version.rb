e7334e783f95141e9b18119e8c3e1d6c6808ea0a
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2007-08-04 13:47:38 +0000

    Applied [#12701] Allow checking of content captured with content_for in view specs (Patch from Jens Kramer)
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@2242 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec/lib/spec/version.rb
+++ rspec/lib/spec/version.rb
@@ -6,7 +6,7 @@ module Spec
       TINY   = 6
       RELEASE_CANDIDATE = nil
 
-      # RANDOM_TOKEN: 0.797848257982499
+      # RANDOM_TOKEN: 0.566331746893558
       REV = "$LastChangedRevision$".match(/LastChangedRevision: (\d+)/)[1]
 
       STRING = [MAJOR, MINOR, TINY].join('.')
--- rspec_on_rails/lib/spec/rails/version.rb
+++ rspec_on_rails/lib/spec/rails/version.rb
@@ -2,7 +2,7 @@ module Spec
   module Rails
     module VERSION #:nodoc:
       unless defined?(REV)
-        # RANDOM_TOKEN: 0.797848257982499
+        # RANDOM_TOKEN: 0.566331746893558
         REV = "$LastChangedRevision$".match(/LastChangedRevision: (\d+)/)[1]
       end
     end
