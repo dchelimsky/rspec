385065e32660257d29b370fdc471a934d946f2ee
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2007-08-21 14:00:56 +0000

    silly bit of cleanup
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@2419 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec_on_rails/lib/spec/rails/dsl/behaviour.rb
+++ rspec_on_rails/lib/spec/rails/dsl/behaviour.rb
@@ -1,18 +1,18 @@
-dir = File.dirname(__FILE__)
+dir = File.expand_path(File.join(File.dirname(__FILE__), '/behaviour'))
 
-require File.expand_path("#{dir}/behaviour/render_observer")
-require File.expand_path("#{dir}/behaviour/rails_test_case")
+require "#{dir}/render_observer"
+require "#{dir}/rails_test_case"
 
-require File.expand_path("#{dir}/behaviour/rails_example_space")
-require File.expand_path("#{dir}/behaviour/functional_example_space")
-require File.expand_path("#{dir}/behaviour/controller_example_space")
-require File.expand_path("#{dir}/behaviour/helper_example_space")
-require File.expand_path("#{dir}/behaviour/view_example_space")
-require File.expand_path("#{dir}/behaviour/model_example_space")
+require "#{dir}/rails_example_space"
+require "#{dir}/functional_example_space"
+require "#{dir}/controller_example_space"
+require "#{dir}/helper_example_space"
+require "#{dir}/view_example_space"
+require "#{dir}/model_example_space"
 
-require File.expand_path("#{dir}/behaviour/rails_behaviour")
-require File.expand_path("#{dir}/behaviour/model_behaviour")
-require File.expand_path("#{dir}/behaviour/functional_behaviour")
-require File.expand_path("#{dir}/behaviour/controller_behaviour")
-require File.expand_path("#{dir}/behaviour/helper_behaviour")
-require File.expand_path("#{dir}/behaviour/view_behaviour")
+require "#{dir}/rails_behaviour"
+require "#{dir}/model_behaviour"
+require "#{dir}/functional_behaviour"
+require "#{dir}/controller_behaviour"
+require "#{dir}/helper_behaviour"
+require "#{dir}/view_behaviour"
--- rspec_on_rails/lib/spec/rails/version.rb
+++ rspec_on_rails/lib/spec/rails/version.rb
@@ -2,7 +2,7 @@ module Spec
   module Rails
     module VERSION #:nodoc:
       unless defined?(REV)
-        # RANDOM_TOKEN: 0.494940965741184
+        # RANDOM_TOKEN: 0.0235922336882256
         REV = "$LastChangedRevision$".match(/LastChangedRevision: (\d+)/)[1]
       end
     end
