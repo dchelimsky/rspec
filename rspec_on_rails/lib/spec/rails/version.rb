ecc372415f708bc2b5ea442c0d83c1b04b00a934
aslak_hellesoy <aslak_hellesoy@410327ef-2207-0410-a325-f78bbcb22a5a>
2007-08-28 18:53:24 +0000

    [#12722] the alternate file command does not work in rails views due to scope
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@2476 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec_on_rails/lib/spec/rails/version.rb
+++ rspec_on_rails/lib/spec/rails/version.rb
@@ -2,7 +2,7 @@ module Spec
   module Rails
     module VERSION #:nodoc:
       unless defined?(REV)
-        # RANDOM_TOKEN: 0.780366184815727
+        # RANDOM_TOKEN: 0.834988586208285
         REV = "$LastChangedRevision$".match(/LastChangedRevision: (\d+)/)[1]
       end
     end
