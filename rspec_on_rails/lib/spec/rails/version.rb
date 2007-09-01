267b0f56381d0793934f371301340750e3c87dec
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2007-09-01 13:52:36 +0000

    Applied [#11254] RSpec syntax coloring and function pop-up integration in TextMate (Patch from Wincent Colaiuta)
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@2487 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec_on_rails/lib/spec/rails/version.rb
+++ rspec_on_rails/lib/spec/rails/version.rb
@@ -2,7 +2,7 @@ module Spec
   module Rails
     module VERSION #:nodoc:
       unless defined?(REV)
-        # RANDOM_TOKEN: 0.653636599519691
+        # RANDOM_TOKEN: 0.369135635513989
         REV = "$LastChangedRevision$".match(/LastChangedRevision: (\d+)/)[1]
       end
     end
