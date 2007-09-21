85fb871e3cef6ecdff222c3fec4edb6da9e90c0e
btakita <btakita@410327ef-2207-0410-a325-f78bbcb22a5a>
2007-09-21 23:14:15 +0000

    Added spec for generated rspec_on_rails spec file. Fixed rspec_on_rails spec file and spec_server file. Still need to add a spec for spec_server file.
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@2613 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec_on_rails/lib/spec/rails/version.rb
+++ rspec_on_rails/lib/spec/rails/version.rb
@@ -2,7 +2,7 @@ module Spec
   module Rails
     module VERSION #:nodoc:
       unless defined?(REV)
-        # RANDOM_TOKEN: 0.358070357139242
+        # RANDOM_TOKEN: 0.598873246522744
         REV = "$LastChangedRevision$".match(/LastChangedRevision: (\d+)/)[1]
       end
     end
