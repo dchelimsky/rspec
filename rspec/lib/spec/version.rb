9b5939e6cb5d039138711edfe1dd32c07f1108ec
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2007-09-23 19:22:38 +0000

    Fixed [#14166] Cannot build trunk
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@2643 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec/lib/spec/version.rb
+++ rspec/lib/spec/version.rb
@@ -6,7 +6,7 @@
       TINY   = 9
       RELEASE_CANDIDATE = nil
 
-      # RANDOM_TOKEN: 0.746730251442059
+      # RANDOM_TOKEN: 0.84958706875657
       REV = "$LastChangedRevision$".match(/LastChangedRevision: (\d+)/)[1]
 
       STRING = [MAJOR, MINOR, TINY].join('.')
--- rspec_on_rails/lib/spec/rails/version.rb
+++ rspec_on_rails/lib/spec/rails/version.rb
@@ -2,7 +2,7 @@ module Spec
   module Rails
     module VERSION #:nodoc:
       unless defined?(REV)
-        # RANDOM_TOKEN: 0.746730251442059
+        # RANDOM_TOKEN: 0.84958706875657
         REV = "$LastChangedRevision$".match(/LastChangedRevision: (\d+)/)[1]
       end
     end
