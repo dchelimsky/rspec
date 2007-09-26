ecc69e9fa613241ed8240dd339e29f41f3e0b84f
btakita <btakita@410327ef-2207-0410-a325-f78bbcb22a5a>
2007-09-26 20:30:52 +0000

    Fixed spec & test for running by itself
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@2674 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec/spec/spec/runner/test_unit_and_rspec_spec.rb
+++ rspec/spec/spec/runner/test_unit_and_rspec_spec.rb
@@ -1,21 +1,26 @@
 require File.dirname(__FILE__) + '/../../spec_helper.rb'
 
+class TestUnitAndRspecTest < Test::Unit::TestCase
+  class << self
+    attr_accessor :tests_run
+  end
+
+  def test_should_run_tests
+    self.class.tests_run = true
+    assert true
+  end
+end
+
 class TestUnitAndRspecSpec < ::Spec::DSL::Example
   class << self
     attr_accessor :examples_run
   end
   
   it "should run tests and specs" do
-    self.class.examples_run = true
+    TestUnitAndRspecTest.tests_run.should be_true
   end
 
   def test_should_have_seamless_transition_from_test_unit
     assert true
-  end
-end
-
-class TestUnitAndRspecTest < Test::Unit::TestCase
-  def test_should_run_tests
-    assert TestUnitAndRspecSpec.examples_run
   end
 end
--- rspec/spec/spec_helper.rb
+++ rspec/spec/spec_helper.rb
@@ -49,11 +49,9 @@ class NonStandardError < Exception; end
 describe "Test::Unit io sink", :shared => true do
   before do
     @test_runner_io = StringIO.new
-#    Test::Unit::UI::Console::TestRunner.const_set(:STDOUT, @test_runner_io)
   end
 
   after do
-#    Test::Unit::UI::Console::TestRunner.__send__(:remove_const, :STDOUT)
   end
 end
 
