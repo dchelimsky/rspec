f629d33286924693ed2dc6e5f71e53d91d152b90
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2007-10-02 06:24:38 +0000

    Applied [#14362] partially mocking objects that define == can blow up (Patch from Pat Maddox)
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@2681 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec/lib/spec/version.rb
+++ rspec/lib/spec/version.rb
@@ -6,7 +6,7 @@
       TINY   = 9
       RELEASE_CANDIDATE = nil
 
-      # RANDOM_TOKEN: 0.0547346202055873
+      # RANDOM_TOKEN: 0.754610901424205
       REV = "$LastChangedRevision$".match(/LastChangedRevision: (\d+)/)[1]
 
       STRING = [MAJOR, MINOR, TINY].join('.')
--- rspec_on_rails/lib/spec/rails/version.rb
+++ rspec_on_rails/lib/spec/rails/version.rb
@@ -2,7 +2,7 @@ module Spec
   module Rails
     module VERSION #:nodoc:
       unless defined?(REV)
-        # RANDOM_TOKEN: 0.0547346202055873
+        # RANDOM_TOKEN: 0.754610901424205
         REV = "$LastChangedRevision$".match(/LastChangedRevision: (\d+)/)[1]
       end
     end
