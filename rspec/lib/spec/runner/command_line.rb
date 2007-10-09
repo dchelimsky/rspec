1ecba49518be2b893cea483584bfe3cf0ead07d7
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2007-10-09 04:22:16 +0000

    words
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@2709 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec/lib/spec/runner/command_line.rb
+++ rspec/lib/spec/runner/command_line.rb
@@ -8,6 +8,10 @@ module Spec
         # Runs specs. +argv+ is the commandline args as per the spec commandline API, +err+
         # and +out+ are the streams output will be written to.
         def run(instance_rspec_options)
+          # NOTE - this call to init_rspec_options is not spec'd, but neither is any of this
+          # swapping of $rspec_options. That is all here to enable rspec to run against itself
+          # and maintain coverage in a single process. Therefore, DO NOT mess with this stuff
+          # unless you know what you are doing!
           init_rspec_options(instance_rspec_options)
           orig_rspec_options = rspec_options
           begin
@@ -22,18 +26,6 @@ module Spec
             $rspec_options = orig_rspec_options
           end
         end
-
-        # def run(instance_rspec_options)
-        #   puts "running"
-        #   puts caller(0)[1]
-        #   $rspec_options = instance_rspec_options
-        #   return true if rspec_options.generate
-        # 
-        #   $rspec_options.load_paths
-        #   success = rspec_options.run_examples
-        #   heckle(rspec_options) if $rspec_options.heckle_runner
-        #   return success
-        # end
 
         protected
         def heckle(options)
