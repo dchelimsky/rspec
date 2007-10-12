e562046c7227d3fa30609bf1a5f00f5516aabbf2
btakita <btakita@410327ef-2207-0410-a325-f78bbcb22a5a>
2007-10-12 06:43:00 +0000

    Fixed clearing AR connections when in a multithreaded environment.
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@2717 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec/lib/spec/version.rb
+++ rspec/lib/spec/version.rb
@@ -6,7 +6,7 @@
       TINY   = 0
       RELEASE_CANDIDATE = nil
 
-      # RANDOM_TOKEN: 0.0358392655762985
+      # RANDOM_TOKEN: 0.66158247219848
       REV = "$LastChangedRevision$".match(/LastChangedRevision: (\d+)/)[1]
 
       STRING = [MAJOR, MINOR, TINY].join('.')
--- rspec_on_rails/lib/spec/rails/version.rb
+++ rspec_on_rails/lib/spec/rails/version.rb
@@ -2,7 +2,7 @@ module Spec
   module Rails
     module VERSION #:nodoc:
       unless defined?(REV)
-        # RANDOM_TOKEN: 0.0358392655762985
+        # RANDOM_TOKEN: 0.66158247219848
         REV = "$LastChangedRevision$".match(/LastChangedRevision: (\d+)/)[1]
       end
     end
