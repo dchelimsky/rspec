10365be0a05e34d5bec55e7699a674b8f2dac7cc
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2007-10-30 12:22:33 +0000

    resolve spec failing in jruby
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@2797 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec/spec/spec/test/unit/output_one_time_with_test_unit_spec.rb
+++ rspec/spec/spec/test/unit/output_one_time_with_test_unit_spec.rb
@@ -14,7 +14,7 @@ module Spec
         dir = File.dirname(__FILE__)
         output =`ruby #{dir}/output_one_time_with_test_unit_fixture_runner.rb`
         output.should_not include("Started")
-        output.should_not include("tests")
+        output.should_not match(/\btests\b/)
         output.should_not include("assertions")
         output.should_not include("0 tests, 0 assertions, 0 failures, 0 errors")
       end
