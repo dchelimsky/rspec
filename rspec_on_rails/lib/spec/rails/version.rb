381b955d565005b2625690778416fd763c4178c9
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2007-11-02 19:14:36 +0000

    story parser is more forgiving for narrative - only cares about 'Scenario:'
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@2808 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec_on_rails/lib/spec/rails/version.rb
+++ rspec_on_rails/lib/spec/rails/version.rb
@@ -2,7 +2,7 @@ module Spec
   module Rails
     module VERSION #:nodoc:
       unless defined?(REV)
-        # RANDOM_TOKEN: 0.420506700874148
+        # RANDOM_TOKEN: 0.970897547224795
         REV = "$LastChangedRevision$".match(/LastChangedRevision: (\d+)/)[1]
       end
     end
