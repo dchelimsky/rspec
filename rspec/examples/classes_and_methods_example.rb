9b474a8069f31a783f9ec56f99a1ed5c3668626d
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2007-11-04 19:47:45 +0000

    better example
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rspec/trunk@2814 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec/examples/classes_and_methods_example.rb
+++ rspec/examples/classes_and_methods_example.rb
@@ -4,12 +4,22 @@ require File.dirname(__FILE__) + "/stack"
 class EmptyStackSpec < Spec::ExampleGroup
   def should_be_empty
     stack = Stack.new
+    
+    # you can use an assertion ...
     assert stack.empty?
+    
+    # ... or an expectation
+    stack.should be_empty
   end
   
   def should_not_be_empty_after_push
     stack = Stack.new
     stack.push 'foo'
+
+    # you can use an assertion ...
     assert !stack.empty?
+    
+    # ... or an expectation
+    stack.should_not be_empty
   end
 end
