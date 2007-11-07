d648a7a0b5b93e2a540f1bdd01e1f565c9131912
aslak_hellesoy <aslak_hellesoy@410327ef-2207-0410-a325-f78bbcb22a5a>
2007-11-07 15:52:13 +0000

    renamed BehaviourFactory#create to BehaviourFacotry#create_behaviour_class so we know what's going on. Renamed variables and fixed lyin RDoc. Moved some examples to a more appropriate place.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rspec/trunk@2822 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec_on_rails/lib/spec/rails/version.rb
+++ rspec_on_rails/lib/spec/rails/version.rb
@@ -2,7 +2,7 @@ module Spec
   module Rails
     module VERSION #:nodoc:
       unless defined?(REV)
-        # RANDOM_TOKEN: 0.958898233957452
+        # RANDOM_TOKEN: 0.588796788792162
         REV = "$LastChangedRevision$".match(/LastChangedRevision: (\d+)/)[1]
       end
     end
