1f6311ce2e93b60ee36f6e230f763cf6cdc927a4
btakita <btakita@410327ef-2207-0410-a325-f78bbcb22a5a>
2007-11-09 10:11:39 +0000

    Move run and heckle logic from CommandLine.run to Options#run_examples.
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@2833 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec/lib/spec/version.rb
+++ rspec/lib/spec/version.rb
@@ -6,7 +6,7 @@ module Spec
       TINY   = 0
       RELEASE_CANDIDATE = nil
 
-      # RANDOM_TOKEN: 0.17107865654644
+      # RANDOM_TOKEN: 0.394947973346547
       REV = "$LastChangedRevision$".match(/LastChangedRevision: (\d+)/)[1]
 
       STRING = [MAJOR, MINOR, TINY].join('.')
--- rspec_on_rails/lib/spec/rails/version.rb
+++ rspec_on_rails/lib/spec/rails/version.rb
@@ -2,7 +2,7 @@ module Spec
   module Rails
     module VERSION #:nodoc:
       unless defined?(REV)
-        # RANDOM_TOKEN: 0.17107865654644
+        # RANDOM_TOKEN: 0.394947973346547
         REV = "$LastChangedRevision$".match(/LastChangedRevision: (\d+)/)[1]
       end
     end
