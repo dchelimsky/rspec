86a7045351452e3c31bd885a4b02d50891b3ca62
aslak_hellesoy <aslak_hellesoy@410327ef-2207-0410-a325-f78bbcb22a5a>
2007-11-10 17:14:01 +0000

    Moved code from Example's meta class to Behaviour module
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rspec/trunk@2841 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec/lib/spec/version.rb
+++ rspec/lib/spec/version.rb
@@ -6,7 +6,7 @@ module Spec
       TINY   = 0
       RELEASE_CANDIDATE = nil
 
-      # RANDOM_TOKEN: 0.366964154365593
+      # RANDOM_TOKEN: 0.762475101533329
       REV = "$LastChangedRevision$".match(/LastChangedRevision: (\d+)/)[1]
 
       STRING = [MAJOR, MINOR, TINY].join('.')
--- rspec_on_rails/lib/spec/rails/version.rb
+++ rspec_on_rails/lib/spec/rails/version.rb
@@ -2,7 +2,7 @@ module Spec
   module Rails
     module VERSION #:nodoc:
       unless defined?(REV)
-        # RANDOM_TOKEN: 0.366964154365593
+        # RANDOM_TOKEN: 0.762475101533329
         REV = "$LastChangedRevision$".match(/LastChangedRevision: (\d+)/)[1]
       end
     end
