2e22912b6f2ad803c2c85056b497c375ce8e590d
aslak_hellesoy <aslak_hellesoy@410327ef-2207-0410-a325-f78bbcb22a5a>
2007-11-10 21:38:02 +0000

    Moved pure examples and Test::Unit related examples to separate folders
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rspec/trunk@2846 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec_on_rails/lib/spec/rails/version.rb
+++ rspec_on_rails/lib/spec/rails/version.rb
@@ -2,7 +2,7 @@ module Spec
   module Rails
     module VERSION #:nodoc:
       unless defined?(REV)
-        # RANDOM_TOKEN: 0.5932388124648
+        # RANDOM_TOKEN: 0.61359325737024
         REV = "$LastChangedRevision$".match(/LastChangedRevision: (\d+)/)[1]
       end
     end
