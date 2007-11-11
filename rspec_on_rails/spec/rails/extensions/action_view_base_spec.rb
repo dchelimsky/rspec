d843af8134dbb7fccb78cc684d732027270655f9
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2007-11-11 23:39:46 +0000

    got one pending example passing
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rspec/trunk@2863 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec_on_rails/spec/rails/extensions/action_view_base_spec.rb
+++ rspec_on_rails/spec/rails/extensions/action_view_base_spec.rb
@@ -8,9 +8,10 @@ describe ActionView::Base, "with RSpec extensions", :behaviour_type => :view do
   end
   
   it "should raise when render has NOT been received" do
-    pending("need a new way to deal with examples that should_raise")
-  # , :should_raise => [Spec::Mocks::MockExpectationError]  do
-  #   template.expect_render(:partial => "name")
+    template.expect_render(:partial => "name")
+    lambda {
+      template.verify_rendered
+    }.should raise_error
   end
   
   it "should not raise when stubbing and render has been received" do
