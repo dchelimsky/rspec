739921447539d86f68c56caa5ecb38681e817a25
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2007-11-12 12:37:01 +0000

    renamed a couple of examples
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rspec/trunk@2879 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec/spec/spec/runner/class_and_argument_parser_spec.rb
+++ rspec/spec/spec/runner/class_and_argument_parser_spec.rb
@@ -3,7 +3,8 @@ require File.dirname(__FILE__) + '/../../spec_helper.rb'
 module Spec
   module Runner
     describe ClassAndArgumentsParser, ".parse" do
-      it "when passed valid string, splits class names with args" do
+      
+      it "should use a single : to separate class names from arguments" do
         ClassAndArgumentsParser.parse('Foo').should == ['Foo', nil]
         ClassAndArgumentsParser.parse('Foo:arg').should == ['Foo', 'arg']
         ClassAndArgumentsParser.parse('Foo::Bar::Zap:arg').should == ['Foo::Bar::Zap', 'arg']
@@ -12,7 +13,7 @@ module Spec
         ClassAndArgumentsParser.parse('Foo::Bar::Zap:drb://foo,drb://bar').should == ['Foo::Bar::Zap', 'drb://foo,drb://bar']
       end
 
-      it "when passed an empty string, raises error" do
+      it "should raise an error when passed an empty string" do
         lambda do
           ClassAndArgumentsParser.parse('')
         end.should raise_error("Couldn't parse \"\"")
