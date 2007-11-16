13f367df935e42bf88ac2f5ec1aa91c8ff93fe55
btakita <btakita@410327ef-2207-0410-a325-f78bbcb22a5a>
2007-11-16 03:54:54 +0000

    Using VENDOR_DEPS instead of contents of vendor/rails directory.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rspec/trunk@2896 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec/lib/spec/version.rb
+++ rspec/lib/spec/version.rb
@@ -6,7 +6,7 @@ module Spec
       TINY   = 0
       RELEASE_CANDIDATE = nil
 
-      # RANDOM_TOKEN: 0.130025970329143
+      # RANDOM_TOKEN: 0.324233804200756
       REV = "$LastChangedRevision$".match(/LastChangedRevision: (\d+)/)[1]
 
       STRING = [MAJOR, MINOR, TINY].join('.')
--- rspec_on_rails/lib/spec/rails/version.rb
+++ rspec_on_rails/lib/spec/rails/version.rb
@@ -2,7 +2,7 @@ module Spec
   module Rails
     module VERSION #:nodoc:
       unless defined?(REV)
-        # RANDOM_TOKEN: 0.130025970329143
+        # RANDOM_TOKEN: 0.324233804200756
         REV = "$LastChangedRevision$".match(/LastChangedRevision: (\d+)/)[1]
       end
     end
