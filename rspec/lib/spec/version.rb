e7ee4e29e5755148ccabd398d948af0304088b49
btakita <btakita@410327ef-2207-0410-a325-f78bbcb22a5a>
2007-11-16 06:57:30 +0000

    Run specs even after tests fail.
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@2902 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec/lib/spec/version.rb
+++ rspec/lib/spec/version.rb
@@ -6,7 +6,7 @@ module Spec
       TINY   = 0
       RELEASE_CANDIDATE = nil
 
-      # RANDOM_TOKEN: 0.326096223934643
+      # RANDOM_TOKEN: 0.503937152054462
       REV = "$LastChangedRevision$".match(/LastChangedRevision: (\d+)/)[1]
 
       STRING = [MAJOR, MINOR, TINY].join('.')
