2aee8f471bd3b970173822fcdf3d80ad035bb359
btakita <btakita@410327ef-2207-0410-a325-f78bbcb22a5a>
2007-11-20 06:53:24 +0000

    Added ExampleGroupMethods#before and #after. #it does not work yet.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rspec/trunk@2920 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec_on_rails/lib/spec/rails/version.rb
+++ rspec_on_rails/lib/spec/rails/version.rb
@@ -2,7 +2,7 @@ module Spec
   module Rails
     module VERSION #:nodoc:
       unless defined?(REV)
-        # RANDOM_TOKEN: 0.30479859029983
+        # RANDOM_TOKEN: 0.436225035513035
         REV = "$LastChangedRevision$".match(/LastChangedRevision: (\d+)/)[1]
       end
     end
