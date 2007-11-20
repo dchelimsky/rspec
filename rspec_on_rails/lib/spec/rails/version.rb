dac361adc672f7a71b1f49282f90b7b12ec76308
btakita <btakita@410327ef-2207-0410-a325-f78bbcb22a5a>
2007-11-20 07:06:05 +0000

    Removed duplicate FakeReporter.
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@2922 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec_on_rails/lib/spec/rails/version.rb
+++ rspec_on_rails/lib/spec/rails/version.rb
@@ -2,7 +2,7 @@ module Spec
   module Rails
     module VERSION #:nodoc:
       unless defined?(REV)
-        # RANDOM_TOKEN: 0.431555357229333
+        # RANDOM_TOKEN: 0.794083564148086
         REV = "$LastChangedRevision$".match(/LastChangedRevision: (\d+)/)[1]
       end
     end
