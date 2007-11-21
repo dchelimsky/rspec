1274227f42c872ddb619726a2dca94e377e0b0b7
aslak_hellesoy <aslak_hellesoy@410327ef-2207-0410-a325-f78bbcb22a5a>
2007-11-21 20:27:31 +0000

    Step parameters are now properly passed to the listeners/formatters with the step 'template' and the parameters. This is needed in order to generate a HTML report that can later serve as an editor (ref my HTML prototype i nlogged about, which is now under story_editor).
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@2940 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec_on_rails/lib/spec/rails/version.rb
+++ rspec_on_rails/lib/spec/rails/version.rb
@@ -2,7 +2,7 @@ module Spec
   module Rails
     module VERSION #:nodoc:
       unless defined?(REV)
-        # RANDOM_TOKEN: 0.0251245163526631
+        # RANDOM_TOKEN: 0.206858579732568
         REV = "$LastChangedRevision$".match(/LastChangedRevision: (\d+)/)[1]
       end
     end
