ef3335f7e20daa4d0ce2ffbbedc09a98bdea64ec
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2007-11-21 13:35:12 +0000

    added example of nesting groups
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@2935 410327ef-2207-0410-a325-f78bbcb22a5a

---
+++ rspec/examples/pure/nested_example.rb
@@ -0,0 +1,27 @@
+require File.dirname(__FILE__) + '/spec_helper'
+require File.dirname(__FILE__) + '/stack'
+
+# TODO - this runs and does the right thing except for
+# output - "Stack" never appears. Getting close though.
+
+describe Stack do
+  before(:each) do
+    @stack = Stack.new
+  end
+  
+  describe "when empty" do
+    it "should be empty" do
+      @stack.should be_empty
+    end
+  end
+  
+  describe "when full" do
+    before(:each) do
+      (1..10).each {|n| @stack.push n}
+    end
+    it "should description" do
+      @stack.should be_full
+    end
+  end
+
+end
