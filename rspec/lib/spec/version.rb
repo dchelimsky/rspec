c1e75ddd513038d7f0e49eb581d84249d907ed64
btakita <btakita@410327ef-2207-0410-a325-f78bbcb22a5a>
2007-11-21 07:02:31 +0000

    Back to 100% rcov. Remove ExampleGroupMethods spec.
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@2930 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec/lib/spec/version.rb
+++ rspec/lib/spec/version.rb
@@ -6,7 +6,7 @@ module Spec
       TINY   = 0
       RELEASE_CANDIDATE = nil
 
-      # RANDOM_TOKEN: 0.914547384806048
+      # RANDOM_TOKEN: 0.931903305857768
       REV = "$LastChangedRevision$".match(/LastChangedRevision: (\d+)/)[1]
 
       STRING = [MAJOR, MINOR, TINY].join('.')
