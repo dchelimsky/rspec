c532eaa022dc1f786b5c3670f6bc2b98dec187a7
btakita <btakita@410327ef-2207-0410-a325-f78bbcb22a5a>
2007-11-22 08:23:28 +0000

    Test::Unit::TestCase::ExampleGroup#run conforms to Test::Unit interface.
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@2949 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec_on_rails/lib/spec/rails/version.rb
+++ rspec_on_rails/lib/spec/rails/version.rb
@@ -2,7 +2,7 @@ module Spec
   module Rails
     module VERSION #:nodoc:
       unless defined?(REV)
-        # RANDOM_TOKEN: 0.862439577463296
+        # RANDOM_TOKEN: 0.343531590493084
         REV = "$LastChangedRevision$".match(/LastChangedRevision: (\d+)/)[1]
       end
     end
