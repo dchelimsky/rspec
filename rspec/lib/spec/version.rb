7e36a79c21ec09f1109d0100283b89b4965b71ff
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2007-11-22 10:00:22 +0000

    Applied LH[#134] Only load spec inside spec_helper.rb (Patch from Mark Van Holstyn)
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@2952 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec/lib/spec/version.rb
+++ rspec/lib/spec/version.rb
@@ -6,7 +6,7 @@ module Spec
       TINY   = 0
       RELEASE_CANDIDATE = nil
 
-      # RANDOM_TOKEN: 0.470563063260406
+      # RANDOM_TOKEN: 0.166480082427232
       REV = "$LastChangedRevision$".match(/LastChangedRevision: (\d+)/)[1]
 
       STRING = [MAJOR, MINOR, TINY].join('.')
