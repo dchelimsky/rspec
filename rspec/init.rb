dd9c0c96d3d247887c2cb5edd4d7899d11a77674
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2007-11-22 10:00:22 +0000

    Applied LH[#134] Only load spec inside spec_helper.rb (Patch from Mark Van Holstyn)
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rspec/trunk@2952 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec/init.rb
+++ rspec/init.rb
@@ -1,3 +1,9 @@
-if ENV['RAILS_ENV'] == 'test'
-  require 'spec'
-end
+# Placeholder to satisfy Rails.
+#
+# Do NOT add any require statements to this file. Doing
+# so will cause Rails to load this plugin all of the time.
+#
+# Running 'ruby script/generate rspec' will
+# generate spec/spec_helper.rb, which includes the necessary
+# require statements and configuration. This file should
+# be required by all of your spec files.
--- rspec_on_rails/lib/spec/rails/version.rb
+++ rspec_on_rails/lib/spec/rails/version.rb
@@ -2,7 +2,7 @@ module Spec
   module Rails
     module VERSION #:nodoc:
       unless defined?(REV)
-        # RANDOM_TOKEN: 0.470563063260406
+        # RANDOM_TOKEN: 0.166480082427232
         REV = "$LastChangedRevision$".match(/LastChangedRevision: (\d+)/)[1]
       end
     end
