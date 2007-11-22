f6a27d8d61b733bae7e0a4db264d737c7c16cc8f
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2007-11-22 11:46:56 +0000

    Applied LH[#12] Fix for TM when switching to alternate file (Patch from Trevor Squires)
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@2955 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec/lib/spec/version.rb
+++ rspec/lib/spec/version.rb
@@ -6,7 +6,7 @@ module Spec
       TINY   = 0
       RELEASE_CANDIDATE = nil
 
-      # RANDOM_TOKEN: 0.350418745941162
+      # RANDOM_TOKEN: 0.377629243066586
       REV = "$LastChangedRevision$".match(/LastChangedRevision: (\d+)/)[1]
 
       STRING = [MAJOR, MINOR, TINY].join('.')
