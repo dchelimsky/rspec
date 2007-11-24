c8f3d5a3e988f80d007aacf9da6b1df302b53e19
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2007-11-24 04:19:11 +0000

    fix version files
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rspec/trunk@2963 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec/lib/spec/version.rb
+++ rspec/lib/spec/version.rb
@@ -6,7 +6,7 @@ module Spec
       TINY   = 0
       RELEASE_CANDIDATE = nil
 
-      # RANDOM_TOKEN: 0.885013695004692
+      # RANDOM_TOKEN: 0.148218996143095
       REV = "$LastChangedRevision$".match(/LastChangedRevision: (\d+)/)[1]
 
       STRING = [MAJOR, MINOR, TINY].join('.')
