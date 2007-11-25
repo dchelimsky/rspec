2106ee350f8ca3e1750b8ca4a3f52a377a646641
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2007-11-25 18:50:31 +0000

    renamed file
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@2983 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec/lib/spec/interop/test.rb
+++ rspec/lib/spec/interop/test.rb
@@ -1,7 +1,7 @@
 require "test/unit"
 require "test/unit/testresult"
 
-require 'spec/interop/test/unit/example_group'
+require 'spec/interop/test/unit/testcase'
 require 'spec/interop/test/unit/testsuite_adapter'
 require 'spec/interop/test/unit/autorunner'
 require 'spec/interop/test/unit/testresult'
