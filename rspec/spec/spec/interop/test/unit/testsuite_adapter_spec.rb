5b0ee2a2026e53b31cbd84c626cc43d40d324f07
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2007-11-25 19:23:24 +0000

    better spec for testsuite_adapter
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rspec/trunk@2985 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec/spec/spec/interop/test/unit/testsuite_adapter_spec.rb
+++ rspec/spec/spec/interop/test/unit/testsuite_adapter_spec.rb
@@ -2,15 +2,14 @@ require File.dirname(__FILE__) + '/../../../../spec_helper.rb'
 require 'spec/interop/test/unit/testsuite_adapter'
 
 describe "TestSuiteAdapter#size" do
-  it "should the number of examples in the example group" do
-    group = Class.new(Spec::ExampleGroup).new("foo")
-    adapter = Test::Unit::TestSuiteAdapter.new("name", group)
-    adapter << group.class.class_eval do
+  it "should return the number of examples in the example group" do
+    group = Class.new(Spec::ExampleGroup) do
+      describe("some examples")
       it("bar") {}
-    end
-    adapter << group.class.class_eval do
       it("baz") {}
     end
-    adapter.size.should == 2
+    suite = Test::Unit::TestSuiteAdapter.new("foo", group)
+    group.send :add_examples, suite
+    suite.size.should == 2
   end
 end
