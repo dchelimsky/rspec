3b5b33fe934f293ddc5433fbf80b106057567b8f
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2007-11-25 14:51:39 +0000

    organizing stories
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rspec/trunk@2979 410327ef-2207-0410-a325-f78bbcb22a5a

---
+++ rspec/stories/resources/helpers/story_helper.rb
@@ -0,0 +1,19 @@
+module StoryHelper
+  def ruby(args, stderr)
+    config       = ::Config::CONFIG
+    interpreter  = File::join(config['bindir'], config['ruby_install_name']) + config['EXEEXT']
+    cmd = "#{interpreter} #{args} 2> #{stderr}"
+    #puts "\nCOMMAND: #{cmd}"
+    `#{cmd}`
+  end
+
+  def spec(args, stderr)
+    ruby("#{File.dirname(__FILE__) + '/../../../bin/spec'} #{args}", stderr)
+  end
+
+  def cmdline(args, stderr)
+    ruby("#{File.dirname(__FILE__) + '/../../resources/helpers/cmdline.rb'} #{args}", stderr)
+  end
+  
+  Spec::Story::World.send :include, self
+end
