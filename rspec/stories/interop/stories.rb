e9365c0e9a57e62176435ab657cee8ff747510cb
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2007-11-26 02:16:13 +0000

    added stories/helper so you can run subsets of stories directly
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@2993 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec/stories/interop/stories.rb
+++ rspec/stories/interop/stories.rb
@@ -1,3 +1,5 @@
+require File.join(File.dirname(__FILE__), *%w[.. helper])
+
 with_steps_for :running_rspec do
   Dir["#{File.dirname(__FILE__)}/*"].each do |file|
     run file if File.file?(file) && !(file =~ /\.rb$/)
