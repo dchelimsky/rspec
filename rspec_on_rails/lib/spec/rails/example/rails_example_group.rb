dchelimsky/rspec

commit 181326c856cdeb333295ce6b3c22ec3700872d1b

Make sure test setup is run with rspec
git-svn-id: svn+ssh://rubyforge.org/var/svn/rspec/trunk@3000 410327ef-2207-0410-a325-f78bbcb22a5a

rspec/lib/spec/interop/test/unit/testcase.rb
@@ -27,6 +27,16 @@ module Test
         def suite
           Test::Unit::TestSuiteAdapter.new(self)
         end
+
+        def before_each_parts # :nodoc:
+          if @before_each_parts.nil?
+            @before_each_parts = []
+            @before_each_parts << lambda do
+              setup
+            end
+          end
+          @before_each_parts
+        end
       end
       
       def initialize(example) #:nodoc:

rspec/lib/spec/version.rb
@@ -6,7 +6,7 @@ module Spec
       TINY   = 0
       RELEASE_CANDIDATE = nil
 
-      BUILD_TIME_UTC = 20071128170208
+      BUILD_TIME_UTC = 20071128211848
 
       STRING = [MAJOR, MINOR, TINY].join('.')
       TAG = "REL_#{[MAJOR, MINOR, TINY, RELEASE_CANDIDATE].compact.join('_')}".upcase.gsub(/\.|-/, '_')

rspec_on_rails/lib/spec/rails/version.rb
@@ -1,7 +1,7 @@
 module Spec
   module Rails
     module VERSION #:nodoc:
-      BUILD_TIME_UTC = 20071128170208
+      BUILD_TIME_UTC = 20071128211848
     end
   end
 end

rspec/lib/spec/example/example_group_methods.rb
@@ -90,8 +90,8 @@ module Spec
 
       # Creates an instance of Spec::Example::Example and adds
       # it to a collection of examples of the current behaviour.
-      def it(description=:__generate_docstring, &block)
-        example = create_example(description, &block)
+      def it(description=:__generate_docstring, &implementation)
+        example = create_example(description, &implementation)
         example_objects << example
         example
       end
@@ -238,8 +238,8 @@ module Spec
       end
 
       def run_before_each(example)
-        execute_in_class_hierarchy(false) do |behaviour|
-          example.eval_each_fail_fast(behaviour.before_each_parts)
+        execute_in_class_hierarchy(false) do |example_group|
+          example.eval_each_fail_fast(example_group.before_each_parts)
         end
       end
       
@@ -259,8 +259,8 @@ module Spec
 
       def run_after_all(example)
         return true if dry_run
-        execute_in_class_hierarchy(true) do |behaviour|
-          example.eval_each_fail_slow(behaviour.after_all_parts)
+        execute_in_class_hierarchy(true) do |example_group|
+          example.eval_each_fail_slow(example_group.after_all_parts)
         end
         return true
       rescue Exception => e
@@ -270,8 +270,8 @@ module Spec
       end
       
       def run_after_each(example)
-        execute_in_class_hierarchy(true) do |behaviour|
-          example.eval_each_fail_slow(behaviour.after_each_parts)
+        execute_in_class_hierarchy(true) do |example_group|
+          example.eval_each_fail_slow(example_group.after_each_parts)
         end
       end
 

rspec/spec/spec/example/configuration_spec.rb
@@ -93,7 +93,7 @@ module Spec
     end
 
     ConfigurationCallbacksSpec =
-      describe Configuration, " callbacks", :shared => true do
+      describe Configuration, "callbacks", :shared => true do
         before do
           @config = Configuration.new
           @special_example_group = Class.new(ExampleGroup)

rspec/spec/spec/example/example_group_description_spec.rb
@@ -2,7 +2,7 @@ require File.dirname(__FILE__) + '/../../spec_helper'
 
 module Spec
   module Example
-    describe ExampleGroupDescription, " constructed with a single String" do
+    describe ExampleGroupDescription, "constructed with a single String" do
       attr_reader :description
       before(:each) {@description = ExampleGroupDescription.new("abc")}
       
@@ -27,7 +27,7 @@ module Spec
       end
     end
     
-    describe ExampleGroupDescription, " constructed with a Type" do
+    describe ExampleGroupDescription, "constructed with a Type" do
       attr_reader :description
       before(:each) {@description = ExampleGroupDescription.new(ExampleGroup)}
 
@@ -39,9 +39,9 @@ module Spec
       end
     end
     
-    describe ExampleGroupDescription, " constructed with a Type and a String" do
+    describe ExampleGroupDescription, "constructed with a Type and a String" do
       attr_reader :description
-      before(:each) {@description = ExampleGroupDescription.new(ExampleGroup, " behaving")}
+      before(:each) {@description = ExampleGroupDescription.new(ExampleGroup, "behaving")}
       
       it "should include the type and second String in its name" do
         description.text.should == "Spec::Example::ExampleGroup behaving"
@@ -87,7 +87,7 @@ module Spec
       end
     end
     
-    describe ExampleGroupDescription, " constructed with options" do
+    describe ExampleGroupDescription, "constructed with options" do
       attr_reader :description
       before(:each) do
         @description = ExampleGroupDescription.new(ExampleGroup, :a => "b", :spec_path => "blah")

rspec/spec/spec/example/example_group_spec.rb
@@ -279,7 +279,7 @@ module Spec
     end
 
     class ExampleModuleScopingSpec < ExampleGroup
-      describe ExampleGroup, " via a class definition"
+      describe ExampleGroup, "via a class definition"
 
       module Foo
         module Bar
@@ -302,7 +302,7 @@ module Spec
     end
 
     class ExampleClassVariablePollutionSpec < ExampleGroup
-      describe ExampleGroup, " via a class definition without a class variable"
+      describe ExampleGroup, "via a class definition without a class variable"
 
       it "should not retain class variables from other Example classes" do
         proc do

rspec/spec/spec/example/example_methods_spec.rb
@@ -20,7 +20,7 @@ module Spec
       end
     end
 
-    describe ExampleMethods, " lifecycle" do
+    describe ExampleMethods, "lifecycle" do
       before do
         @options = ::Spec::Runner::Options.new(StringIO.new, StringIO.new)
         @options.formatters << mock("formatter", :null_object => true)

commit 75916b8a85c5debcabf179c7326e6fcd96bc33c3

Fixed bug introduced in r3000 in which rails apps with use_transactional_fixtures were not getting the db cleared out between examples.
git-svn-id: http://rspec.rubyforge.org/svn/trunk@3007 410327ef-2207-0410-a325-f78bbcb22a5a

rspec/lib/spec/interop/test/unit/testcase.rb
@@ -36,7 +36,7 @@ module Test
         self.class.before_each_parts << lambda do
           setup
         end
-
+        
         self.class.after_each_parts << lambda do
           teardown
         end

rspec/lib/spec/version.rb
@@ -6,7 +6,7 @@ module Spec
       TINY   = 0
       RELEASE_CANDIDATE = nil
 
-      BUILD_TIME_UTC = 20071129063351
+      BUILD_TIME_UTC = 20071129064738
 
       STRING = [MAJOR, MINOR, TINY].join('.')
       TAG = "REL_#{[MAJOR, MINOR, TINY, RELEASE_CANDIDATE].compact.join('_')}".upcase.gsub(/\.|-/, '_')

rspec_on_rails/lib/spec/rails/example.rb
@@ -2,13 +2,14 @@ dir = File.dirname(__FILE__)
 
 require 'spec/rails/example/ivar_proxy'
 require 'spec/rails/example/assigns_hash_proxy'
-require 'spec/rails/example/controller_example_group'
-require 'spec/rails/example/functional_example_group'
-require 'spec/rails/example/helper_example_group'
-require 'spec/rails/example/model_example_group'
-require 'spec/rails/example/rails_example_group'
-require 'spec/rails/example/render_observer'
-require 'spec/rails/example/view_example_group'
+
+require "spec/rails/example/render_observer"
+require "spec/rails/example/rails_example_group"
+require "spec/rails/example/model_example_group"
+require "spec/rails/example/functional_example_group"
+require "spec/rails/example/controller_example_group"
+require "spec/rails/example/helper_example_group"
+require "spec/rails/example/view_example_group"
 
 module Spec
   module Rails

rspec_on_rails/lib/spec/rails/example/rails_example_group.rb
@@ -30,6 +30,15 @@ module Spec
           end
         end
 
+        # TODO - do NOT remove this override
+        # of the init in RSpec's core without
+        # running rspec_on_rails examples against
+        # sqlite3 and mysql.
+        def initialize(example) #:nodoc:
+          @_example = example
+          @_result = ::Test::Unit::TestResult.new
+        end
+
         before(:each) {setup}
         after(:each) {teardown}
 

rspec_on_rails/lib/spec/rails/version.rb
@@ -1,7 +1,7 @@
 module Spec
   module Rails
     module VERSION #:nodoc:
-      BUILD_TIME_UTC = 20071129063351
+      BUILD_TIME_UTC = 20071129064738
     end
   end
 end