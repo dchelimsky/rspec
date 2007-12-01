97e1adad8bb4513b718b919b2ed1329ce33ac1a5
btakita <btakita@410327ef-2207-0410-a325-f78bbcb22a5a>
2007-12-01 08:26:39 +0000

    Moved Example#description :__generate_docstring default into Example#description=
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@3024 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec/lib/spec/example/example.rb
+++ rspec/lib/spec/example/example.rb
@@ -4,13 +4,21 @@ module Spec
       PENDING_EXAMPLE_BLOCK = lambda {
         raise Spec::Example::ExamplePendingError.new("Not Yet Implemented")
       }
-      attr_accessor :description
+      attr_reader :description
       attr_reader :from
       
       def initialize(description, &implementation)
         @from = caller(0)[3]
         @implementation = implementation || PENDING_EXAMPLE_BLOCK
-        @description = description
+        self.description = description
+      end
+
+      def description=(value)
+        if value == :__generate_docstring
+          @description = "NO NAME (Because of --dry-run)"
+        else
+          @description = value
+        end
       end
       
       def run_in(context)
