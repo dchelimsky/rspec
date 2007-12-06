2af270a0a2496f4683a362ec44cd3a4f80c683c1
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2007-12-06 12:33:51 +0000

    Patch from Bryan Helmkamp to generate a comment about alternate mocking frameworks. Closes 163.
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@3063 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec_on_rails/generators/rspec/templates/spec_helper.rb
+++ rspec_on_rails/generators/rspec/templates/spec_helper.rb
@@ -10,15 +10,27 @@ Spec::Runner.configure do |config|
   config.use_instantiated_fixtures  = false
   config.fixture_path = RAILS_ROOT + '/spec/fixtures/'
 
+  # == Fixtures
+  #
   # You can declare fixtures for each example_group like this:
   #   describe "...." do
   #     fixtures :table_a, :table_b
   #
   # Alternatively, if you prefer to declare them only once, you can
-  # do so here, like so ...
+  # do so right here. Just uncomment the next line and replace the fixture
+  # names with your fixtures.
   #
-  #   config.global_fixtures = :table_a, :table_b
+  # config.global_fixtures = :table_a, :table_b
   #
   # If you declare global fixtures, be aware that they will be declared
   # for all of your examples, even those that don't use them.
+  #
+  # == Mock Framework
+  #
+  # RSpec uses it's own mocking framework by default. If you prefer to
+  # use mocha, flexmock or RR, uncomment the appropriate line:
+  #
+  # config.mock_with :mocha
+  # config.mock_with :flexmock
+  # config.mock_with :rr
 end
