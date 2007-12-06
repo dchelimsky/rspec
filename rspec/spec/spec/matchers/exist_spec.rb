97e1d59ee4dbcd10a5b343f63fea3b23481906a5
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2007-12-06 01:36:02 +0000

    Reinstated the exist matcher and removed the exist predicate_matcher. Closes LH[#9]"
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@3059 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec/spec/spec/matchers/exist_spec.rb
+++ rspec/spec/spec/matchers/exist_spec.rb
@@ -1,27 +1,5 @@
 require File.dirname(__FILE__) + '/../../spec_helper.rb'
 
-# NOTE - this was initially handled by an explicit matcher, but is now
-# handled by a default set of predicate_matchers.
-
-# module Spec
-#   module Matchers
-#     class Exist
-#       def matches? actual
-#         @actual = actual
-#         @actual.exist?
-#       end
-#       def failure_message
-#         "expected #{@actual.inspect} to exist, but it doesn't."
-#       end
-#       def negative_failure_message
-#         "expected #{@actual.inspect} to not exist, but it does."
-#       end
-#     end
-#     def exist; Exist.new; end
-#   end
-# end
-
-
 class Substance
   def initialize exists, description
     @exists = exists
@@ -45,31 +23,32 @@ class SubstanceTester
   end
 end
 
-describe "should exist" do
+describe "should exist," do
+  
   before(:each) do
     @real = Substance.new true, 'something real'
     @imaginary = Substance.new false, 'something imaginary'
   end
-  
-  it "should pass if target exists" do
-    @real.should exist
-  end
-  
-  it "should fail if target does not exist" do
-    lambda { @imaginary.should exist }.
-      should fail
-  end
-end
 
-describe "should exist, outside of a behavior" do
-  before(:each) do
-    @real = Substance.new true, 'something real'
-    @imaginary = Substance.new false, 'something imaginary'
+  describe "within a behaviour" do
+  
+    it "should pass if target exists" do
+      @real.should exist
+    end
+  
+    it "should fail if target does not exist" do
+      lambda { @imaginary.should exist }.should fail
+    end
+    
   end
-  it "should pass if target exists" do
-    pending("need to either find a way to include stock predicate matchers in Spec::Matchers or add Bret's Exist matcher") do
+
+  describe "outside of a behavior" do
+
+    it "should pass if target exists" do
       real_tester = SubstanceTester.new @real
       real_tester.should_exist
     end
+
   end
+
 end
