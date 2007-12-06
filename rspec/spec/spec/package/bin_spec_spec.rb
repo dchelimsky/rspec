846e56532d0389e202308e6bd988deb0f384732f
aslak_hellesoy <aslak_hellesoy@410327ef-2207-0410-a325-f78bbcb22a5a>
2007-12-06 09:50:48 +0000

    Simplified ExampleGroupFactory leveraging Hash' default value. Made a RubyForker module for forking new rubies of the same type (needed for running specs and stories properly on JRuby and other rubies)
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rspec/trunk@3060 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec/spec/spec/package/bin_spec_spec.rb
+++ rspec/spec/spec/package/bin_spec_spec.rb
@@ -1,17 +1,14 @@
-require "#{File.dirname(__FILE__)}/../../spec_helper"
-require 'rbconfig'
+require File.dirname(__FILE__) + '/../../spec_helper'
+require File.dirname(__FILE__) + '/../../ruby_forker'
 
 describe "The bin/spec script" do
+  include RubyForker
+  
   it "should have no warnings" do
     pending "Hangs on JRuby" if PLATFORM =~ /java/
     spec_path = "#{File.dirname(__FILE__)}/../../../bin/spec"
-    output = nil
 
-    config       = ::Config::CONFIG
-    ruby         = File::join(config['bindir'], config['ruby_install_name']) + config['EXEEXT']
-    IO.popen("#{ruby} -w #{spec_path} --help 2>&1") do |io|
-      output = io.read
-    end
+    output = ruby "-w #{spec_path} --help 2>&1"
     output.should_not =~ /warning/n
   end
 end
--- rspec_on_rails/lib/spec/rails/version.rb
+++ rspec_on_rails/lib/spec/rails/version.rb
@@ -1,7 +1,7 @@
 module Spec
   module Rails
     module VERSION #:nodoc:
-      BUILD_TIME_UTC = 20071206013106
+      BUILD_TIME_UTC = 20071206094325
     end
   end
 end
