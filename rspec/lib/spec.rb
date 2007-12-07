c6d1036a2558b51c5d9e05d534cc181c14506076
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2007-12-07 02:46:01 +0000

    Undo patch for LH[#136] (r3061) because it caused a regression which prevented running single files with the ruby command.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rspec/trunk@3064 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec/lib/spec.rb
+++ rspec/lib/spec.rb
@@ -29,12 +29,8 @@ module Spec
   end
 end
 
-# TODO - checking for Story here is a hack to make sure that
-# the example summary doesn't appear when running stories. What
-# we should really be doing is making sure only the right formatters
-# get loaded.
 at_exit do \
-  unless $! || Spec.const_defined?(:Story) || Spec.run?; \
+  unless $! || Spec.run?; \
     success = Spec.run; \
     exit success if Spec.exit?; \
   end \
