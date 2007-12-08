60df8141e596b03ea8375de12f7f24d67779bb36
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2007-12-08 04:33:40 +0000

    plain text formatter (stories) ignores unkown messages
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@3082 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec/spec/spec/runner/formatter/story/plain_text_formatter_spec.rb
+++ rspec/spec/spec/runner/formatter/story/plain_text_formatter_spec.rb
@@ -11,19 +11,19 @@ module Spec
             @out = StringIO.new
             @options = mock('options')
             @options.stub!(:colour).and_return(false)
-            @reporter = PlainTextFormatter.new(@options, @out)
+            @formatter = PlainTextFormatter.new(@options, @out)
           end
         
           it 'should summarize the number of scenarios when the run ends' do
             # when
-            @reporter.run_started(3)
-            @reporter.scenario_started(nil, nil)
-            @reporter.scenario_succeeded('story', 'scenario1')
-            @reporter.scenario_started(nil, nil)
-            @reporter.scenario_succeeded('story', 'scenario2')
-            @reporter.scenario_started(nil, nil)
-            @reporter.scenario_succeeded('story', 'scenario3')
-            @reporter.run_ended
+            @formatter.run_started(3)
+            @formatter.scenario_started(nil, nil)
+            @formatter.scenario_succeeded('story', 'scenario1')
+            @formatter.scenario_started(nil, nil)
+            @formatter.scenario_succeeded('story', 'scenario2')
+            @formatter.scenario_started(nil, nil)
+            @formatter.scenario_succeeded('story', 'scenario3')
+            @formatter.run_ended
           
             # then
             @out.string.should include('3 scenarios')
@@ -31,14 +31,14 @@ module Spec
         
           it 'should summarize the number of successful scenarios when the run ends' do
             # when
-            @reporter.run_started(3)
-            @reporter.scenario_started(nil, nil)
-            @reporter.scenario_succeeded('story', 'scenario1')
-            @reporter.scenario_started(nil, nil)
-            @reporter.scenario_succeeded('story', 'scenario2')
-            @reporter.scenario_started(nil, nil)
-            @reporter.scenario_succeeded('story', 'scenario3')
-            @reporter.run_ended
+            @formatter.run_started(3)
+            @formatter.scenario_started(nil, nil)
+            @formatter.scenario_succeeded('story', 'scenario1')
+            @formatter.scenario_started(nil, nil)
+            @formatter.scenario_succeeded('story', 'scenario2')
+            @formatter.scenario_started(nil, nil)
+            @formatter.scenario_succeeded('story', 'scenario3')
+            @formatter.run_ended
           
             # then
             @out.string.should include('3 scenarios: 3 succeeded')
@@ -46,14 +46,14 @@ module Spec
         
           it 'should summarize the number of failed scenarios when the run ends' do
             # when
-            @reporter.run_started(3)
-            @reporter.scenario_started(nil, nil)
-            @reporter.scenario_succeeded('story', 'scenario1')
-            @reporter.scenario_started(nil, nil)
-            @reporter.scenario_failed('story', 'scenario2', exception_from { raise RuntimeError, 'oops' })
-            @reporter.scenario_started(nil, nil)
-            @reporter.scenario_failed('story', 'scenario3', exception_from { raise RuntimeError, 'oops' })
-            @reporter.run_ended
+            @formatter.run_started(3)
+            @formatter.scenario_started(nil, nil)
+            @formatter.scenario_succeeded('story', 'scenario1')
+            @formatter.scenario_started(nil, nil)
+            @formatter.scenario_failed('story', 'scenario2', exception_from { raise RuntimeError, 'oops' })
+            @formatter.scenario_started(nil, nil)
+            @formatter.scenario_failed('story', 'scenario3', exception_from { raise RuntimeError, 'oops' })
+            @formatter.run_ended
           
             # then
             @out.string.should include("3 scenarios: 1 succeeded, 2 failed")
@@ -61,14 +61,14 @@ module Spec
         
           it 'should summarize the number of pending scenarios when the run ends' do
             # when
-            @reporter.run_started(3)
-            @reporter.scenario_started(nil, nil)
-            @reporter.scenario_succeeded('story', 'scenario1')
-            @reporter.scenario_started(nil, nil)
-            @reporter.scenario_pending('story', 'scenario2', 'message')
-            @reporter.scenario_started(nil, nil)
-            @reporter.scenario_pending('story', 'scenario3', 'message')
-            @reporter.run_ended
+            @formatter.run_started(3)
+            @formatter.scenario_started(nil, nil)
+            @formatter.scenario_succeeded('story', 'scenario1')
+            @formatter.scenario_started(nil, nil)
+            @formatter.scenario_pending('story', 'scenario2', 'message')
+            @formatter.scenario_started(nil, nil)
+            @formatter.scenario_pending('story', 'scenario3', 'message')
+            @formatter.run_ended
           
             # then
             @out.string.should include("3 scenarios: 1 succeeded, 0 failed, 2 pending")
@@ -76,15 +76,15 @@ module Spec
         
           it "should only count the first failure in one scenario" do
             # when
-            @reporter.run_started(3)
-            @reporter.scenario_started(nil, nil)
-            @reporter.scenario_succeeded('story', 'scenario1')
-            @reporter.scenario_started(nil, nil)
-            @reporter.scenario_failed('story', 'scenario2', exception_from { raise RuntimeError, 'oops' })
-            @reporter.scenario_failed('story', 'scenario2', exception_from { raise RuntimeError, 'oops again' })
-            @reporter.scenario_started(nil, nil)
-            @reporter.scenario_failed('story', 'scenario3', exception_from { raise RuntimeError, 'oops' })
-            @reporter.run_ended
+            @formatter.run_started(3)
+            @formatter.scenario_started(nil, nil)
+            @formatter.scenario_succeeded('story', 'scenario1')
+            @formatter.scenario_started(nil, nil)
+            @formatter.scenario_failed('story', 'scenario2', exception_from { raise RuntimeError, 'oops' })
+            @formatter.scenario_failed('story', 'scenario2', exception_from { raise RuntimeError, 'oops again' })
+            @formatter.scenario_started(nil, nil)
+            @formatter.scenario_failed('story', 'scenario3', exception_from { raise RuntimeError, 'oops' })
+            @formatter.run_ended
           
             # then
             @out.string.should include("3 scenarios: 1 succeeded, 2 failed")
@@ -92,15 +92,15 @@ module Spec
         
           it "should only count the first pending in one scenario" do
             # when
-            @reporter.run_started(3)
-            @reporter.scenario_started(nil, nil)
-            @reporter.scenario_succeeded('story', 'scenario1')
-            @reporter.scenario_started(nil, nil)
-            @reporter.scenario_pending('story', 'scenario2', 'because ...')
-            @reporter.scenario_pending('story', 'scenario2', 'because ...')
-            @reporter.scenario_started(nil, nil)
-            @reporter.scenario_pending('story', 'scenario3', 'because ...')
-            @reporter.run_ended
+            @formatter.run_started(3)
+            @formatter.scenario_started(nil, nil)
+            @formatter.scenario_succeeded('story', 'scenario1')
+            @formatter.scenario_started(nil, nil)
+            @formatter.scenario_pending('story', 'scenario2', 'because ...')
+            @formatter.scenario_pending('story', 'scenario2', 'because ...')
+            @formatter.scenario_started(nil, nil)
+            @formatter.scenario_pending('story', 'scenario3', 'because ...')
+            @formatter.run_ended
           
             # then
             @out.string.should include("3 scenarios: 1 succeeded, 0 failed, 2 pending")
@@ -108,15 +108,15 @@ module Spec
         
           it "should only count a failure before the first pending in one scenario" do
             # when
-            @reporter.run_started(3)
-            @reporter.scenario_started(nil, nil)
-            @reporter.scenario_succeeded('story', 'scenario1')
-            @reporter.scenario_started(nil, nil)
-            @reporter.scenario_pending('story', 'scenario2', exception_from { raise RuntimeError, 'oops' })
-            @reporter.scenario_failed('story', 'scenario2', exception_from { raise RuntimeError, 'oops again' })
-            @reporter.scenario_started(nil, nil)
-            @reporter.scenario_failed('story', 'scenario3', exception_from { raise RuntimeError, 'oops' })
-            @reporter.run_ended
+            @formatter.run_started(3)
+            @formatter.scenario_started(nil, nil)
+            @formatter.scenario_succeeded('story', 'scenario1')
+            @formatter.scenario_started(nil, nil)
+            @formatter.scenario_pending('story', 'scenario2', exception_from { raise RuntimeError, 'oops' })
+            @formatter.scenario_failed('story', 'scenario2', exception_from { raise RuntimeError, 'oops again' })
+            @formatter.scenario_started(nil, nil)
+            @formatter.scenario_failed('story', 'scenario3', exception_from { raise RuntimeError, 'oops' })
+            @formatter.run_ended
           
             # then
             @out.string.should include("3 scenarios: 1 succeeded, 1 failed, 1 pending")
@@ -124,15 +124,15 @@ module Spec
         
           it 'should produce details of the first failure each failed scenario when the run ends' do
             # when
-            @reporter.run_started(3)
-            @reporter.scenario_started(nil, nil)
-            @reporter.scenario_succeeded('story', 'scenario1')
-            @reporter.scenario_started(nil, nil)
-            @reporter.scenario_failed('story', 'scenario2', exception_from { raise RuntimeError, 'oops2' })
-            @reporter.scenario_failed('story', 'scenario2', exception_from { raise RuntimeError, 'oops2 - this one should not appear' })
-            @reporter.scenario_started(nil, nil)
-            @reporter.scenario_failed('story', 'scenario3', exception_from { raise RuntimeError, 'oops3' })
-            @reporter.run_ended
+            @formatter.run_started(3)
+            @formatter.scenario_started(nil, nil)
+            @formatter.scenario_succeeded('story', 'scenario1')
+            @formatter.scenario_started(nil, nil)
+            @formatter.scenario_failed('story', 'scenario2', exception_from { raise RuntimeError, 'oops2' })
+            @formatter.scenario_failed('story', 'scenario2', exception_from { raise RuntimeError, 'oops2 - this one should not appear' })
+            @formatter.scenario_started(nil, nil)
+            @formatter.scenario_failed('story', 'scenario3', exception_from { raise RuntimeError, 'oops3' })
+            @formatter.run_ended
           
             # then
             @out.string.should include("FAILURES:\n")
@@ -145,10 +145,10 @@ module Spec
         
           it 'should produce details of each pending step when the run ends' do
             # when
-            @reporter.run_started(2)
-            @reporter.scenario_pending('story', 'scenario2', 'todo2')
-            @reporter.scenario_pending('story', 'scenario3', 'todo3')
-            @reporter.run_ended
+            @formatter.run_started(2)
+            @formatter.scenario_pending('story', 'scenario2', 'todo2')
+            @formatter.scenario_pending('story', 'scenario3', 'todo3')
+            @formatter.run_ended
           
             # then
             @out.string.should include("Pending Steps:\n")
@@ -158,7 +158,7 @@ module Spec
         
           it 'should document a story title and narrative' do
             # when
-            @reporter.story_started 'story', 'narrative'
+            @formatter.story_started 'story', 'narrative'
           
             # then
             @out.string.should include("Story: story\n\n  narrative")
@@ -166,7 +166,7 @@ module Spec
         
           it 'should document a scenario name' do
             # when
-            @reporter.scenario_started 'story', 'scenario'
+            @formatter.scenario_started 'story', 'scenario'
           
             # then
             @out.string.should include("\n\n  Scenario: scenario")
@@ -174,9 +174,9 @@ module Spec
         
           it 'should document a step by sentence-casing its name' do
             # when
-            @reporter.step_succeeded :given, 'a context'
-            @reporter.step_succeeded :when, 'an event'
-            @reporter.step_succeeded :then, 'an outcome'
+            @formatter.step_succeeded :given, 'a context'
+            @formatter.step_succeeded :when, 'an event'
+            @formatter.step_succeeded :then, 'an outcome'
           
             # then
             @out.string.should include("\n\n    Given a context\n\n    When an event\n\n    Then an outcome")
@@ -184,9 +184,9 @@ module Spec
         
           it 'should document additional givens using And' do
             # when
-            @reporter.step_succeeded :given, 'step 1'
-            @reporter.step_succeeded :given, 'step 2'
-            @reporter.step_succeeded :given, 'step 3'
+            @formatter.step_succeeded :given, 'step 1'
+            @formatter.step_succeeded :given, 'step 2'
+            @formatter.step_succeeded :given, 'step 3'
           
             # then
             @out.string.should include("    Given step 1\n    And step 2\n    And step 3")
@@ -194,9 +194,9 @@ module Spec
         
           it 'should document additional events using And' do
             # when
-            @reporter.step_succeeded :when, 'step 1'
-            @reporter.step_succeeded :when, 'step 2'
-            @reporter.step_succeeded :when, 'step 3'
+            @formatter.step_succeeded :when, 'step 1'
+            @formatter.step_succeeded :when, 'step 2'
+            @formatter.step_succeeded :when, 'step 3'
           
             # then
             @out.string.should include("    When step 1\n    And step 2\n    And step 3")
@@ -204,9 +204,9 @@ module Spec
         
           it 'should document additional outcomes using And' do
             # when
-            @reporter.step_succeeded :then, 'step 1'
-            @reporter.step_succeeded :then, 'step 2'
-            @reporter.step_succeeded :then, 'step 3'
+            @formatter.step_succeeded :then, 'step 1'
+            @formatter.step_succeeded :then, 'step 2'
+            @formatter.step_succeeded :then, 'step 3'
           
             # then
             @out.string.should include("    Then step 1\n    And step 2\n    And step 3")
@@ -214,67 +214,73 @@ module Spec
         
           it 'should document a GivenScenario followed by a Given using And' do
             # when
-            @reporter.step_succeeded :'given scenario', 'a scenario'
-            @reporter.step_succeeded :given, 'a context'
+            @formatter.step_succeeded :'given scenario', 'a scenario'
+            @formatter.step_succeeded :given, 'a context'
           
             # then
             @out.string.should include("    Given scenario a scenario\n    And a context")
           end
         
           it 'should document steps with replaced params' do
-            @reporter.step_succeeded :given, 'a $coloured dog with $n legs', 'pink', 21
+            @formatter.step_succeeded :given, 'a $coloured dog with $n legs', 'pink', 21
             @out.string.should include("  Given a pink dog with 21 legs")
           end
         
           it "should append PENDING for the first pending step" do
-            @reporter.scenario_started('','')
-            @reporter.step_pending(:given, 'a context')
+            @formatter.scenario_started('','')
+            @formatter.step_pending(:given, 'a context')
           
             @out.string.should include('Given a context (PENDING)')
           end
         
           it "should append PENDING for pending after already pending" do
-            @reporter.scenario_started('','')
-            @reporter.step_pending(:given, 'a context')
-            @reporter.step_pending(:when, 'I say hey')
+            @formatter.scenario_started('','')
+            @formatter.step_pending(:given, 'a context')
+            @formatter.step_pending(:when, 'I say hey')
           
             @out.string.should include('When I say hey (PENDING)')
           end
         
           it "should append FAILED for the first failiure" do
-            @reporter.scenario_started('','')
-            @reporter.step_failed(:given, 'a context')
+            @formatter.scenario_started('','')
+            @formatter.step_failed(:given, 'a context')
           
             @out.string.should include('Given a context (FAILED)')
           end
         
           it "should append SKIPPED for the second failiure" do
-            @reporter.scenario_started('','')
-            @reporter.step_failed(:given, 'a context')
-            @reporter.step_failed(:when, 'I say hey')
+            @formatter.scenario_started('','')
+            @formatter.step_failed(:given, 'a context')
+            @formatter.step_failed(:when, 'I say hey')
           
             @out.string.should include('When I say hey (SKIPPED)')
           end
         
           it "should append SKIPPED for the a failiure after PENDING" do
-            @reporter.scenario_started('','')
-            @reporter.step_pending(:given, 'a context')
-            @reporter.step_failed(:when, 'I say hey')
+            @formatter.scenario_started('','')
+            @formatter.step_pending(:given, 'a context')
+            @formatter.step_failed(:when, 'I say hey')
           
             @out.string.should include('When I say hey (SKIPPED)')
           end
         
           it 'should print some white space after each story' do
             # when
-            @reporter.story_ended 'title', 'narrative'
+            @formatter.story_ended 'title', 'narrative'
           
             # then
             @out.string.should include("\n\n")
           end
           
           it "should print nothing for collected_steps" do
-            @reporter.collected_steps(['Given a $coloured $animal', 'Given a $n legged eel'])
+            @formatter.collected_steps(['Given a $coloured $animal', 'Given a $n legged eel'])
             @out.string.should == ("")
+          end
+          
+          it "should ignore messages it doesn't care about" do
+            lambda {
+              @formatter.this_method_does_not_exist
+            }.should_not raise_error
           end
         end
       end
