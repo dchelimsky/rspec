939267b01d584cbc7870acbfe6945b5242d3b51c
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2007-12-20 00:09:05 +0000

    Removed obsolete line in HelperExampleGroup. Closes LH[#199]
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@3193 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec/lib/spec/version.rb
+++ rspec/lib/spec/version.rb
@@ -6,7 +6,7 @@ module Spec
       TINY   = 1
       RELEASE_CANDIDATE = nil
 
-      BUILD_TIME_UTC = 20071219112345
+      BUILD_TIME_UTC = 20071220000304
 
       STRING = [MAJOR, MINOR, TINY].join('.')
       TAG = "REL_#{[MAJOR, MINOR, TINY, RELEASE_CANDIDATE].compact.join('_')}".upcase.gsub(/\.|-/, '_')
--- rspec_on_rails/lib/spec/rails/version.rb
+++ rspec_on_rails/lib/spec/rails/version.rb
@@ -1,7 +1,7 @@
 module Spec
   module Rails
     module VERSION #:nodoc:
-      BUILD_TIME_UTC = 20071219112345
+      BUILD_TIME_UTC = 20071220000304
     end
   end
 end
