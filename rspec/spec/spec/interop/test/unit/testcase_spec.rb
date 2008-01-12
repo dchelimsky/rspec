992ede8bf0902a071d14bffeba701724e2b25325
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2008-01-12 18:00:08 +0000

    sometimes modules are just modules
    git-svn-id: svn+ssh://rubyforge.org/var/svn/rspec/trunk@3221 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec/spec/spec/interop/test/unit/testcase_spec.rb
+++ rspec/spec/spec/interop/test/unit/testcase_spec.rb
@@ -1,7 +1,7 @@
 require File.dirname(__FILE__) + '/test_unit_spec_helper'
 
 describe "Test::Unit::TestCase" do
-  it_should_behave_like "Test::Unit interop"
+  include TestUnitSpecHelper
   it "should pass" do
     dir = File.dirname(__FILE__)
     output = run_script("#{dir}/testcase_spec_with_test_unit.rb")
