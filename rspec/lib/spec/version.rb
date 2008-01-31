cc9577e2894caf475ac6a419c047cc9951e6c312
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2008-01-31 12:08:44 +0000

    ZenTest-3.9.0 compatibility
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@3273 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec/lib/spec/version.rb
+++ rspec/lib/spec/version.rb
@@ -6,7 +6,7 @@ module Spec
       TINY   = 3
       RELEASE_CANDIDATE = nil
 
-      BUILD_TIME_UTC = 20080130034748
+      BUILD_TIME_UTC = 20080131103500
 
       STRING = [MAJOR, MINOR, TINY].join('.')
       TAG = "REL_#{[MAJOR, MINOR, TINY, RELEASE_CANDIDATE].compact.join('_')}".upcase.gsub(/\.|-/, '_')
