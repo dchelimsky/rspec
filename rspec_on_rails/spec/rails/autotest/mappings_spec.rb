9ebaf94cd6e55594d7f247c3230a3f867250cf3c
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2008-01-31 12:08:39 +0000

    got rspec on rails working w/ zentest 3.8.1
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@3271 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec_on_rails/spec/rails/autotest/mappings_spec.rb
+++ rspec_on_rails/spec/rails/autotest/mappings_spec.rb
@@ -1,6 +1,6 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 require File.join(File.dirname(__FILE__), *%w[.. .. .. lib autotest rails_rspec])
-require File.join(File.dirname(__FILE__), *%w[.. .. .. .. rspec spec autotest_helper])
+require File.join(File.dirname(__FILE__), *%w[.. .. .. .. rspec spec autotest_matchers])
 
 describe Autotest::RailsRspec, "file mapping" do
   before(:each) do
@@ -9,23 +9,28 @@ describe Autotest::RailsRspec, "file mapping" do
   end
   
   it "should map model example to model" do
-    @autotest.should map_specs(['spec/models/thing_spec.rb']).to('app/models/thing.rb')
+    @autotest.should map_specs(['spec/models/thing_spec.rb']).
+                            to('app/models/thing.rb')
   end
   
-  xit "should map controller example to controller" do
-    ensure_mapping(['spec/controllers/things_controller_spec.rb'], 'app/controllers/things_controller.rb')
+  it "should map controller example to controller" do
+    @autotest.should map_specs(['spec/controllers/things_controller_spec.rb']).
+                            to('app/controllers/things_controller.rb')
   end
   
-  xit "should map view.rhtml" do
-    ensure_mapping(['spec/views/things/index.rhtml_spec.rb'], 'app/views/things/index.rhtml')
+  it "should map view.rhtml" do
+    @autotest.should map_specs(['spec/views/things/index.rhtml_spec.rb']).
+                            to('app/views/things/index.rhtml')
   end
   
-  xit "should map view.rhtml with underscores in example filename" do
-    ensure_mapping(['spec/views/things/index_rhtml_spec.rb'], 'app/views/things/index.rhtml')
+  it "should map view.rhtml with underscores in example filename" do
+    @autotest.should map_specs(['spec/views/things/index_rhtml_spec.rb']).
+                            to('app/views/things/index.rhtml')
   end
   
-  xit "should map view.html.erb" do
-    ensure_mapping(['spec/views/things/index.html.erb_spec.rb'], 'app/views/things/index.html.erb')
+  it "should map view.html.erb" do
+    @autotest.should map_specs(['spec/views/things/index.html.erb_spec.rb']).
+                            to('app/views/things/index.html.erb')
   end
   
 end
