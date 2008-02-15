c293bc8262f6a1f515c5b45ce7f3f32e9fce9e6b
dchelimsky <dchelimsky@410327ef-2207-0410-a325-f78bbcb22a5a>
2008-02-15 06:04:22 +0000

    Undid patch #279 because it introduced new rspec/autotest bugs discovered by users
    git-svn-id: http://rspec.rubyforge.org/svn/trunk@3308 410327ef-2207-0410-a325-f78bbcb22a5a

--- rspec/lib/spec/version.rb
+++ rspec/lib/spec/version.rb
@@ -6,7 +6,7 @@ module Spec
       TINY   = 3
       RELEASE_CANDIDATE = nil
 
-      BUILD_TIME_UTC = 20080213090704
+      BUILD_TIME_UTC = 20080215054643
 
       STRING = [MAJOR, MINOR, TINY].join('.')
       TAG = "REL_#{[MAJOR, MINOR, TINY, RELEASE_CANDIDATE].compact.join('_')}".upcase.gsub(/\.|-/, '_')
