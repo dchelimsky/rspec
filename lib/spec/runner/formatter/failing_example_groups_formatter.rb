f703149272c4404b9724a805d9dcd82bcfc3cf84
David Chelimsky <dchelimsky@gmail.com>
2008-05-22 07:59:46 -0500

    Revert "Remove unneeded defensive code."
    This reverts commit dff59d633d71eeb621bcd6b9f5d1238f78c62647.
    
    Conflicts:
    
    	lib/spec/version.rb

--- lib/spec/runner/formatter/failing_example_groups_formatter.rb
+++ lib/spec/runner/formatter/failing_example_groups_formatter.rb
@@ -5,13 +5,15 @@ module Spec
     module Formatter
       class FailingExampleGroupsFormatter < BaseTextFormatter
         def example_failed(example, counter, failure)
-          description_parts = @example_group.description_parts.collect do |description|
-            description =~ /(.*) \(druby.*\)$/ ? $1 : description
-          end
-          @output.puts ::Spec::Example::ExampleGroupMethods.description_text(*description_parts)
+          if @example_group
+            description_parts = @example_group.description_parts.collect do |description|
+              description =~ /(.*) \(druby.*\)$/ ? $1 : description
+            end
+            @output.puts ::Spec::Example::ExampleGroupMethods.description_text(*description_parts)
 
-          @output.flush
-          @example_group = nil
+            @output.flush
+            @example_group = nil
+          end
         end
 
         def dump_failure(counter, failure)
