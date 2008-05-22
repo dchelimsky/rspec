05138e5c973f3b69dd349570e29bad6ed55c9078
Brian Takita <brian.takita@gmail.com>
2008-05-21 23:47:38 -0700

    Speed up main rspec suite

--- lib/spec/version.rb
+++ lib/spec/version.rb
@@ -6,7 +6,7 @@ module Spec
       TINY   = 3
       RELEASE_CANDIDATE = nil
 
-      BUILD_TIME_UTC = 20080522063005
+      BUILD_TIME_UTC = 20080522064453
 
       STRING = [MAJOR, MINOR, TINY].join('.')
       TAG = "REL_#{[MAJOR, MINOR, TINY, RELEASE_CANDIDATE].compact.join('_')}".upcase.gsub(/\.|-/, '_')
