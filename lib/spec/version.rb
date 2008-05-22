36f74abcba1c916a04ed0688b8ea1452ff04951d
Brian Takita <brian.takita@gmail.com>
2008-05-21 23:33:48 -0700

    ran tests

--- lib/spec/version.rb
+++ lib/spec/version.rb
@@ -6,7 +6,7 @@ module Spec
       TINY   = 3
       RELEASE_CANDIDATE = nil
 
-      BUILD_TIME_UTC = 20080522044210
+      BUILD_TIME_UTC = 20080522063005
 
       STRING = [MAJOR, MINOR, TINY].join('.')
       TAG = "REL_#{[MAJOR, MINOR, TINY, RELEASE_CANDIDATE].compact.join('_')}".upcase.gsub(/\.|-/, '_')
