03cea68df522c0036768cc22c9164c77f631f298
Brian Takita <brian.takita@gmail.com>
2008-06-01 14:37:00 -0700

    Ignoring lib/spec/runner.rb so we no longer need to have \ to satisfy rcov.

--- lib/spec/version.rb
+++ lib/spec/version.rb
@@ -6,7 +6,7 @@ module Spec
       TINY   = 4
       RELEASE_CANDIDATE = nil
 
-      BUILD_TIME_UTC = 20080601212037
+      BUILD_TIME_UTC = 20080601213257
 
       STRING = [MAJOR, MINOR, TINY].join('.')
       TAG = "REL_#{[MAJOR, MINOR, TINY, RELEASE_CANDIDATE].compact.join('_')}".upcase.gsub(/\.|-/, '_')
