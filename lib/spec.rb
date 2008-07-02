d9b6ae83dff8c9ab11ba10739ea646e1bd752d94
David Chelimsky <dchelimsky@gmail.com>
2008-07-02 17:37:22 -0500

    using hoe (more work to do)

--- lib/spec.rb
+++ lib/spec.rb
@@ -1,4 +1,3 @@
-require 'spec/version'
 require 'spec/matchers'
 require 'spec/expectations'
 require 'spec/example'
@@ -26,6 +25,26 @@ module Spec
     
     def exit?
       !Object.const_defined?(:Test) || Test::Unit.run?
+    end
+  end
+
+  module VERSION
+    unless defined? MAJOR
+      MAJOR  = 1
+      MINOR  = 1
+      TINY   = 4
+      RELEASE_CANDIDATE = nil
+
+      BUILD_TIME_UTC = 20080628203842
+
+      STRING = [MAJOR, MINOR, TINY].join('.')
+      TAG = "REL_#{[MAJOR, MINOR, TINY, RELEASE_CANDIDATE].compact.join('_')}".upcase.gsub(/\.|-/, '_')
+      FULL_VERSION = "#{[MAJOR, MINOR, TINY, RELEASE_CANDIDATE].compact.join('.')} (build #{BUILD_TIME_UTC})"
+
+      NAME   = "RSpec"
+      URL    = "http://rspec.rubyforge.org/"  
+
+      DESCRIPTION = "#{NAME}-#{FULL_VERSION} - BDD for Ruby\n#{URL}"
     end
   end
 end
