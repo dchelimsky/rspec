fa494fc9bddfac0fbd2a89ca202e88d7904df1b7
David Chelimsky <dchelimsky@gmail.com>
2008-09-28 11:34:56 -0500

    add failing example (pending) for [#501]

--- spec/spec/mocks/hash_including_matcher_spec.rb
+++ spec/spec/mocks/hash_including_matcher_spec.rb
@@ -4,34 +4,51 @@ module Spec
   module Mocks
     module ArgumentConstraints
       describe HashIncludingConstraint do
-      
-        it "should match the same hash" do
-          hash_including(:a => 1).should == {:a => 1}
-        end
-      
-        it "should not match a non-hash" do
-          hash_including(:a => 1).should_not == 1
-        end
-
-        it "should match a hash with extra stuff" do
-          hash_including(:a => 1).should == {:a => 1, :b => 2}
-        end
-      
-        it "should not match a hash with a missing key" do
-          hash_including(:a => 1).should_not == {:b => 2}
-        end
-
-        it "should not match a hash with an incorrect value" do
-          hash_including(:a => 1, :b => 2).should_not == {:a => 1, :b => 3}
-        end
         
-        it "should not match when values are nil but keys are different" do
-          hash_including(:a => nil).should_not == {:b => nil}
-        end
-
         it "should describe itself properly" do
           HashIncludingConstraint.new(:a => 1).description.should == "hash_including(:a=>1)"
         end      
+
+        describe "passing" do
+          it "should match the same hash" do
+            hash_including(:a => 1).should == {:a => 1}
+          end
+
+          it "should match a hash with extra stuff" do
+            hash_including(:a => 1).should == {:a => 1, :b => 2}
+          end
+          
+          describe "when matching against other constraints" do
+            it "should match an int against anything()" do
+              hash_including(:a => anything, :b => 2).should == {:a => 1, :b => 2}
+            end
+
+            it "should match a string against anything()" do
+              pending("fix to http://rspec.lighthouseapp.com/projects/5645/tickets/501") do
+                hash_including(:a => anything, :b => 2).should == {:a => "1", :b => 2}
+              end
+            end
+          end
+        end
+        
+        describe "failing" do
+          it "should not match a non-hash" do
+            hash_including(:a => 1).should_not == 1
+          end
+
+
+          it "should not match a hash with a missing key" do
+            hash_including(:a => 1).should_not == {:b => 2}
+          end
+
+          it "should not match a hash with an incorrect value" do
+            hash_including(:a => 1, :b => 2).should_not == {:a => 1, :b => 3}
+          end
+
+          it "should not match when values are nil but keys are different" do
+            hash_including(:a => nil).should_not == {:b => nil}
+          end
+        end
       end
     end
   end
