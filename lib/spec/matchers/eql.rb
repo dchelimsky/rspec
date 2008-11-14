50129741b8a1b41acae91b92bf3346bc6fa3e2e0
David Chelimsky <dchelimsky@gmail.com>
2008-11-13 21:02:40 -0600

    use simple_matcher for eql

--- lib/spec/matchers/eql.rb
+++ lib/spec/matchers/eql.rb
@@ -1,34 +1,11 @@
 module Spec
   module Matchers
   
-    class Eql #:nodoc:
-      def initialize(expected)
-        @expected = expected
-      end
-  
-      def matches?(given)
-        @given = given
-        @given.eql?(@expected)
-      end
-
-      def failure_message
-        return "expected #{@expected.inspect}, got #{@given.inspect} (using .eql?)", @expected, @given
-      end
-      
-      def negative_failure_message
-        return "expected #{@given.inspect} not to equal #{@expected.inspect} (using .eql?)", @expected, @given
-      end
-
-      def description
-        "eql #{@expected.inspect}"
-      end
-    end
-    
     # :call-seq:
     #   should eql(expected)
     #   should_not eql(expected)
     #
-    # Passes if given and expected are of equal value, but not necessarily the same object.
+    # Passes if actual and expected are of equal value, but not necessarily the same object.
     #
     # See http://www.ruby-doc.org/core/classes/Object.html#M001057 for more information about equality in Ruby.
     #
@@ -37,7 +14,12 @@ module Spec
     #   5.should eql(5)
     #   5.should_not eql(3)
     def eql(expected)
-      Matchers::Eql.new(expected)
+      simple_matcher do |actual, matcher|
+        matcher.failure_message = "expected #{expected.inspect}, got #{actual.inspect} (using .eql?)", expected, actual
+        matcher.negative_failure_message = "expected #{actual.inspect} not to equal #{expected.inspect} (using .eql?)", expected, actual
+        matcher.description = "eql #{expected.inspect}"
+        actual.eql?(expected)
+      end
     end
   end
 end
