22fbd35323f3e2698097d4e2bc2ff579e347bac7
David Chelimsky <dchelimsky@gmail.com>
2008-11-13 21:07:35 -0600

    use simple_matcher for exist matcher

--- lib/spec/matchers/exist.rb
+++ lib/spec/matchers/exist.rb
@@ -1,22 +1,16 @@
 module Spec
   module Matchers
-    class Exist
-      def matches?(given)
-        @given = given
-        @given.exist?
-      end
-      def failure_message
-        "expected #{@given.inspect} to exist, but it doesn't."
-      end
-      def negative_failure_message
-        "expected #{@given.inspect} to not exist, but it does."
-      end
-    end
     # :call-seq:
     #   should exist
     #   should_not exist
     #
-    # Passes if given.exist?
-    def exist; Exist.new; end
+    # Passes if actual.exist?
+    def exist
+      simple_matcher do |actual, matcher|
+        matcher.failure_message = "expected #{actual.inspect} to exist, but it doesn't."
+        matcher.negative_failure_message = "expected #{actual.inspect} to not exist, but it does."
+        actual.exist?
+      end
+    end
   end
 end
