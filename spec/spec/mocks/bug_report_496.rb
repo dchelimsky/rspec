6fa3315b06b3a933cb7a05b129717607e7cecc9f
David Chelimsky <dchelimsky@gmail.com>
2008-11-16 16:22:54 -0600

    wrap bug report examples in module

--- spec/spec/mocks/bug_report_496.rb
+++ spec/spec/mocks/bug_report_496.rb
@@ -1,16 +1,18 @@
 require File.dirname(__FILE__) + '/../../spec_helper.rb'
 
-class BaseClass
-end
+module BugReport496
+  class BaseClass
+  end
 
-class SubClass < BaseClass
-end
+  class SubClass < BaseClass
+  end
 
-describe "a message expectation on a base class object" do
-  it "should correctly pick up message sent to it subclass" do
-    pending("fix for http://rspec.lighthouseapp.com/projects/5645-rspec/tickets/496") do
-      BaseClass.should_receive(:new).once
-      SubClass.new
+  describe "a message expectation on a base class object" do
+    it "should correctly pick up message sent to it subclass" do
+      pending("fix for http://rspec.lighthouseapp.com/projects/5645/tickets/496") do
+        BaseClass.should_receive(:new).once
+        SubClass.new
+      end
     end
   end
 end
