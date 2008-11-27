55c566efa74626efa7cb34b67deeec53ea019e09
David Chelimsky <dchelimsky@gmail.com>
2008-11-27 06:16:55 -0600

    reformat match array examples

--- spec/spec/matchers/match_array_spec.rb
+++ spec/spec/matchers/match_array_spec.rb
@@ -10,71 +10,72 @@ describe "array.should =~ other_array" do
   end
 
   it "should fail if target includes extra items" do
-    message =  "expected collection contained:  [1, 2, 3]\n"
-    message += "actual collection contained:    [1, 2, 3, 4]\n"
-    message += "the extra elements were:        [4]\n"
-
     lambda {
       [1,2,3,4].should =~ [1,2,3]
-    }.should fail_with(message)
+    }.should fail_with(<<-MESSAGE)
+expected collection contained:  [1, 2, 3]
+actual collection contained:    [1, 2, 3, 4]
+the extra elements were:        [4]
+MESSAGE
   end
 
   it "should fail if target is missing items" do
-    message =  "expected collection contained:  [1, 2, 3]\n"
-    message += "actual collection contained:    [1, 2]\n"
-    message += "the missing elements were:      [3]\n"
-
     lambda {
       [1,2].should =~ [1,2,3]
-    }.should fail_with(message)
+    }.should fail_with(<<-MESSAGE)
+expected collection contained:  [1, 2, 3]
+actual collection contained:    [1, 2]
+the missing elements were:      [3]
+MESSAGE
   end
 
   it "should fail if target is missing items and has extra items" do
-    message =  "expected collection contained:  [1, 2, 3]\n"
-    message += "actual collection contained:    [1, 2, 4]\n"
-    message += "the missing elements were:      [3]\n"
-    message += "the extra elements were:        [4]\n"
 
     lambda {
       [1,2,4].should =~ [1,2,3]
-    }.should fail_with(message)
+    }.should fail_with(<<-MESSAGE)
+expected collection contained:  [1, 2, 3]
+actual collection contained:    [1, 2, 4]
+the missing elements were:      [3]
+the extra elements were:        [4]
+MESSAGE
   end
 
   it "should sort items in the error message" do
-    message =  "expected collection contained:  [1, 2, 3, 4]\n"
-    message += "actual collection contained:    [1, 2, 5, 6]\n"
-    message += "the missing elements were:      [3, 4]\n"
-    message += "the extra elements were:        [5, 6]\n"
-
     lambda {
       [6,2,1,5].should =~ [4,1,2,3]
-    }.should fail_with(message)
+    }.should fail_with(<<-MESSAGE)
+expected collection contained:  [1, 2, 3, 4]
+actual collection contained:    [1, 2, 5, 6]
+the missing elements were:      [3, 4]
+the extra elements were:        [5, 6]
+MESSAGE
   end
 
   it "should accurately report extra elements when there are duplicates" do
-    message =  "expected collection contained:  [1, 5]\n"
-    message += "actual collection contained:    [1, 1, 1, 5]\n"
-    message += "the extra elements were:        [1, 1]\n"
-
     lambda {
       [1,1,1,5].should =~ [1,5]
-    }.should fail_with(message)
+    }.should fail_with(<<-MESSAGE)
+expected collection contained:  [1, 5]
+actual collection contained:    [1, 1, 1, 5]
+the extra elements were:        [1, 1]
+MESSAGE
   end
 
   it "should accurately report missing elements when there are duplicates" do
-    message =  "expected collection contained:  [1, 1, 5]\n"
-    message += "actual collection contained:    [1, 5]\n"
-    message += "the missing elements were:      [1]\n"
-
     lambda {
       [1,5].should =~ [1,1,5]
-    }.should fail_with(message)
+    }.should fail_with(<<-MESSAGE)
+expected collection contained:  [1, 1, 5]
+actual collection contained:    [1, 5]
+the missing elements were:      [1]
+MESSAGE
   end
 
 end
 
-describe "should_not [:with, :multiple, :args]" do
-  it "should not exist" do
+describe "should_not =~ [:with, :multiple, :args]" do
+  it "should not be supported" do
     lambda {
       [1,2,3].should_not =~ [1,2,3]
     }.should fail_with(/Matcher does not support should_not/)
