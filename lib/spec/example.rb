7a6058aca573f9d0c1cdf0b7be73c9966cbaae44
David Chelimsky <dchelimsky@gmail.com>
2008-12-16 09:47:52 -0600

    move ExampleGroup and Example doco to Spec::Example

--- lib/spec/example.rb
+++ lib/spec/example.rb
@@ -1,22 +1,162 @@
 module Spec
+  # == ExampleGroups and Code Examples
+  #
+  # A Code Example is an executable example of how a bit of code is expected
+  # to behave.
+  #
+  # An Example Group is a group of code examples.
+  #
+  # RSpec exposes a DSL to describe groups of examples.
+  #  
+  #   describe Account do
+  #     it "should have a balance of $0" do
+  #       account = Account.new
+  #       account.balance.should == Money.new(0, :dollars)
+  #     end
+  #   end
+  #  
+  # == Before and After
+  #  
+  # You can use before() and after() to extract common code within an Example
+  # Group. Both methods take an optional scope argument so you can run the
+  # block before :each example or before :all examples
+  #  
+  #   describe "..." do
+  #     before :all do
+  #       ...
+  #     end
+  #  
+  #     before :each do
+  #       ...
+  #     end
+  #  
+  #     it "should do something" do
+  #       ...
+  #     end
+  #  
+  #     it "should do something else" do
+  #       ...
+  #     end
+  #  
+  #     after :each do
+  #       ...
+  #     end
+  #  
+  #     after :all do
+  #       ...
+  #     end
+  #  
+  #   end
+  #  
+  # The <tt>before :each</tt> block will run before each of the examples, once
+  # for each example. Likewise, the <tt>after :each</tt> block will run after
+  # each of the examples.
+  #  
+  # It is also possible to specify a <tt>before :all</tt> and <tt>after
+  # :all</tt> block that will run only once for each behaviour, respectively
+  # before the first <code>before :each</code> and after the last <code>after
+  # :each</code>. The use of these is generally discouraged, because it
+  # introduces dependencies between the examples. Still, it might prove useful
+  # for very expensive operations if you know what you are doing.
+  #  
+  # == Local helper methods
+  #  
+  # You can include local helper methods by simply expressing them within a
+  # context:
+  #  
+  #   describe "..." do
+  #    
+  #     it "..." do
+  #       helper_method
+  #     end
+  #  
+  #     def helper_method
+  #       ...
+  #     end
+  #  
+  #   end
+  #  
+  # == Included helper methods
+  #  
+  # You can include helper methods in multiple contexts by expressing them
+  # within a module, and then including that module in your context:
+  #  
+  #   module AccountExampleHelperMethods
+  #     def helper_method
+  #       ...
+  #     end
+  #   end
+  #  
+  #   describe "A new account" do
+  #     include AccountExampleHelperMethods
+  #     before do
+  #       @account = Account.new
+  #     end
+  #  
+  #     it "should have a balance of $0" do
+  #       helper_method
+  #       @account.balance.should eql(Money.new(0, :dollars))
+  #     end
+  #   end
+  #  
+  # == Shared Example Groups
+  #  
+  # You can define a shared Example Group, that may be used on other groups
+  #
+  #  share_examples_for "All Editions" do
+  #    it "all editions behaviour" ...
+  #  end
+  #
+  #  describe SmallEdition do
+  #    it_should_behave_like "All Editions"
+  #   
+  #    it "should do small edition stuff" do
+  #      ...
+  #    end
+  #  end
+  #
+  # You can also assign the shared group to a module and include that
+  #
+  #  share_as :AllEditions do
+  #    it "should do all editions stuff" ...
+  #  end
+  #
+  #  describe SmallEdition do
+  #    it_should_behave_like AllEditions
+  #   
+  #    it "should do small edition stuff" do
+  #      ...
+  #    end
+  #  end
+  #
+  # And, for those of you who prefer to use something more like Ruby, you can
+  # just include the module directly
+  #
+  #  describe SmallEdition do
+  #    include AllEditions
+  #   
+  #    it "should do small edition stuff" do
+  #      ...
+  #    end
+  #  end
   module Example
     class << self
-      def args_and_options(*args)
+      def args_and_options(*args) # :nodoc:
         with_options_from(args) do |options|
           return args, options
         end
       end
 
-      def scope_from(*args)
+      def scope_from(*args) # :nodoc:
         args[0] || :each
       end
 
-      def scope_and_options(*args)
+      def scope_and_options(*args) # :nodoc:
         args, options = args_and_options(*args)
         return scope_from(*args), options
       end
 
-      def add_spec_path_to(args)
+      def add_spec_path_to(args) # :nodoc:
         args << {} unless Hash === args.last
         args.last[:spec_path] ||= caller(0)[2]
       end
