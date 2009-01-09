13826f1ed15227f3eab92054c0980019d8d52db0
David Chelimsky <dchelimsky@gmail.com>
2009-01-09 16:41:41 -0600

    interim gem 1.1.11.7

--- lib/spec/version.rb
+++ lib/spec/version.rb
@@ -4,10 +4,10 @@ module Spec
       MAJOR  = 1
       MINOR  = 1
       TINY   = 11
-      MINESCULE = 6
+      MINESCULE = 7
       
 
-      STRING = [MAJOR, MINOR, TINY, MINESCULE].join('.')
+      STRING = [MAJOR, MINOR, TINY, MINESCULE].compact.join('.')
 
       SUMMARY = "rspec #{STRING}"
     end
