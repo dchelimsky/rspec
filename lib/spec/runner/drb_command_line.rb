84e6a41a164dc718e3b8dd6d2fe8761148de7586
David Chelimsky <dchelimsky@gmail.com>
2009-01-20 07:52:40 -0600

    Remove DRb patch, but init DRb with start_service("druby://localhost:0") to avoid bug associated with start_service(nil).
    See http://redmine.ruby-lang.org/issues/show/496

--- lib/spec/runner/drb_command_line.rb
+++ lib/spec/runner/drb_command_line.rb
@@ -1,20 +1,5 @@
 require "drb/drb"
 
-# Monkeypatch to fix http://redmine.ruby-lang.org/issues/show/496
-module DRb
-  class << self
-    alias orig_start_service start_service
-  end
-  
-  def self.start_service(uri = nil, front = nil, config = nil)
-    if uri.nil?
-      orig_start_service("druby://localhost:0", front, config)
-    else
-      orig_start_service(uri, front, config)
-   end
-  end
-end
-
 module Spec
   module Runner
     # Facade to run specs by connecting to a DRB server
@@ -23,7 +8,8 @@ module Spec
       # CommandLine - making it possible for clients to use both interchangeably.
       def self.run(options)
         begin
-          DRb.start_service
+          # See http://redmine.ruby-lang.org/issues/show/496 as to why we specify localhost:0
+          DRb.start_service("druby://localhost:0")
           spec_server = DRbObject.new_with_uri("druby://127.0.0.1:8989")
           spec_server.run(options.argv, options.error_stream, options.output_stream)
         rescue DRb::DRbConnError => e
