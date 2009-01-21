ba18d326813a0783564e95a36e0fd336bb3fd0d9
David Chelimsky <dchelimsky@gmail.com>
2009-01-21 10:00:25 -0600

    minor refactoring

--- lib/spec/runner/formatter/failing_example_groups_formatter.rb
+++ lib/spec/runner/formatter/failing_example_groups_formatter.rb
@@ -6,20 +6,26 @@ module Spec
       class FailingExampleGroupsFormatter < BaseTextFormatter
         def example_failed(example, counter, failure)
           if @example_group
-            description_parts = @example_group.description_parts.collect do |description|
-              description =~ /(.*) \(druby.*\)$/ ? $1 : description
-            end
-            @output.puts ::Spec::Example::ExampleGroupMethods.description_text(*description_parts)
+            @output.puts ::Spec::Example::ExampleGroupMethods.
+              description_text(*clean_druby_refs(@example_group.description_parts))
 
             @output.flush
             @example_group = nil
           end
         end
-
+        
         def dump_failure(counter, failure)
         end
 
         def dump_summary(duration, example_count, failure_count, pending_count)
+        end
+        
+      private
+      
+        def clean_druby_refs(description_parts)
+          description_parts.collect do |description|
+            description =~ /(.*) \(druby.*\)$/ ? $1 : description
+          end
         end
       end
     end
