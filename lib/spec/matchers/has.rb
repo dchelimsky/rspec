d7f11a7a9e3e0a389576fbe8263a212efebc6abd
David Chelimsky <dchelimsky@gmail.com>
2009-01-28 01:27:33 -0600

    matcher class for have_foo => has_foo?

--- lib/spec/matchers/has.rb
+++ lib/spec/matchers/has.rb
@@ -1,15 +1,35 @@
 module Spec
   module Matchers
-    def has(sym, *args) # :nodoc:
-      simple_matcher do |actual, matcher|
-        def matcher.predicate(sym)
-          "#{sym.to_s.sub("have_","has_")}?".to_sym
-        end
-        matcher.failure_message          = "expected ##{matcher.predicate(sym)}(#{args[0].inspect}) to return true, got false"
-        matcher.negative_failure_message = "expected ##{matcher.predicate(sym)}(#{args[0].inspect}) to return false, got true"
-        matcher.description              = "have key #{args[0].inspect}"
-        actual.__send__(matcher.predicate(sym), *args)
+    
+    class Has
+      
+      def initialize(expected, *args)
+        @expected, @args = expected, args
       end
+      
+      def matches?(actual)
+        actual.__send__(predicate(@expected), *@args)
+      end
+      
+      def failure_message
+        "expected ##{predicate(@expected)}(#{@args[0].inspect}) to return true, got false"
+      end
+      
+      def negative_failure_message
+        "expected ##{predicate(@expected)}(#{@args[0].inspect}) to return false, got true"
+      end
+      
+      def description
+        "have key #{@args[0].inspect}"
+      end
+    
+    private
+    
+      def predicate(sym)
+        "#{sym.to_s.sub("have_","has_")}?".to_sym
+      end
+      
     end
+    
   end
 end
--- lib/spec/matchers/method_missing.rb
+++ lib/spec/matchers/method_missing.rb
@@ -2,7 +2,7 @@ module Spec
   module Matchers
     def method_missing(sym, *args, &block) # :nodoc:
       return Matchers::Be.new(sym, *args) if sym.to_s =~ /^be_/
-      return has(sym, *args) if sym.to_s =~ /^have_/
+      return Matchers::Has.new(sym, *args) if sym.to_s =~ /^have_/
       super
     end
   end
