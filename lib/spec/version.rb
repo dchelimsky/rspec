e7b515b26a9732fef47f67d2cd1c64a34c5ff5fc
David Chelimsky <dchelimsky@gmail.com>
2009-03-04 08:17:48 -0600

    bump version to 1.1.99.11

--- lib/spec/version.rb
+++ lib/spec/version.rb
@@ -4,7 +4,7 @@ module Spec
       MAJOR  = 1
       MINOR  = 1
       TINY   = 99
-      MINESCULE = 10
+      MINESCULE = 11
       
 
       STRING = [MAJOR, MINOR, TINY, MINESCULE].compact.join('.')
