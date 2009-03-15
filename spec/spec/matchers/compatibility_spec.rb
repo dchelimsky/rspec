dc48fa30b324b12e1f4f980b729adb8335668cd2
David Chelimsky <dchelimsky@gmail.com>
2009-03-14 23:27:51 -0500

    silly indentation

--- spec/spec/matchers/compatibility_spec.rb
+++ spec/spec/matchers/compatibility_spec.rb
@@ -8,27 +8,27 @@ end
 
 module Spec
   module Matchers
-      describe Spec::Matchers do
-        (Spec::Matchers.constants.sort).each do |c|
-          if (Class === (klass = Spec::Matchers.const_get(c)))
-            describe klass do
-              if klass.public_instance_methods.include?('failure_message_for_should')
-                describe "called with should" do
-                  subject { klass }
-                  it { should have_public_instance_method('failure_message_for_should')}
-                  it { should have_public_instance_method('failure_message')}
-                end
+    describe Spec::Matchers do
+      (Spec::Matchers.constants.sort).each do |c|
+        if (Class === (klass = Spec::Matchers.const_get(c)))
+          describe klass do
+            if klass.public_instance_methods.include?('failure_message_for_should')
+              describe "called with should" do
+                subject { klass }
+                it { should have_public_instance_method('failure_message_for_should')}
+                it { should have_public_instance_method('failure_message')}
               end
-              if klass.public_instance_methods.include?('failure_message_for_should_not')
-                describe "called with should not" do
-                  subject { klass }
-                  it { should have_public_instance_method('failure_message_for_should_not')}
-                  it { should have_public_instance_method('negative_failure_message')}
-                end
+            end
+            if klass.public_instance_methods.include?('failure_message_for_should_not')
+              describe "called with should not" do
+                subject { klass }
+                it { should have_public_instance_method('failure_message_for_should_not')}
+                it { should have_public_instance_method('negative_failure_message')}
               end
             end
           end
         end
       end
+    end
   end
 end
