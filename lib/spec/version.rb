884d751516b8cdfc338f8b33f7dc331475f2a50b
David Chelimsky <dchelimsky@gmail.com>
2009-03-16 04:36:05 -0500

    append build number (Time.new.to_i) to non-release gems

--- lib/spec/version.rb
+++ lib/spec/version.rb
@@ -1,13 +1,13 @@
-module Spec
-  module VERSION
+module Spec # :nodoc:
+  module VERSION # :nodoc:
     unless defined? MAJOR
+      RELEASE = false
       MAJOR  = 1
       MINOR  = 2
       TINY   = 0
-      MINESCULE = nil
+      BUILD = lambda {RELEASE ? nil : Time.new.to_i}
       
-
-      STRING = [MAJOR, MINOR, TINY, MINESCULE].compact.join('.')
+      STRING = [MAJOR, MINOR, TINY, BUILD.call].compact.join('.')
 
       SUMMARY = "rspec #{STRING}"
     end
