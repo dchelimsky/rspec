44b9e0d682444be1c5e5aaf68979dbb987dd6e04
David Chelimsky <dchelimsky@gmail.com>
2009-03-16 22:24:31 -0500

    more coverage tricks - this is getting old

--- lib/spec/runner/extensions/kernel.rb
+++ lib/spec/runner/extensions/kernel.rb
@@ -1,7 +1,6 @@
 module Kernel
-  
-   unless respond_to?(:debugger)
-    # Starts a debugging session if ruby-debug has been loaded, use the -u/--debugger option to  load it
+  unless respond_to?(:debugger)
+    # Start a debugging session if ruby-debug is loaded with the -u/--debugger option
     def debugger(steps=1)
       # If not then just comment and proceed
       $stderr.puts "debugger statement ignored, use -u or --debugger option on rspec to enable debugging"
