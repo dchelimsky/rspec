0ea5a855552c372433b4d16e3a1ba76a389b430c
David Chelimsky <dchelimsky@gmail.com>
2009-03-16 21:03:58 -0500

    restored loading rubygems when necessary
    - if you really don't want to use rubygems, you
      can set an DONT_MAKE_ME_USE_RUBYGEMS environment
      variable, and RSpec won't force it on you.

--- lib/spec/expectations/differs/default.rb
+++ lib/spec/expectations/differs/default.rb
@@ -1,7 +1,23 @@
 begin
-  require 'diff/lcs' #necessary due to loading bug on some machines - not sure why - DaC
-  require 'diff/lcs/hunk'
-rescue LoadError ; raise "You must gem install diff-lcs to use diffing" ; end
+  require 'diff/lcs'
+rescue LoadError
+  if ENV['DONT_MAKE_ME_USE_RUBYGEMS']
+    raise <<-MESSAGE
+
+We need to load the diff-lcs gem, but you've set
+the DONT_MAKE_ME_USE_RUBYGEMS environment variable 
+and failed to supply a means of loading diff-lcs.
+MESSAGE
+  end
+  begin
+    require 'rubygems'
+    require 'diff/lcs'
+  rescue LoadError
+    raise "You must gem install diff-lcs to use diffing"
+  end
+end
+
+require 'diff/lcs/hunk'
 
 require 'pp'
 
--- spec/autotest/autotest_helper.rb
+++ spec/autotest/autotest_helper.rb
@@ -1,4 +1,17 @@
-require 'autotest'
+begin
+  require 'autotest'
+rescue LoadError
+  if ENV['DONT_MAKE_ME_USE_RUBYGEMS']
+    raise <<-MESSAGE
+
+We need to load autotest, but you've set the
+DONT_MAKE_ME_USE_RUBYGEMS environment variable 
+and failed to supply a means of loading autotest.
+MESSAGE
+  end
+  require 'rubygems'
+  require 'autotest'
+end
 dir = File.dirname(__FILE__)
 require File.expand_path("#{dir}/../spec_helper")
 require File.expand_path("#{dir}/../../lib/autotest/rspec")
