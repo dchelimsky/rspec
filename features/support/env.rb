da25cd30ce29bf14b846327b4dee298ccaf8e438
Ben Mabey <ben@benmabey.com>
2009-03-29 22:03:21 -0600

    * refactored the RspecWorld to be a class.  * added the concept of a working_dir.  * ensure proper setup and cleanup of working_dir.  * added a step definition for more explicit use of the spec command.  * modified the line feature to use new step definition.  * extracted duplications from step_definitions into World as helpers.

--- features/support/env.rb
+++ features/support/env.rb
@@ -6,21 +6,73 @@ require File.dirname(__FILE__) + '/../../spec/ruby_forker'
 require File.dirname(__FILE__) + '/matchers/smart_match'
 
 
-module RspecWorld
+class RspecWorld
   include Spec::Expectations
   include Spec::Matchers
   include RubyForker
 
-  def spec(args, stderr)
-    ruby("#{File.dirname(__FILE__) + '/../../bin/spec'} #{args}", stderr)
+  def self.working_dir
+    @working_dir ||= File.expand_path(File.join(File.dirname(__FILE__), "/../../tmp"))
   end
 
-  def cmdline(args, stderr)
-    ruby("#{File.dirname(__FILE__) + '/../../resources/helpers/cmdline.rb'} #{args}", stderr)
+  def working_dir
+    self.class.working_dir
   end
+
+  def spec(args)
+    ruby("#{spec_file} #{args}")
+  end
+
+  def cmdline(args)
+    ruby("#{cmdline_file} #{args}")
+  end
+
+  def create_file(file_name, contents)
+    @path = File.join(working_dir, file_name)
+    File.open(@path, "w") { |f| f << contents }
+  end
+
+  def last_stdout
+    @stdout
+  end
+
+  def last_stderr
+    @stderr
+  end
+
+  def last_exit_code
+    @exit_code
+  end
+
+  # it seems like this, and the last_* methods, could be moved into RubyForker-- is that being used anywhere but the features?
+  def ruby(args)
+    stderr_file = Tempfile.new('rspec')
+    stderr_file.close
+    Dir.chdir(working_dir) do
+      @stdout = super(args, stderr_file.path)
+    end
+    @stderr = IO.read(stderr_file.path)
+    @exit_code = $?.to_i
+  end
+
+  def spec_file
+    @spec_command ||= File.expand_path(File.join(working_dir, "/../bin/spec"))
+  end
+
+  def cmdline_file
+    @cmdline_file ||= File.expand_path(File.join(working_dir, "/../resources/helpers/cmdline.rb"))
+  end
+
 end
 
-World do |world|
-  world.extend(RspecWorld)
-  world
+# Global Setup
+FileUtils.mkdir(RspecWorld.working_dir) unless test ?d, RspecWorld.working_dir
+
+After do
+  FileUtils.rm_rf RspecWorld.working_dir
+  FileUtils.mkdir RspecWorld.working_dir
+end
+
+World do
+  RspecWorld.new
 end
