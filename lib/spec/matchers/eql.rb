f33f051b2fa590d14ebb06e254fe3b707a4df9e7
David Chelimsky <dchelimsky@gmail.com>
2009-04-11 16:59:05 -0300

    convert eql matcher to Matcher

--- lib/spec/matchers/eql.rb
+++ lib/spec/matchers/eql.rb
@@ -1,29 +1,5 @@
 module Spec
   module Matchers
-    class Eql
-      def initialize(expected)
-        @expected = expected
-      end
-      
-      def matches?(actual)
-        @actual = actual
-        @actual.eql?(@expected)
-      end
-      
-      def failure_message_for_should
-        return "expected #{@expected.inspect}, got #{@actual.inspect} (using .eql?)", @expected, @actual
-      end
-      
-      def failure_message_for_should_not
-        return "expected #{@actual.inspect} not to equal #{@expected.inspect} (using .eql?)", @expected, @actual
-      end
-      
-      def description
-        "eql #{@expected.inspect}"
-      end
-    end
-    
-  
     # :call-seq:
     #   should eql(expected)
     #   should_not eql(expected)
@@ -37,7 +13,30 @@ module Spec
     #   5.should eql(5)
     #   5.should_not eql(3)
     def eql(expected)
-      Eql.new(expected)
+      Matcher.new :eql, expected do |expected|
+        match do |actual|
+          actual.eql?(expected)
+        end
+
+        failure_message_for_should do |actual|
+          <<-MESSAGE
+
+expected #{expected.inspect}
+     got #{actual.inspect}
+
+(compared using eql?)
+MESSAGE
+        end
+
+        failure_message_for_should_not do |actual|
+          <<-MESSAGE
+
+expected #{actual.inspect} not to equal #{expected.inspect}
+
+(compared using eql?)
+MESSAGE
+        end
+      end
     end
   end
 end
