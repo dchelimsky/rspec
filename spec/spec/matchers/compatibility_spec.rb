b633f0514bed854cfdccae5e86fdc232e9176359
David Chelimsky <dchelimsky@gmail.com>
2009-04-15 21:29:24 -0300

    get matcher compatibility working w/ ruby19

--- spec/spec/matchers/compatibility_spec.rb
+++ spec/spec/matchers/compatibility_spec.rb
@@ -2,31 +2,25 @@ require File.dirname(__FILE__) + '/../../spec_helper.rb'
 
 Spec::Matchers.define :have_public_instance_method do |method|
   match do |klass|
-    klass.public_instance_methods.include?(method)
+    klass.public_instance_methods.any? {|m| [method, method.to_sym].include?(m)}
   end
 end
 
-module Spec
-  module Matchers
-    describe Spec::Matchers do
-      (Spec::Matchers.constants.sort).each do |c|
-        if (Class === (klass = Spec::Matchers.const_get(c)))
-          describe klass do
-            if klass.public_instance_methods.include?('failure_message_for_should')
-              describe "called with should" do
-                subject { klass }
-                it { should have_public_instance_method('failure_message_for_should')}
-                it { should have_public_instance_method('failure_message')}
-              end
-            end
-            if klass.public_instance_methods.include?('failure_message_for_should_not')
-              describe "called with should not" do
-                subject { klass }
-                it { should have_public_instance_method('failure_message_for_should_not')}
-                it { should have_public_instance_method('negative_failure_message')}
-              end
-            end
-          end
+(Spec::Matchers.constants.sort).each do |c|
+  if (Class === (klass = Spec::Matchers.const_get(c)))
+    describe klass do
+      if klass.public_instance_methods.any? {|m| ['failure_message_for_should',:failure_message_for_should].include?(m)}
+        describe "called with should" do
+          subject { klass }
+          it { should have_public_instance_method('failure_message_for_should')}
+          it { should have_public_instance_method('failure_message')}
+        end
+      end
+      if klass.public_instance_methods.any? {|m| ['failure_message_for_should_not',:failure_message_for_should_not].include?(m)}
+        describe "called with should not" do
+          subject { klass }
+          it { should have_public_instance_method('failure_message_for_should_not')}
+          it { should have_public_instance_method('negative_failure_message')}
         end
       end
     end
