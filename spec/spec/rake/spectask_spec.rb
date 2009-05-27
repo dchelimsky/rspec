ea5fddc534e2b7aab86a962b69c32da334308b1c
David Chelimsky <dchelimsky@gmail.com>
2009-05-27 05:07:08 -0400

    clean up some examples

--- spec/spec/rake/spectask_spec.rb
+++ spec/spec/rake/spectask_spec.rb
@@ -56,51 +56,46 @@ module Spec
         MockTask.reset_tasks
       end
 
-      it "should execute rakes ruby path by default" do
-        @it = SpecTask.new
+      it "should execute rake's ruby path by default" do
+        task = SpecTask.new
         MockTask.last_instance.call
-        MockTask.last_cmd.should match(/^#{@it.default_ruby_path} /)
+        MockTask.last_cmd.should match(/^#{task.default_ruby_path} /)
       end
 
-      it "should allow ruby_cmd to be overriden" do
-        lambda {SpecTask.new {|t| t.ruby_cmd = "path_to_multiruby"}}.should_not raise_error
-      end
-
-      # A reminder for David C to look at this when he changes the default not to fork
-      it "should run execute the command with system if ruby_cmd is specificed" do
-        @it = SpecTask.new {|t| t.ruby_cmd = "path_to_multiruby"}
-        @it.should_receive(:system).and_return(true)
+      it "should execute the command with system if ruby_cmd is specified" do
+        task = SpecTask.new {|t| t.ruby_cmd = "path_to_multiruby"}
+        task.should_receive(:system).and_return(true)
         MockTask.last_instance.call
       end
 
       it "should execute the ruby_cmd path if specified" do
-        @it = SpecTask.new {|t| t.ruby_cmd = "path_to_multiruby"}
+        SpecTask.new {|t| t.ruby_cmd = "path_to_multiruby"}
         MockTask.last_instance.call
         MockTask.last_cmd.should match(/^path_to_multiruby /)
       end
       
       it "should produce a deprecation warning if the out option is used" do
-        @it = SpecTask.new {|t| t.out = "somewhere_over_the_rainbow"}
+        SpecTask.new {|t| t.out = "somewhere_over_the_rainbow"}
         STDERR.should_receive(:puts).with("The Spec::Rake::SpecTask#out attribute is DEPRECATED and will be removed in a future version. Use --format FORMAT:WHERE instead.")
         MockTask.last_instance.call
       end
       
       it "should produce an error if failure_message is set and the command fails" do
-        @it = SpecTask.new {|t| t.failure_message = "oops"; t.fail_on_error = false}
+        task = SpecTask.new {|t| t.failure_message = "oops"; t.fail_on_error = false}
         STDERR.should_receive(:puts).with("oops")
-        @it.stub(:system).and_return(false)
+        task.stub(:system).and_return(false)
         MockTask.last_instance.call
       end
       
-      it "should produce raise an error if fail_on_error is set and the command fails" do
-        @it = SpecTask.new
-        @it.stub(:system).and_return(false)
+      it "should raise if fail_on_error is set and the command fails" do
+        task = SpecTask.new
+        task.stub(:system).and_return(false)
         lambda {MockTask.last_instance.call}.should raise_error
       end
       
-      it "should produce not raise an error if fail_on_error is not set and the command fails" do
-        @it = SpecTask.new {|t| t.fail_on_error = false}
-        @it.stub(:system).and_return(false)
+      it "should not raise if fail_on_error is not set and the command fails" do
+        task = SpecTask.new {|t| t.fail_on_error = false}
+        task.stub(:system).and_return(false)
         lambda {MockTask.last_instance.call}.should_not raise_error
       end
       
@@ -109,25 +104,25 @@ module Spec
         it "should create a clobber_rcov task" do
           MockTask.stub!(:create_task)
           MockTask.should_receive(:create_task).with(:clobber_rcov)
-          @it = SpecTask.new(:rcov) {|t| t.rcov = true}
+          SpecTask.new(:rcov) {|t| t.rcov = true}
         end
 
         it "should setup the clobber_rcov task to remove the rcov directory" do
-          @it = SpecTask.new(:rcov) {|t| t.rcov = true; t.rcov_dir = "path_to_rcov_directory"}
-          @it.should_receive(:rm_r).with("path_to_rcov_directory")
+          task = SpecTask.new(:rcov) {|t| t.rcov = true; t.rcov_dir = "path_to_rcov_directory"}
+          task.should_receive(:rm_r).with("path_to_rcov_directory")
           MockTask.task(:clobber_rcov).call
         end
 
         it "should make the clobber task depend on clobber_rcov" do
           MockTask.stub!(:create_task)
           MockTask.should_receive(:create_task).with(:clobber => [:clobber_rcov])
-          @it = SpecTask.new(:rcov) {|t| t.rcov = true}
+          SpecTask.new(:rcov) {|t| t.rcov = true}
         end
 
         it "should make the rcov task depend on clobber_rcov" do
           MockTask.stub!(:create_task)
           MockTask.should_receive(:create_task).with(:rcov => :clobber_rcov)
-          @it = SpecTask.new(:rcov) {|t| t.rcov = true}
+          SpecTask.new(:rcov) {|t| t.rcov = true}
         end
       end
     end
