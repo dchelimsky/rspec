2370ecab974b815df04e0716862f3f3773d49577
David Chelimsky <dchelimsky@gmail.com>
2009-06-09 23:27:20 -0500

    fix 1.9 scoping problem in spectask spec

--- spec/spec/rake/spectask_spec.rb
+++ spec/spec/rake/spectask_spec.rb
@@ -4,54 +4,54 @@ require File.dirname(__FILE__) + '/../../../lib/spec/rake/spectask.rb'
 module Spec
   module Rake
 
-    describe SpecTask do
-
-      class MockTask
-        class << self
-          attr_accessor :last_instance, :last_cmd
-        end
-
-        def self.tasks
-          @tasks ||= {}
-        end
-        
-        def self.reset_tasks
-          @tasks = {}
-        end
-        
-        def self.task(name)
-          tasks[name]
-        end
-        
-        def self.register_task(name, block)
-          tasks[name] = block
-        end
-
-        def initialize(name, &block)
-          MockTask.register_task(name, block)
-          MockTask.last_instance = block
-        end
-        
-        def self.create_task(name, &block)
-          new(name, &block)
-        end
+    class MockTask
+      class << self
+        attr_accessor :last_instance, :last_cmd
       end
 
-      class SpecTask
-        def task(name, &block)
-          MockTask.create_task(name, &block)
-        end
-
-        def system(cmd)
-          MockTask.last_cmd = cmd
-          true
-        end
-
-        def default_ruby_path
-          RUBY
-        end
+      def self.tasks
+        @tasks ||= {}
       end
       
+      def self.reset_tasks
+        @tasks = {}
+      end
+      
+      def self.task(name)
+        tasks[name]
+      end
+      
+      def self.register_task(name, block)
+        tasks[name] = block
+      end
+
+      def initialize(name, &block)
+        MockTask.register_task(name, block)
+        MockTask.last_instance = block
+      end
+      
+      def self.create_task(name, &block)
+        new(name, &block)
+      end
+    end
+
+    class SpecTask
+      def task(name, &block)
+        MockTask.create_task(name, &block)
+      end
+
+      def system(cmd)
+        MockTask.last_cmd = cmd
+        true
+      end
+
+      def default_ruby_path
+        RUBY
+      end
+    end
+    
+    describe SpecTask do
+
       before(:each) do
         MockTask.reset_tasks
       end
