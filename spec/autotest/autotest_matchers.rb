ba9f0c2cc748ecce3e90dc1f1ef641c5adad1b5c
David Chelimsky <dchelimsky@gmail.com>
2009-06-30 10:25:26 -0500

    remove some dead code

--- spec/autotest/autotest_matchers.rb
+++ spec/autotest/autotest_matchers.rb
@@ -11,7 +11,7 @@ module Spec
       end
   
       def matches?(autotest)
-        @autotest = prepare autotest
+        @autotest = prepare(autotest)
         @actual = autotest.test_files_for(@file)
         @actual == @specs
       end
@@ -20,21 +20,12 @@ module Spec
         "expected #{@autotest.class} to map #{@specs.inspect} to #{@file.inspect}\ngot #{@actual.inspect}"
       end
   
-      private
-      def prepare autotest
-        stub_found_files autotest
-        stub_find_order autotest
-        autotest
-      end
-  
-      def stub_found_files autotest
-        found_files = @specs.inject({}){|h,f| h[f] = Time.at(0)}
-        autotest.stub!(:find_files).and_return(found_files)
-      end
+    private
 
-      def stub_find_order autotest
+      def prepare(autotest)
         find_order = @specs.dup << @file
         autotest.instance_eval { @find_order = find_order }
+        autotest
       end
 
     end
