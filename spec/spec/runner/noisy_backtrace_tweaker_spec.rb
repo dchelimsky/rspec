adbf2872fb17501f4d5b1aefb7cda7ae603f7cee
David Chelimsky <dchelimsky@gmail.com>
2009-08-09 08:45:08 -0400

    clean up backtrace specs

--- spec/spec/runner/noisy_backtrace_tweaker_spec.rb
+++ spec/spec/runner/noisy_backtrace_tweaker_spec.rb
@@ -2,13 +2,25 @@ require File.dirname(__FILE__) + '/../../spec_helper.rb'
 
 module Spec
   module Runner
-    describe "NoisyBacktraceTweaker" do
+    describe NoisyBacktraceTweaker do
       before(:each) do
         @error = RuntimeError.new
         @tweaker = NoisyBacktraceTweaker.new
       end
-        
-      it "should leave anything in lib spec dir" do
+
+      it "gracefully handles nil backtrace" do
+        lambda do
+          @tweaker.tweak_backtrace(@error)
+        end.should_not raise_error
+      end
+
+      it "cleans up double slashes" do
+        @error.set_backtrace(["/a//b/c//d.rb"])
+        @tweaker.tweak_backtrace(@error)
+        @error.backtrace.should include("/a/b/c/d.rb")
+      end
+
+      it "preserves lines in lib/spec" do
         ["expectations", "mocks", "runner", "stubs"].each do |child|
           @error.set_backtrace(["/lib/spec/#{child}/anything.rb"])
           @tweaker.tweak_backtrace(@error)
@@ -16,37 +28,24 @@ module Spec
         end
       end
 
-      it "should leave anything in spec dir" do
+      it "preserves lines in spec/" do
         @error.set_backtrace(["/lib/spec/expectations/anything.rb"])
         @tweaker.tweak_backtrace(@error)
         @error.backtrace.should_not be_empty
       end
 
-      it "should leave bin spec" do
+      it "preserves lines in bin/spec" do
         @error.set_backtrace(["bin/spec:"])
         @tweaker.tweak_backtrace(@error)
         @error.backtrace.should_not be_empty
       end
 
-      it "should leave custom ignored backtrace patterns" do
+      it "ignores custom patterns" do
         @tweaker.ignore_patterns(/custom_pattern/)
         @error.set_backtrace(["custom_pattern"])
         @tweaker.tweak_backtrace(@error)
         @error.backtrace.should_not be_empty
       end
-
-      it "should not barf on nil backtrace" do
-        lambda do
-          @tweaker.tweak_backtrace(@error)
-        end.should_not raise_error
-      end
-      
-      it "should clean up double slashes" do
-        @error.set_backtrace(["/a//b/c//d.rb"])
-        @tweaker.tweak_backtrace(@error)
-        @error.backtrace.should include("/a/b/c/d.rb")
-      end
-      
     end
   end
 end
--- spec/spec/runner/quiet_backtrace_tweaker_spec.rb
+++ spec/spec/runner/quiet_backtrace_tweaker_spec.rb
@@ -2,80 +2,72 @@ require File.dirname(__FILE__) + '/../../spec_helper.rb'
 
 module Spec
   module Runner
-    describe "QuietBacktraceTweaker" do
+    describe QuietBacktraceTweaker do
       before(:each) do
         @error = RuntimeError.new
         @tweaker = QuietBacktraceTweaker.new
       end
 
-      it "should not barf on nil backtrace" do
+      it "gracefully handles nil backtrace" do
         lambda do
           @tweaker.tweak_backtrace(@error)
         end.should_not raise_error
       end
 
-      it "should remove anything from textmate ruby bundle" do
-        @error.set_backtrace(["/Applications/TextMate.app/Contents/SharedSupport/Bundles/Ruby.tmbundle/Support/tmruby.rb:147"])
+      it "gracefully handle backtraces with newlines" do
+        @error.set_backtrace(["we like\nbin/spec:\nnewlines"])
         @tweaker.tweak_backtrace(@error)
-        @error.backtrace.should be_empty
+        @error.backtrace.should include("we like\nnewlines")
       end
 
-      it "should remove anything in lib spec dir" do
-        ["expectations", "mocks", "runner"].each do |child|
-          element="/lib/spec/#{child}/anything.rb"
-          @error.set_backtrace([element])
-          @tweaker.tweak_backtrace(@error)
-          unless (@error.backtrace.empty?)
-            raise("Should have tweaked away '#{element}'")
-          end
-        end
-      end
-
-      it "should remove mock_frameworks/rspec" do
-        element = "mock_frameworks/rspec"
-        @error.set_backtrace([element])
-        @tweaker.tweak_backtrace(@error)
-        unless (@error.backtrace.empty?)
-          raise("Should have tweaked away '#{element}'")
-        end
-      end
-
-      it "should remove bin spec" do
-        @error.set_backtrace(["bin/spec:"])
-        @tweaker.tweak_backtrace(@error)
-        @error.backtrace.should be_empty
-      end
-
-      it "should remove custom patterns" do
-        element = "/vendor/lib/custom_pattern/"
-        @tweaker.ignore_patterns /custom_pattern/
-        @error.set_backtrace([element])
-        @tweaker.tweak_backtrace(@error)
-        unless (@error.backtrace.empty?)
-          raise("Should have tweaked away '#{element}'")
-        end
-      end
-
-      it "should remove custom patterns added as a string" do
-        element = "/vendor/lib/custom_pattern/"
-        @tweaker.ignore_patterns "custom_pattern"
-        @error.set_backtrace([element])
-        @tweaker.tweak_backtrace(@error)
-        unless (@error.backtrace.empty?)
-          raise("Should have tweaked away '#{element}'")
-        end
-      end
-      
-      it "should clean up double slashes" do
+      it "cleans up double slashes" do
         @error.set_backtrace(["/a//b/c//d.rb"])
         @tweaker.tweak_backtrace(@error)
         @error.backtrace.should include("/a/b/c/d.rb")
       end
 
-      it "should gracefully handle backtraces with newlines" do
-        @error.set_backtrace(["we like\nbin/spec:\nnewlines"])
+      it "preserves lines from textmate ruby bundle" do
+        @error.set_backtrace(["/Applications/TextMate.app/Contents/SharedSupport/Bundles/Ruby.tmbundle/Support/tmruby.rb:147"])
         @tweaker.tweak_backtrace(@error)
-        @error.backtrace.should include("we like\nnewlines")
+        @error.backtrace.should be_empty
+      end
+
+      it "removes lines in lib/spec" do
+        ["expectations", "mocks", "runner"].each do |child|
+          element="/lib/spec/#{child}/anything.rb"
+          @error.set_backtrace([element])
+          @tweaker.tweak_backtrace(@error)
+          @error.backtrace.should be_empty, "Should have removed line with '#{element}'"
+        end
+      end
+
+      it "removes lines in bin/spec" do
+        @error.set_backtrace(["bin/spec:"])
+        @tweaker.tweak_backtrace(@error)
+        @error.backtrace.should be_empty
+      end
+
+      it "removes lines in mock_frameworks/rspec" do
+        element = "mock_frameworks/rspec"
+        @error.set_backtrace([element])
+        @tweaker.tweak_backtrace(@error)
+        @error.backtrace.should be_empty, "Should have removed line with '#{element}'"
+      end
+
+      it "removes custom patterns" do
+        element = "/vendor/lib/custom_pattern/"
+        @tweaker.ignore_patterns /custom_pattern/
+        @error.set_backtrace([element])
+        @tweaker.tweak_backtrace(@error)
+        @error.backtrace.should be_empty, "Should have removed line with '#{element}'"
+      end
+
+      it "removes custom patterns added as a string" do
+        element = "/vendor/lib/custom_pattern/"
+        @tweaker.ignore_patterns "custom_pattern"
+        @error.set_backtrace([element])
+        @tweaker.tweak_backtrace(@error)
+        @error.backtrace.should be_empty, "Should have removed line with '#{element}'"
       end
     end
   end
