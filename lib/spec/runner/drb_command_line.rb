1762539dacedeb73a8a42440aa95c492d50a52ae
David Chelimsky <dchelimsky@gmail.com>
2009-09-06 17:31:53 -0500

    back to trying localhost:0 first, then :0 (was failing on RCR)

--- lib/spec/runner/drb_command_line.rb
+++ lib/spec/runner/drb_command_line.rb
@@ -8,7 +8,11 @@ module Spec
       # CommandLine - making it possible for clients to use both interchangeably.
       def self.run(options)
         begin
-          DRb.start_service("druby://:0")
+          begin; \
+            DRb.start_service("druby://localhost:0"); \
+          rescue SocketError; \
+            DRb.start_service("druby://:0"); \
+          end
           spec_server = DRbObject.new_with_uri("druby://127.0.0.1:8989")
           spec_server.run(options.argv, options.error_stream, options.output_stream)
           true
