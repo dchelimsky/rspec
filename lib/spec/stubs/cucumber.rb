80dfc2a72bc69224bf982615eda6a5d335b5fc97
KURODA Hiraku <hirakuro@gmail.com>
2009-11-21 17:17:18 +0900

    Rename global variable of Spec::Mocks::Space for Cucumber. Because old-name '' is not used in spec/mocks/proxy.rb, but '' is used.
    [#912 state:resolved milestone:'Next Release']

--- lib/spec/stubs/cucumber.rb
+++ lib/spec/stubs/cucumber.rb
@@ -1,5 +1,5 @@
 # This plugs RSpec's mocking/stubbing framework into cucumber
 require 'spec/mocks'
-Before {$rspec_stubs ||= Spec::Mocks::Space.new}
-After  {$rspec_stubs.reset_all}
+Before {$rspec_mocks ||= Spec::Mocks::Space.new}
+After  {$rspec_mocks.reset_all}
 World(Spec::Mocks::ExampleMethods)
