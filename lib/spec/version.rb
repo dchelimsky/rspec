45e9b8ca53c74c03471da09d527bf3c9704bf8de
David Chelimsky <dchelimsky@gmail.com>
2011-04-11 22:12:32 -0500

    bump to 1.3.2

--- lib/spec/version.rb
+++ lib/spec/version.rb
@@ -3,7 +3,7 @@ module Spec # :nodoc:
     unless defined? MAJOR
       MAJOR  = 1
       MINOR  = 3
-      TINY   = 1
+      TINY   = 2
       PRE    = nil
 
       STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
